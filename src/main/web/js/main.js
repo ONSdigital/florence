9fe16d62f0bfda51b780cccf82debab85d644e0b
Pastor <pastor.methods@gmail.com>
2015-02-18 17:42:34 +0000

    Clean up main.js

--- src/main/web/js/main.js
+++ src/main/web/js/main.js
@@ -14,17 +14,14 @@ function($) {
 			$(window.location).trigger("change", {
 				newpage: getPageData()
 			});
-			console.log("got here");
 		}
 	}
 
 	function getPageData(){
-		// var pageurl = window.location.href;
 		pageurldata = pageurl.replace("#!", "data");
 
 		$.ajax({
 			url:pageurldata,
-			// url:"http://localhost:7000/json/home.json",
 			dataType: 'json', // Notice! JSONP <-- P (lowercase)
 			crossDomain: true,
 			// jsonpCallback: 'callback',
@@ -32,16 +29,15 @@ function($) {
 			success:function(data){
 				// do stuff with json (in this case an array)
 				// console.log("Success");
-				console.log(data);
+				// console.log(data);
 				if(data.level === 't1'){
 					console.log('t1 page');
 					t1(data);
 				}
-				if(data.level === 't2'){
+				else if(data.level === 't2'){
 					console.log('t2 page');
 					t2(data);
 				}
-
 			},
 			error:function(){
 				console.log('Error');
@@ -49,41 +45,23 @@ function($) {
 		});
 	}
 
+	var florenceForm = '<div class="florence-editarea-home"><a href="#" class="florence-editbtn">Edit</a>' +
+			'<div class="florence-editform"><a href="#" class="florence-cancelbtn">Cancel</a><form onsubmit="return false;"><textarea id="json"></textarea>' +
+			'<button class="florence-update" >Update</button></form></div></div>';
+
 	function t1(data){
-		// var content;
-		// $.each(data.sections, function() {
-		// 	content += "<section><h1>" + this.name + "</h1><h2>" + this.items[0].name + "</h2>" + this.items[0].uri + "</section>";
-		// });
-
 		var editabledata;
-		$('.slate--home--hero-banner .grid-wrap').prepend('<div class="florence-editarea-home"><a href="#" class="florence-editbtn">Edit</a><div class="florence-editform"><a href="#" class="florence-cancelbtn">Cancel</a><form onsubmit="return false;"><textarea id="json"></textarea><button class="florence-update" >Update</button></form></div></div>');
-
-		$('.florence-editbtn').click(function(){
-			$('.florence-editform').show();
-		});
-
-		$('.florence-cancelbtn').click(function(){
-			$('.florence-editform').hide();
-		});
-
-		$('.florence-update').click(function(){
-			updatePage();
-		});
-
-		$("#json").val(JSON.stringify(data, null, 3));
-
-
+		$('.slate--home--hero-banner .grid-wrap').prepend(florenceForm);
+		editableform(data);
 	}
 
 	function t2(data){
-		// var content;
-		// $.each(data.sections, function() {
-		// 	content += "<section><h1>" + this.name + "</h1><h2>" + this.items[0].name + "</h2>" + this.items[0].uri + "</section>";
-		// });
-
 		var editabledata;
-		$('.panel .grid-wrap').prepend('<div class="florence-editarea-home"><a href="#" class="florence-editbtn">Edit</a><div class="florence-editform"><a href="#" class="florence-cancelbtn">Cancel</a><form onsubmit="return false;"><textarea id="json"></textarea><button class="florence-update" >Update</button></form></div></div>');
-
+		$('.panel').prepend(florenceForm);
+		editableform(data);
+	}
+	// Create a form to modify text and pass JSON data
+	function editableform(data) {
 		$('.florence-editbtn').click(function(){
 			$('.florence-editform').show();
 		});
@@ -95,15 +73,14 @@ function($) {
 		$('.florence-update').click(function(){
 			updatePage();
 		});
-
-		$("#json").val(JSON.stringify(data));
-
+		$("#json").val(JSON.stringify(data, null, 3));
 	}
 
 
-	function renderPage(){
-		getPageData();
-	}
+
+	// function renderPage(){
+	// 	getPageData();
+	// }
 
 	function setupFlorence(){
 		$('head').prepend('<link href="http://localhost:8081/css/main.css" rel="stylesheet" type="text/css">');
