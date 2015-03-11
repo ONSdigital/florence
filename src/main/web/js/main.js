a32ff21fb6bcb11456e3fcd0a821adc8d38fef26
CarlHembrough <carlhuk@gmail.com>
2015-03-11 16:50:54 +0000

    Adding save / approve / publish functionality.

--- src/main/web/js/main.js
+++ src/main/web/js/main.js
@@ -4,9 +4,13 @@
   var pageurl = window.location.href;
   var pageData;
 
+    function getPathName() {
+        var parsedUrl = window.location.href.split("#!/")[1];
+        return parsedUrl;
+    }
 
   //florence menu
-  var florence_menu = 
+  var florence_menu =
     '<section class="fl-panel fl-panel--admin-bar">' +
       '<nav>' +
         '<ul class="fl-admin-menu">' +
@@ -48,7 +52,7 @@
   //florence create menu
 
   //florence edit menu
-  var florence_menu_edit = 
+  var florence_menu_edit =
   '<section class="fl-panel fl-panel--editor">' +
     '<nav class="fl-panel--editor__breadcrumb">' +
       '<input type="text" value="" placeholder="Publish owner" class="fl-panel--editor__publish-owner" />' +
@@ -57,24 +61,11 @@
     '<textarea class="fl-editor" name="fl-editor" cols="40" rows="5"></textarea>' +
     '<nav class="fl-panel--editor__nav">' +
       '<button class="fl-panel--editor__nav__save">Save</button>' +
+      '<button class="fl-panel--editor__nav__approve">Approve</button>' +
+      '<button class="fl-panel--editor__nav__publish">Publish</button>' +
     '</nav>' +
   '</section>';
 
-  //florence approve menu
-
-
-
-
-  // URI simple parser
-  var parser = document.createElement('a');
-  parser.href = pageurl.replace("#!/", ""); //takes out #! from Angular.js
-  parser.protocol; // => "http:"
-  parser.hostname; // => "example.com"
-  parser.port; // => "3000"
-  parser.pathname; // => "/pathname/"
-  parser.search; // => "?search=test"
-  parser.hash; // => "#hash"
-  parser.host; // => "example.com:3000"
 
   setupFlorence();
 
@@ -82,7 +73,7 @@
   function setupFlorence(){
     $('head').prepend('<link href="http://localhost:8081/css/main.min.css" rel="stylesheet" type="text/css">');
     var bodycontent = $('body').html();
-    
+
 
     $('body').wrapInner('<section class="fl-panel fl-panel--preview"><div class="fl-panel--preview__inner"></div></section>');
     // $('body').wrapInner('<section class="fl-container"></section>');
@@ -94,7 +85,106 @@
 
   }
 
-  function LoadPageDataIntoEditor(){
+    function save(collectionName, data) {
+
+
+        // Create the collection
+        $.ajax({
+            url: "http://localhost:8082/collection",
+            dataType: 'json',
+            crossDomain: true,
+            type: 'POST',
+            data: JSON.stringify({name: collectionName}),
+            success: function () {
+                console.log("Collection created")
+                openFileForEditing(collectionName, data);
+            },
+            error: function () {
+                console.log('Error creating collection');
+                openFileForEditing(collectionName, data);
+            }
+        });
+    }
+
+    function openFileForEditing(collectionName, data) {
+        // Open the file for editing
+        $.ajax({
+            url: "http://localhost:8082/edit/" + collectionName,
+            dataType: 'json',
+            crossDomain: true,
+            type: 'POST',
+            data: JSON.stringify({uri: getPathName() + "/data.json"}),
+            success: function () {
+                console.log("File opened for editing");
+                updateContent(collectionName, data);
+            },
+            error: function () {
+                console.log('Error opening file for edit');
+                updateContent(collectionName, data);
+                console.log("update content called.");
+            }
+        });
+    }
+
+    function updateContent(collectionName, data) {
+        // Update content
+        $.ajax({
+            url: "http://localhost:8082/content/" + collectionName + "?uri=" + getPathName() + "/data.json",
+            dataType: 'json',
+            crossDomain: true,
+            type: 'POST',
+            data: data,
+            success: function (message) {
+                console.log("Updating completed" + message);
+            },
+            error: function (error) {
+                console.log(error);
+            }
+        });
+
+        document.cookie = 'collection=' + collectionName;
+    }
+
+    function approve(collectionName) {
+
+
+        // Open the file for editing
+        $.ajax({
+            url: "http://localhost:8082/approve/" + collectionName + "?uri=" + getPathName() + "/data.json",
+            dataType: 'json',
+            crossDomain: true,
+            type: 'POST',
+            success: function () {
+                console.log("File approved!")
+                alert("Your file is now approved");
+            },
+            error: function () {
+                console.log('Error');
+            }
+        });
+    }
+
+    function publish(collectionName) {
+
+        // Open the file for editing
+        $.ajax({
+            url: "http://localhost:8082/publish/" + collectionName,
+            dataType: 'json',
+            crossDomain: true,
+            type: 'POST',
+            success: function () {
+                console.log("File published")
+                document.cookie = 'collection=; expires=Thu, 01 Jan 1970 00:00:01 GMT;';
+                alert("Published!");
+            },
+            error: function () {
+                console.log('Error');
+            }
+        });
+    }
+
+
+    function LoadPageDataIntoEditor(){
 
     // var pageurl = window.location.href;
 
@@ -127,7 +217,7 @@
   function watchForEditorChanges(){}
 
   function setupFlorenceWorkspace(caller){
-    
+
     //console.log(caller.parent().attr('class'));
     // console.log($('.fl-panel--preview__inner').height())
 
@@ -164,21 +254,28 @@
       $('.fl-panel--editor__nav__save').click(function() {
         if($('.fl-panel--editor__publish-owner').val().length != 0 && $('.fl-panel--editor__publish-id').val().length != 0){
           pageData = $('.fl-editor').val();
-          updatePage();
+          save("testCollection", pageData);
           //console.log(parser.pathname);
         } else {
           alert('Publish owner and Publish id cannot be blank!');
         }
-        
+
       });
-      
+
+        $('.fl-panel--editor__nav__approve').click(function () {
+            approve(collectionName);
+        });
+        $('.fl-panel--editor__nav__publish').click(function () {
+            publish(collectionName);
+        });
+
     }
 
     if (caller.parent().hasClass('fl-main-menu__item--approve')){
       //
     }
 
-    
+
 
     // else if (caller.parent().hasClass('fl-main-menu__item--publish')){
     //   //
@@ -213,36 +310,5 @@ function checkEditPageLocation() {
     });
   }
 }
-
-function updatePage() {
-
-  /*--cookie stuff - to be moved into a sepperate function eventually--*/
-  var owner = $('.fl-panel--editor__publish-owner').val();
-  var release = $('.fl-panel--editor__publish-id').val();
-  document.cookie = 'owner=' + owner;
-  document.cookie = 'release=' + release;
-  /*---*/
-
-
-   $.ajax({
-        url: "http://localhost:8081/data",
-        type: "POST",
-        data: JSON.stringify({
-            json: pageData,
-            id: parser.pathname
-        }),
-           xhrFields: {
-               withCredentials: true
-           },
-           dataType: 'json', // Notice! JSONP <-- P (lowercase)
-           crossDomain: true,
-        contentType: "application/json; charset=utf-8"
-    }).done(function () {
-        console.log("Done!")
-    }).fail(function (jqXHR, textStatus) {
-        alert(textStatus);
-    })
-  }
-
 
 })(jQuery);
