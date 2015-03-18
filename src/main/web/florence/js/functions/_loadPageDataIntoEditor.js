c1cf5a9c53b14ede1af3d0549d1c6bf7bfa11180
Kane Morgan <kane.a.s.morgan@gmail.com>
2015-03-18 16:37:31 +0000

    removed unnecessary files

--- src/main/web/florence/js/functions/_loadPageDataIntoEditor.js
+++ src/main/web/florence/js/functions/_loadPageDataIntoEditor.js
@@ -61,6 +61,7 @@ function loadPageDataIntoEditor(){
             name: pageurldata + section.title,
             autoSave: true,
             defaultContent: section.markdown
+
           }
         };
 
