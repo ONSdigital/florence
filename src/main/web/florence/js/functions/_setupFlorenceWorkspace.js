6ca1a9083e8e38ea50acf4a43735b0aac854054d
Kane Morgan <kane.a.s.morgan@gmail.com>
2015-03-19 12:15:46 +0000

    form working to create a new bulletin - still needs to work out what the metadata will be

--- src/main/web/florence/js/functions/_setupFlorenceWorkspace.js
+++ src/main/web/florence/js/functions/_setupFlorenceWorkspace.js
@@ -38,10 +38,13 @@ function setupFlorenceWorkspace(caller){
       '<section class="fl-creator__page_details">' +
         '<span class="fl-creator__title"> Select the Parent</span>' +
         '<input class="fl-creator__parent" name="fl-editor__headline" cols="40" rows="1"></input>' +
-
+        '<br>' +
+        '<span class="fl-creator__title"> enter the new page name </span>' +
+        '<input class="fl-creator__new_name" name="fl-editor__headline" cols="40" rows="1"></input>' +
+        '<br>' +
         '<section class="fl-creator__title"> Select a Page Type' +
         '<select class="fl-creator__page_type_list_select">'+
-          '<option> bulletin </option>' +
+          '<option>bulletin</option>' +
         '</select></section>'+
       '</section>' +
     '</section>' +
