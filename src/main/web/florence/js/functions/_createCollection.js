e161802542d175a188c9ebc1a1e1bc06b76066cb
CarlHembrough <carlhuk@gmail.com>
2015-03-20 13:42:32 +0000

    Setting the publishDate field on create collection.

--- src/main/web/florence/js/functions/_createCollection.js
+++ src/main/web/florence/js/functions/_createCollection.js
@@ -1,24 +1,26 @@
 function createCollection() {
-  var publishDay,publishMonth,publishYear,publishTime,collectionName
-  //zebedee doesnt currently use the date
-  publishDay   = $('.fl-collection-publish-day').val()
-  publishMonth = $('.fl-collection-publish-month').val()
-  publishYear  = $('.fl-collection-publish-year').val()
-  publishTime  = $('.fl-collection-publish-time').val()
-  collectionName = $('.fl-collection-name-input').val()
+  var publishDay,publishMonth,publishYear,publishTime,collectionName;
+  publishDay   = $('.fl-collection-publish-day').val();
+  publishMonth = $('.fl-collection-publish-month').val();
+  publishYear  = $('.fl-collection-publish-year').val();
+  publishTime  = $('.fl-collection-publish-time').val();
+  collectionName = $('.fl-collection-name-input').val();
+
+  var publishDate = new Date(2015, publishMonth, publishDay, 9, 30, 0, 0);
+
     // Create the collection
-    console.log(collectionName)
+    console.log(collectionName + " " + publishDate);
     $.ajax({
         url: "http://localhost:8082/collection",
         dataType: 'json',
         crossDomain: true,
         type: 'POST',
-        data: JSON.stringify({name: collectionName}),
+        data: JSON.stringify({name: collectionName, publishDate: publishDate}),
         success: function () {
           console.log("Collection " + collectionName + " created" );
         },
-        error: function () {
-          console.log('Error creating collection');
+        error: function (jqxhr) {
+          console.log('Error creating collection' + jqxhr.responseText);
         }
     });
 }
