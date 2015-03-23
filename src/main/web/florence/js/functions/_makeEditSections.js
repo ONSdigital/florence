e80e4949e2b0520bc94d9f03e2a1aee8d686e08b
CarlHembrough <carlhuk@gmail.com>
2015-03-23 12:47:54 +0000

    Fixing syntax error - removing extra curly brace.

--- src/main/web/florence/js/functions/_makeEditSections.js
+++ src/main/web/florence/js/functions/_makeEditSections.js
@@ -1,15 +1,15 @@
 function makeEditSections(response){
   if (response.type === 'bulletin'){
       bulletinEditor(response);
+    } else {
+      $('.fl-editor__sections').hide();
+      $("#addSection").remove();
+      $('.fl-editor__headline').show();
+      $('.fl-editor__headline').val(JSON.stringify(response, null, 2));
+      $('.fl-panel--editor__nav__save').click(function() {
+        pageData = $('.fl-editor__headline').val();
+        save("testCollection", pageData);
+      });
     }
-  else {
-    $('.fl-editor__sections').hide();
-    $("#addSection").remove();
-    $('.fl-editor__headline').show();
-    $('.fl-editor__headline').val(JSON.stringify(response, null, 2));
-    $('.fl-panel--editor__nav__save').click(function() {
-      pageData = $('.fl-editor__headline').val();
-      save("testCollection", pageData);
-    });
   }
 }
