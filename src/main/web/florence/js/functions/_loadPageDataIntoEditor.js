c11ea9f4d01d0a9f5eef3c27aa1ebceba73d42dc
CarlHembrough <carlhuk@gmail.com>
2015-03-23 14:00:46 +0000

    Reverted back to reading data from tredegar instead of zebedee directly.

--- src/main/web/florence/js/functions/_loadPageDataIntoEditor.js
+++ src/main/web/florence/js/functions/_loadPageDataIntoEditor.js
@@ -1,11 +1,7 @@
 function loadPageDataIntoEditor(collectionName){
 
   var pageurl = $('.fl-panel--preview__content').contents().get(0).location.href;
-  var dataPath = pageurl.split("#!")[1];
-  if (dataPath.charAt(dataPath.length -1) != "/") dataPath += "/"
-  var dataUrl = "/zebedee/content/" + collectionName + "?uri=" + dataPath + "data.json"
-
-  //console.log("Url to load data from: " + dataUrl);
+  var dataUrl = "/data" + pageurl.split("#!")[1];
 
   $.ajax({
     url: dataUrl,
