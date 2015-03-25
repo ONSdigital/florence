8d9cd09177838a0d94dd0d2ad00777ebf98b6cf7
CarlHembrough <carlhuk@gmail.com>
2015-03-25 09:10:14 +0000

    Adding only required headers back into proxy. Its working on Heroku when not copying all headers.

--- src/main/java/com/github/onsdigital/florence/filter/TredegarProxy.java
+++ src/main/java/com/github/onsdigital/florence/filter/TredegarProxy.java
@@ -3,6 +3,7 @@ package com.github.onsdigital.florence.filter;
 import com.github.davidcarboni.restolino.framework.Filter;
 import com.github.onsdigital.florence.configuration.Configuration;
 import org.apache.commons.io.IOUtils;
+import org.apache.commons.lang3.StringUtils;
 import org.apache.http.Header;
 import org.apache.http.HttpEntity;
 import org.apache.http.client.methods.CloseableHttpResponse;
@@ -74,20 +75,19 @@ public class TredegarProxy implements Filter {
             Enumeration<String> headerNames = request.getHeaderNames();
             String accessToken = "";
 
-//            while (headerNames.hasMoreElements()) {
-//                String headerName = headerNames.nextElement();
-//
-//                if (!headerName.equals("Content-Length") &&
-//                        !headerName.equals("Transfer-Encoding"))
-//                    proxyRequest.addHeader(headerName, request.getHeader(headerName));
-//
-//                if (headerName.equals("access_token"))
-//                    accessToken = request.getHeader("access_token");
-//            }
-//
-//            if (requestBaseUrl == zebedeeBaseUrl && StringUtils.isNotEmpty(accessToken)) {
-//                proxyRequest.addHeader("X-Florence-Token", accessToken);
-//            }
+            while (headerNames.hasMoreElements()) {
+                String headerName = headerNames.nextElement();
+
+                if (headerName.equals("Cookie"))
+                    proxyRequest.addHeader(headerName, request.getHeader(headerName));
+
+                if (headerName.equals("access_token"))
+                    accessToken = request.getHeader("access_token");
+            }
+
+            if (requestBaseUrl == zebedeeBaseUrl && StringUtils.isNotEmpty(accessToken)) {
+                proxyRequest.addHeader("X-Florence-Token", accessToken);
+            }
 
             CloseableHttpResponse proxyResponse = httpClient.execute(proxyRequest);
 
