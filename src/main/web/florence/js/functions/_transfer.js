8d87a373c6a7a0efedc3c68f5af8032fe188a9b6
Kane Morgan <kane.a.s.morgan@gmail.com>
2015-03-26 11:26:07 +0000

    test button added into Florence for Transfer functionality - WIP

---
+++ src/main/web/florence/js/functions/_transfer.js
@@ -0,0 +1,17 @@
+function transfer(source,destination,uri){
+  var transferRequest = {
+    source: source,
+    destination: destination,
+    uri: uri
+  }
+  console.log(transferRequest)
+  $.ajax({
+    url: "/zebedee/transfer",
+    type: "POST",
+    dataType: "json",
+    data: JSON.stringify(transferRequest),
+    headers: {"X-Florence-Token": accessToken()},
+    success: function(response){console.log(' file has been moved')},
+    error: function(response){console.log('error moving file')}
+  })
+}
