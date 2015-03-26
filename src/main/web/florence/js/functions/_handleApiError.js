0223b122bf66aac469aea9b259844bbe9bb1fed3
CarlHembrough <carlhuk@gmail.com>
2015-03-26 11:32:17 +0000

    Added handle api error method for common checks.

---
+++ src/main/web/florence/js/functions/_handleApiError.js
@@ -0,0 +1,13 @@
+function handleApiError(response) {
+
+  if(!response || response.status == 200)
+    return;
+
+  if(response.status == 403) {
+    logout();
+  }
+  else {
+    console.log('Error: ' + response.responseText);
+    alert(response.responseText);
+  }
+}
