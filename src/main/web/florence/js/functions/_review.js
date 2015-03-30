1ac1656ba59a37a734b13c5053c355b92b290767
CarlHembrough <carlhuk@gmail.com>
2015-03-30 12:07:17 +0100

    renamed approve to review.

---
+++ src/main/web/florence/js/functions/_review.js
@@ -0,0 +1,16 @@
+function review(collectionName, path) {
+
+  // Open the file for editing
+  $.ajax({
+    url: "/zebedee/review/" + collectionName + "?uri=" + path + "/data.json",
+    dataType: 'json',
+    type: 'POST',
+    success: function () {
+      console.log("File set to reviewed.");
+      alert("The file is now awaiting approval.");
+    },
+    error: function () {
+      console.log('Error');
+    }
+  });
+}
