23ba0d5632a3a164b4b7027be5791358a8d54839
Kane Morgan <kane.a.s.morgan@gmail.com>
2015-03-30 11:40:33 +0100

    Transfer functionality working, and the form allows you to chose any of the collections to move files to.

---
+++ src/main/web/florence/js/functions/_collectionContent.js
@@ -0,0 +1,9 @@
+function collectionContent(){
+
+  var view_collection =
+  '<h1 class="collection-name"></h1>' +
+  '<h2>In progress</h2>'+
+  '<section class="fl-collection" id="in-progress-uris"></section>'+
+  '<h2>Approved</h2>'+
+  '<section class="fl-collection" id="approved-uris"></section>'
+}
