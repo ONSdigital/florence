6454fb541ec6bd64542cd04cbac0ebe94dfc4dc4
CarlHembrough <carlhuk@gmail.com>
2015-04-07 15:08:01 +0100

    Change create collection button text to save.

--- src/main/web/florence/js/functions/_viewCollections.js
+++ src/main/web/florence/js/functions/_viewCollections.js
@@ -20,7 +20,7 @@ function viewCollections() {
   });
 
   function populateCollectionTable(data) {
-      var page = $('.fl-collections-holder')
+      var page = $('.fl-collections-holder');
       var collection_table =
         '<table class="fl-collections-table">' +
           '<tbody>' +
@@ -37,14 +37,14 @@ function viewCollections() {
         //var date = Date.parse(stringDate);
 
         var date = new Date(collection.publishDate);
-        var minutes = (date.getMinutes()<10?'0':'') + date.getMinutes()
+        var minutes = (date.getMinutes()<10?'0':'') + date.getMinutes();
 
         $('tbody',page).append(
             '<tr class="fl-collections-table-row" data-id="' + collection.id + '">' +
               '<td class= "collection-name">' + collection.name + '</td>' +
               '<td>' + $.datepicker.formatDate('dd/mm/yy', date) + ' ' + date.getHours() + ':' + minutes + '</td>' +
             '</tr>'
-          )
+          );
 
         makeCollectionView(collection.id,data);
 
@@ -52,7 +52,7 @@ function viewCollections() {
 
       page.append(
           '</tbody>' +
-        '</table>')
+        '</table>');
 
 
       $('.fl-collections-table-row').click(function() {
@@ -143,7 +143,7 @@ function viewCollections() {
 	'<select class="fl-collection-publish-time">' +
 		'<option value="0930" selected>09:30</option>' +
 	'</select>' +
-	'<button class="fl-button fl-create-collection--submit-button">Create collection</button>' +
+	'<button class="fl-button fl-create-collection--submit-button">Save</button>' +
 	'<button class="fl-button fl-button--cancel">Cancel</button>' +
 	'</section>';
 
