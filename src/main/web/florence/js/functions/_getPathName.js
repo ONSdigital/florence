acfb29c65937f66b7fb1408a0dbe3926f08dff24
CarlHembrough <carlhuk@gmail.com>
2015-04-07 15:03:26 +0100

    Added case for homepage.

--- src/main/web/florence/js/functions/_getPathName.js
+++ src/main/web/florence/js/functions/_getPathName.js
@@ -1,5 +1,9 @@
 function getPathName() {
   var parsedUrl = $('.fl-panel--preview__content').contents().get(0).location.href.split("#!")[1];
+
+  if (parsedUrl === '/')
+    parsedUrl = '';
+
   return parsedUrl;
 }
 
