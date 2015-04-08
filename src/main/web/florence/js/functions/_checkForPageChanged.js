574e229e0f5975ee6afbbdede22e1ed2c42b1d47
CarlHembrough <carlhuk@gmail.com>
2015-04-08 11:59:11 +0100

    Don't try and read url from iframe if it doesn't exist. Safeguard against the timer still running when navigating away from workspace view.

--- src/main/web/florence/js/functions/_checkForPageChanged.js
+++ src/main/web/florence/js/functions/_checkForPageChanged.js
@@ -1,10 +1,13 @@
 function checkForPageChanged(onChanged) {
 
+  var iframe = $('.fl-panel--preview__content').contents().get(0);
 
-  iframeUrl = localStorage.getItem("pageurl");
-  nowUrl = $('.fl-panel--preview__content').contents().get(0).location.href;
-  if (iframeUrl !== nowUrl) {
-    onChanged();
-    localStorage.setItem("pageurl", nowUrl);
+  if(iframe) {
+    var iframeUrl = localStorage.getItem("pageurl");
+    var nowUrl = $('.fl-panel--preview__content').contents().get(0).location.href;
+    if (iframeUrl !== nowUrl) {
+      onChanged();
+      localStorage.setItem("pageurl", nowUrl);
+    }
   }
 }
