a8dff577f412e9b8e732b6ef7cec96eab9a4ca1c
CarlHembrough <carlhuk@gmail.com>
2015-04-13 11:35:37 +0100

    handling 401 unauthorised code from API when updating content.

--- src/main/web/florence/js/functions/_completeContent.js
+++ src/main/web/florence/js/functions/_completeContent.js
@@ -7,6 +7,9 @@ function saveAndCompleteContent(collectionName, path, content) {
       if (response.status === 400) {
         alert("Cannot edit this file. It is already part of another collection.");
       }
+      else if (response.status == 401) {
+        alert("You are not authorised to update content.");
+      }
       else {
         handleApiError(response);
       }
