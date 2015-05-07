70c1718e48409fdaea65ff16694e5b0fb18f792b
pastor-methods <pastor.methods@gmail.com>
2015-05-07 11:41:01 +0100

    Publish pages
    Former-commit-id: cb1d822a190dec8d35c33ab13bd9febd62490ebc

--- src/main/web/florence/js/functions/_viewPublishDetails.js
+++ src/main/web/florence/js/functions/_viewPublishDetails.js
@@ -1,98 +1,42 @@
 function viewPublishDetails(collections) {
 
-  var date = Florence.collectionToPublish.publishDate;
-  var collectionDetails = [];
-
-        var page_list = [];
-        var page_list1 = [];
-        var pageDataRequests = []; // list of promises - one for each ajax request to load page data.
+  var result = {
+    date: Florence.collectionToPublish.publishDate,
+    collectionDetails: [],
+  }
+  var pageDataRequests = []; // list of promises - one for each ajax request to load page data.
 
   $.each(collections, function (i, collectionId) {
-
-    getCollection(collectionId,
-      success = function (response) {
-//        var page_list = [];
-//        var page_list1 = [];
-//        var pageDataRequests = []; // list of promises - one for each ajax request to load page data.
-
-        function isJsonFile(uri) { return uri.indexOf('data.json', uri.length - 'data.json'.length) !== -1 }
-        response.reviewedUris = response.reviewedUris.filter(function(uri) { return isJsonFile(uri) });
-
-        $.each(response.reviewedUris, function (i, uri) {
-          pageDataRequests.push(getPageData(collectionId, uri,
-            success = function (response) {
-              var path = uri.replace('/data.json', '');
-              path = path.length === 0 ? '/' : path;
-              page_list.push({path: path, name: response.name});
-//              console.log(page_list);
-            },
-            error = function (response) {
-              handleApiError(response);
-            })
-          );
-        });
-
-//        $.when.apply($, pageDataRequests).then(function () {
-//          page_list1.push(page_list);
-//          console.log(page_list1);
-//        });
-
-      },
-      error = function (response) {
-        handleApiError(response);
-      }
+    pageDataRequests.push(
+      getCollectionDetails(collectionId,
+        success = function (response) {
+          function isJsonFile(uri) { return uri.indexOf('data.json', uri.length - 'data.json'.length) !== -1 }
+          console.log(response);
+          response.reviewed = response.reviewed.filter(function(page) { return isJsonFile(page.uri) });
+          result.collectionDetails.push({id: response.id, name: response.name, pageDetails: response.reviewed});
+        },
+        error = function (response) {
+          handleApiError(response);
+        }
+      )
     );
   });
-
   $.when.apply($, pageDataRequests).then(function () {
-    page_list1.push(page_list);
-//    console.log(page_list1);
+    var publishDetails = templates.publishDetails(result);
+//    console.log(publishDetails);
+    $('.publish-selected').html(publishDetails);
+    console.log(JSON.stringify(result));
   });
 
-  var result = {date: date, };
+  $('.collections-accordion').accordion({
+    header: '.collections-section__head',
+    active: false,
+    collapsible: true
+  });
 
-  var publishDetails = templates.publishDetails(result);
-      $('.section').html(publishDetails);
-
-
-
-//  $('.collection-name').click(function () {
-//    var collectionId = $(this).attr('data-id');
-//
-//
-//    getCollection(collectionId,
-//      success = function (response) {
-//        var page_list = '';
-//        var pageDataRequests = []; // list of promises - one for each ajax request to load page data.
-//
-//        function isJsonFile(uri) { return uri.indexOf('data.json', uri.length - 'data.json'.length) !== -1 }
-//        response.reviewedUris = response.reviewedUris.filter(function(uri) { return isJsonFile(uri) });
-//
-//        $.each(response.reviewedUris, function (i, uri) {
-//          pageDataRequests.push(getPageData(collectionId, uri,
-//            success = function (response) {
-//              var path = uri.replace('/data.json', '');
-//              path = path.length === 0 ? '/' : path;
-//              page_list += '<p class="fl-review-page-list-item" data-path="' + path + '">' +
-//              response.name + '</p>';
-//
-//              console.log(response.name);
-//            },
-//            error = function (response) {
-//              handleApiError(response);
-//            }));
-//        });
-//
-//        $.when.apply($, pageDataRequests).then(function () {
-//          page_list += '</ul>';
-//          $('.fl-panel--publish-collection-' + collectionId).html(page_list);
-//          //updateReviewScreenWithCollection(response);
-//
-//        });
-//
-//      },
-//      error = function (response) {
-//        handleApiError(response);
-//      });
-//  });
+  $('.publish-selected .btn-cancel').click(function(){
+    $('.publish-selected').animate({right: "-50%"}, 500);
+    $('.publish-select').animate({marginLeft: "25%"}, 800);
+    $('.publish-select-table tbody tr').removeClass('selected');
+  });
 }
