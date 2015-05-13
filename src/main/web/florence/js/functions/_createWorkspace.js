60cb4987eee108a8eb1ac33bd45ccfee572da227
pastor-methods <pastor.methods@gmail.com>
2015-05-13 10:48:57 +0100

    Solve breadcrumb bug

--- src/main/web/florence/js/functions/_createWorkspace.js
+++ src/main/web/florence/js/functions/_createWorkspace.js
@@ -13,6 +13,41 @@ function createWorkspace(path, collectionName, menu) {
   var workSpace = templates.workSpace(Florence.tredegarBaseUrl + path);
   $('.section').html(workSpace);
 
+  //click handlers
+    $('.nav--workspace > li').click(function () {
+      menu = '';
+      if (Florence.Editor.isDirty) {
+        var result = confirm("You have unsaved changes. Are you sure you want to continue");
+        if (result === true) {
+          Florence.Editor.isDirty = false;
+          processMenuClick(this);
+        } else {
+          return false;
+        }
+      } else {
+        processMenuClick(this);
+      }
+
+    });
+
+    function processMenuClick(clicked) {
+
+      var menuItem = $(clicked);
+
+      $('.nav--workspace li').removeClass('selected');
+      menuItem.addClass('selected');
+
+      if (menuItem.is('#browse')) {
+        loadBrowseScreen('click');
+      } else if (menuItem.is('#create')) {
+        loadCreateScreen(collectionName);
+      } else if (menuItem.is('#edit')) {
+        loadPageDataIntoEditor(getPathName(document.getElementById('iframe').contentWindow.location.href), Florence.collection.id);
+      } else {
+        loadBrowseScreen();
+      }
+    }
+
   document.getElementById('iframe').onload = function () {
     var browserLocation = document.getElementById('iframe').contentWindow.location.href;
     $('.browser-location').val(browserLocation);
--- src/main/web/florence/js/functions/_viewWorkspace.js
+++ src/main/web/florence/js/functions/_viewWorkspace.js
@@ -24,39 +24,6 @@ function viewWorkspace(path, collectionName, menu) {
     loadPageDataIntoEditor(path, collectionName);
   }
 
-  //click handlers
-  $('.nav--workspace > li').click(function () {
-    menu = '';
-    if (Florence.Editor.isDirty) {
-      var result = confirm("You have unsaved changes. Are you sure you want to continue");
-      if (result === true) {
-        Florence.Editor.isDirty = false;
-        processMenuClick(this);
-      } else {
-        return false;
-      }
-    } else {
-      processMenuClick(this);
-    }
 
-  });
-
-  function processMenuClick(clicked) {
-
-    var menuItem = $(clicked);
-
-    $('.nav--workspace li').removeClass('selected');
-    menuItem.addClass('selected');
-
-    if (menuItem.is('#browse')) {
-      loadBrowseScreen('click');
-    } else if (menuItem.is('#create')) {
-      loadCreateScreen(collectionName);
-    } else if (menuItem.is('#edit')) {
-      loadPageDataIntoEditor(getPathName(document.getElementById('iframe').contentWindow.location.href), Florence.collection.id);
-    } else {
-      loadBrowseScreen();
-    }
-  }
 }
 
