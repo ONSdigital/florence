64a436fcf781dd180681e0a81a79f511cec8b833
carlhuk <carlhuk@gmail.com>
2015-05-27 10:22:00 +0100

    Reverted last change to simplify rebase.
    Former-commit-id: 260c523e38853fa1a2b7818f0b66a938af74ba30

--- src/main/web/florence/js/functions/_loadT7Creator.js
+++ src/main/web/florence/js/functions/_loadT7Creator.js
@@ -88,88 +88,87 @@ function loadT7Creator (collectionName, pageType) {
         );
       }
   });
+  }
+}
 
-    function pageTypeData(pageType) {
+function pageTypeData(pageType) {
 
-      if (pageType === "static") {
-        return {
-          "summary": "",
-          "keywords": [],
-          "metaDescription": "",
+  if (pageType === "static") {
+    return {
+      "summary": "",
+      "keywords": [],
+      "metaDescription": "",
+      "name": "",
+      "releaseDate": "",
+      "content": "",
+      type: pageType,
+      "uri": "",
+      "fileName": "",
+      "breadcrumb": "",
+    };
+  }
+
+  else if (pageType === "qmi") {
+      return {
+        "contact": {
           "name": "",
-          "releaseDate": "",
-          "content": "",
-          type: pageType,
-          "uri": "",
-          "fileName": "",
-          "breadcrumb": "",
-        };
-      }
-
-      else if (pageType === "qmi") {
-        return {
-          "contact": {
-            "name": "",
-            "email": "",
-            "phone": ""
-          },
-          "surveyName": "",
-          "frequency": "",
-          "compilation": "",
-          "geoCoverage": [],
-          "sampleSize": "",
-          "lastRevised": "",
-          "content": "",
-          "summary": "",
-          "keywords": [],
-          "metaDescription": "",
-          "name": "",
-          "download": [],
-          type: pageType,
-          "uri": "",
-          "fileName": "",
-          "breadcrumb": "",
-        };
-      }
-
-      else if (pageType === "foi") {
-        return {
-          "download": [],
-          "content": "",
-          "summary": "",
-          "keywords": [],
-          "metaDescription": "",
-          "name": "",
-          "releaseDate": "",
-          type: pageType,
-          "uri": "",
-          "fileName": "",
-          "breadcrumb": "",
-        };
-      }
-
-      else if (pageType === "adHoc") {
-        return {
-          "download": [],
-          "content": "",
-          "summary": "",
-          "keywords": [],
-          "metaDescription": "",
-          "name": "",
-          "releaseDate": "",
-          "reference": "",
-          type: pageType,
-          "uri": "",
-          "fileName": "",
-          "breadcrumb": "",
-        };
-      }
-
-      else {
-        alert('unsupported page type');
-      }
+          "email": "",
+          "phone": ""
+        },
+        "surveyName": "",
+        "frequency": "",
+        "compilation": "",
+        "geoCoverage": [],
+        "sampleSize": "",
+        "lastRevised": "",
+        "content": "",
+        "summary": "",
+        "keywords": [],
+        "metaDescription": "",
+        "name": "",
+        "download": [],
+        type: pageType,
+        "uri": "",
+        "fileName": "",
+        "breadcrumb": "",
+      };
     }
 
+  else if (pageType === "foi") {
+    return {
+      "download": [],
+      "content": "",
+      "summary": "",
+      "keywords": [],
+      "metaDescription": "",
+      "name": "",
+      "releaseDate": "",
+      type: pageType,
+      "uri": "",
+      "fileName": "",
+      "breadcrumb": "",
+    };
+  }
+
+  else if (pageType === "adHoc") {
+    return {
+      "download": [],
+      "content": "",
+      "summary": "",
+      "keywords": [],
+      "metaDescription": "",
+      "name": "",
+      "releaseDate": "",
+      "reference": "",
+      type: pageType,
+      "uri": "",
+      "fileName": "",
+      "breadcrumb": "",
+    };
+  }
+
+  else {
+    alert('unsupported page type');
   }
 }
 
