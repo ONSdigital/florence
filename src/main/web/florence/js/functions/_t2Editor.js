bfba4d5f3b8f0d507764628e3b4fa64811b9bfa7
pastor-methods <pastor.methods@gmail.com>
2015-05-28 10:16:05 +0100

    add t2 editor
    Former-commit-id: 165bd01ad5eff1b54b2a33ad7b3523d3e3b9d360

---
+++ src/main/web/florence/js/functions/_t2Editor.js
@@ -0,0 +1,52 @@
+function t2Editor(collectionId, data) {
+
+  var setActiveTab, getActiveTab;
+
+  $(".edit-accordion").on('accordionactivate', function(event, ui) {
+    setActiveTab = $(".edit-accordion").accordion("option", "active");
+    if (setActiveTab !== false) {
+      localStorage.setItem('activeTab', setActiveTab);
+    }
+  });
+
+  getActiveTab = localStorage.getItem('activeTab');
+  accordion(getActiveTab);
+
+  // Metadata load, edition and saving
+  $("#name").on('click keyup', function () {
+    $(this).textareaAutoSize();
+    data.name = $(this).val();
+  });
+  $("#summary").on('click keyup', function () {
+    $(this).textareaAutoSize();
+    data.lede = $(this).val();
+  });
+  $("#keywords").on('click keyup', function () {
+    $(this).textareaAutoSize();
+    data.keywords = $(this).val();
+  });
+  $("#metaDescription").on('click keyup', function () {
+    $(this).textareaAutoSize();
+    data.metaDescription = $(this).val();
+  });
+
+  // Save
+  var editNav = $('.edit-nav');
+  editNav.off(); // remove any existing event handlers.
+
+  editNav.on('click', '.btn-edit-save', function () {
+    updateContent(collectionId, getPathName(), JSON.stringify(data));
+  });
+
+  // completed to review
+  editNav.on('click', '.btn-edit-save-and-submit-for-review', function () {
+    //pageData = $('.fl-editor__headline').val();
+    saveAndCompleteContent(collectionId, getPathName(), JSON.stringify(data));
+  });
+
+  // reviewed to approve
+  editNav.on('click', '.btn-edit-save-and-submit-for-approval', function () {
+    saveAndReviewContent(collectionId, getPathName(), JSON.stringify(data));
+  });
+}
+
