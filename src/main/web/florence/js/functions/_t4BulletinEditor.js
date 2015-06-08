153a5afc12e28a4a1319fdf545cc38cb065c122d
pastor-methods <pastor.methods@gmail.com>
2015-06-08 16:16:25 +0100

    Add release version
    Former-commit-id: 55752d54032f05d70d0f763d065fea608c08c44a

--- src/main/web/florence/js/functions/_t4BulletinEditor.js
+++ src/main/web/florence/js/functions/_t4BulletinEditor.js
@@ -1,5 +1,6 @@
 function bulletinEditor(collectionId, data) {
 
+  var index = data.release.value;
   var newSections = [], newTabs = [], newRelated = [], newLinks = [];
   var lastIndexRelated;
   var setActiveTab, getActiveTab;
@@ -34,10 +35,14 @@ function bulletinEditor(collectionId, data) {
     $(this).textareaAutoSize();
     data.name = $(this).val();
   });
+  $('#release').datepicker({dateFormat: "MM yy", altFormat: "yymm", altField: "#alt-release"});
   $("#release").on('click keyup', function () {
 //    $(this).textareaAutoSize();
-    data.release = $(this).val();
+    data.release.label = $(this).val();
   });
+  $("#alt-release").on('change', function () {
+      data.release.value = $(this).val();
+    });
   $("#releaseDate").on('click keyup', function () {
     $(this).textareaAutoSize();
     data.releaseDate = $(this).val();
@@ -290,7 +295,7 @@ function bulletinEditor(collectionId, data) {
 
   editNav.on('click', '.btn-edit-save', function () {
     save();
-    updateContent(collectionId, getPathName(), JSON.stringify(data));
+    updateVersionedContent(collectionId, getPathName(), JSON.stringify(data), index);
   });
 
   // completed to review
@@ -344,5 +349,58 @@ function bulletinEditor(collectionId, data) {
   }
 
   loadChartsList(data, collectionId);
+}
+
+// Source: http://stackoverflow.com/questions/497790
+var dates = {
+    convert:function(d) {
+        // Converts the date in d to a date-object. The input can be:
+        //   a date object: returned without modification
+        //  an array      : Interpreted as [year,month,day]. NOTE: month is 0-11.
+        //   a number     : Interpreted as number of milliseconds
+        //                  since 1 Jan 1970 (a timestamp)
+        //   a string     : Any format supported by the javascript engine, like
+        //                  "YYYY/MM/DD", "MM/DD/YYYY", "Jan 31 2009" etc.
+        //  an object     : Interpreted as an object with year, month and date
+        //                  attributes.  **NOTE** month is 0-11.
+        return (
+            d.constructor === Date ? d :
+            d.constructor === Array ? new Date(d[0],d[1],d[2]) :
+            d.constructor === Number ? new Date(d) :
+            d.constructor === String ? new Date(d) :
+            typeof d === "object" ? new Date(d.year,d.month,d.date) :
+            NaN
+        );
+    },
+    compare:function(a,b) {
+        // Compare two dates (could be of any type supported by the convert
+        // function above) and returns:
+        //  -1 : if a < b
+        //   0 : if a = b
+        //   1 : if a > b
+        // NaN : if a or b is an illegal date
+        // NOTE: The code inside isFinite does an assignment (=).
+        return (
+            isFinite(a=this.convert(a).valueOf()) &&
+            isFinite(b=this.convert(b).valueOf()) ?
+            (a>b)-(a<b) :
+            NaN
+        );
+    },
+    inRange:function(d,start,end) {
+        // Checks if date in d is between dates in start and end.
+        // Returns a boolean or NaN:
+        //    true  : if d is between start and end (inclusive)
+        //    false : if d is before start or after end
+        //    NaN   : if one or more of the dates is illegal.
+        // NOTE: The code inside isFinite does an assignment (=).
+       return (
+            isFinite(d=this.convert(d).valueOf()) &&
+            isFinite(start=this.convert(start).valueOf()) &&
+            isFinite(end=this.convert(end).valueOf()) ?
+            start <= d && d <= end :
+            NaN
+        );
+    }
 }
 
