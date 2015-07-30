855053e656161c5f94ad029eba48519b826334e2
pastor-methods <pastor.methods@gmail.com>
2015-07-30 17:52:47 +0100

    Check path for safe uri
    Former-commit-id: f359bd81b4764b394025af1322575fc3b2ea3837

--- src/main/web/florence/js/functions/_checkPathParsed.js
+++ src/main/web/florence/js/functions/_checkPathParsed.js
@@ -2,11 +2,10 @@ function checkPathParsed (uri) {
   if (uri.charAt(0) !== '/') {
     uri = '/' + uri;
   }
-  var safeUrl;
   var myUrl = parseURL(uri);
-  if (myUrl.pathname.charAt(myUrl.pathname.length-1) === '/') {
-    myUrl.pathname = myUrl.pathname.slice(0, -1);
-    safeUrl = myUrl.pathname;
+  var safeUrl = myUrl.pathname;
+  if (safeUrl.charAt(safeUrl.length-1) === '/') {
+    safeUrl = safeUrl.slice(0, -1);
   }
   return safeUrl;
 }
