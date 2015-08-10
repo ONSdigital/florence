8b3c9c480c84d8721becaa7dd338ea0bf1f7b20f
pastor-methods <pastor.methods@gmail.com>
2015-08-10 12:59:54 +0100

    Solve bug when creating new pages
    Former-commit-id: 7477c9a0620972fdc07e769db076c85b7878c740

--- src/main/web/florence/js/functions/_loadCreator.js
+++ src/main/web/florence/js/functions/_loadCreator.js
@@ -20,21 +20,26 @@ function loadCreator (collectionId) {
     var parentUrl = Florence.globalVars.pagePath;
 
     if (pageType === 'bulletin' || pageType === 'article') {
+      $('.edition').empty();
       loadT4Creator(collectionId, releaseDate, pageType, parentUrl);
     }
 
     else if (pageType.match(/compendium_.+/)) {
+      $('.edition').empty();
       loadT6Creator(collectionId, releaseDate, pageType, parentUrl);
     }
 
     else if (pageType.match(/static_.+/)) {
+      $('.edition').empty();
       loadT7Creator(collectionId, releaseDate, pageType, parentUrl);
     }
 
     else if (pageType === 'reference_tables' || pageType === 'dataset') {
+      $('.edition').empty();
       loadT8Creator(collectionId, releaseDate, pageType, parentUrl);
     }
     else if (pageType === 'release') {
+      $('.edition').empty();
       loadT16Creator(collectionId, releaseDate, pageType, parentUrl);
     }
   });
