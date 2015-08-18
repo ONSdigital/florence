eae3906dac4ba2096845c9377d16775fa9cf5f2c
pastor-methods <pastor.methods@gmail.com>
2015-08-18 17:47:21 +0100

    Allow saving in the same place for Welsh
    Former-commit-id: 54df382c9663bddd349405241b408a001ce4eaab

--- src/main/web/florence/js/functions/_checkSaveContent.js
+++ src/main/web/florence/js/functions/_checkSaveContent.js
@@ -1,30 +1,38 @@
 function checkSaveContent(collectionId, uri, data) {
-  // check if the page exists
-  var getUri = uri + '/data.json';
-  getPageData(collectionId, getUri,
-    success = function() {
-      alert('This page already exists');
-    },
-    // if the page does not exist, create it
-    error = function() {
-      postContent(collectionId, uri, JSON.stringify(data),
-        success = function (message) {
-          console.log("Updating completed " + message);
-          viewWorkspace(uri, collectionId, 'edit');
-          refreshPreview(uri);
-        },
-        error = function (response) {
-          if (response.status === 400) {
-            alert("Cannot edit this file. It is already part of another collection.");
-          }
-          else if (response.status === 401) {
-            alert("You are not authorised to update content.");
-          }
-          else {
-            handleApiError(response);
-          }
+  if (!Florence.globalVars.welsh) {
+    // check if the page exists
+    var getUri = uri + '/data.json';
+    getPageData(collectionId, getUri,
+      success = function() {
+        alert('This page already exists');
+      },
+      // if the page does not exist, create it
+      error = function() {
+        save(collectionId, uri, data);
+      }
+    );
+  } else {
+    save(collectionId, uri, data);
+  }
+
+  function save (collectionId, uri, data) {
+    postContent(collectionId, uri, JSON.stringify(data),
+      success = function (message) {
+        console.log("Updating completed " + message);
+        viewWorkspace(uri, collectionId, 'edit');
+        refreshPreview(uri);
+      },
+      error = function (response) {
+        if (response.status === 400) {
+          alert("Cannot edit this file. It is already part of another collection.");
         }
-      );
-    }
-  );
+        else if (response.status === 401) {
+          alert("You are not authorised to update content.");
+        }
+        else {
+          handleApiError(response);
+        }
+      }
+    );
+  }
 }
