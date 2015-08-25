2548d3160805ed42cb713d2a51de85121957aa4c
pastor-methods <pastor.methods@gmail.com>
2015-08-25 15:40:40 +0100

    Solve conflict result
    Former-commit-id: 56adc68b1680b9800dde49b4ca29b5cc4fe17f91

--- src/main/web/florence/js/functions/_createWorkspace.js
+++ src/main/web/florence/js/functions/_createWorkspace.js
@@ -1,13 +1,14 @@
 function createWorkspace(path, collectionId, menu, stopEventListener) {
   var safePath = '';
-  $("#working-on").on('click', function () {}); // add event listener to mainNav
+  $("#working-on").on('click', function () {
+  }); // add event listener to mainNav
 
-  if(stopEventListener) {
+  if (stopEventListener) {
     document.getElementById('iframe').onload = function () {
       var browserLocation = document.getElementById('iframe').contentWindow.location.href;
       $('.browser-location').val(browserLocation);
       var iframeEvent = document.getElementById('iframe').contentWindow;
-          iframeEvent.removeEventListener('click', Florence.Handler, true);
+      iframeEvent.removeEventListener('click', Florence.Handler, true);
     };
     return false;
   } else {
@@ -26,7 +27,7 @@ function createWorkspace(path, collectionId, menu, stopEventListener) {
     Florence.refreshAdminMenu();
 
     var workSpace = templates.workSpace(Florence.tredegarBaseUrl + safePath);
-     $('.section').html(workSpace);
+    $('.section').html(workSpace);
 
     document.getElementById('iframe').onload = function () {
       $('.browser-location').val(Florence.tredegarBaseUrl + Florence.globalVars.pagePath);
@@ -58,54 +59,53 @@ function createWorkspace(path, collectionId, menu, stopEventListener) {
       function processMenuClick(clicked) {
         var menuItem = $(clicked);
 
-      $('.nav--workspace li').removeClass('selected');
-      menuItem.addClass('selected');  
-        
-      if (menuItem.is('#browse')) {
-        loadBrowseScreen(collectionId, 'click');
-      } else if (menuItem.is('#create')) {
-        loadCreateScreen(Florence.globalVars.pagePath, collectionId);
-      } else if (menuItem.is('#edit')) {
-        Florence.globalVars.pagePath = getPathName();
-        loadPageDataIntoEditor(Florence.globalVars.pagePath, Florence.collection.id);
-      } else {
-        loadBrowseScreen(collectionId);
+        $('.nav--workspace li').removeClass('selected');
+        menuItem.addClass('selected');
+
+        if (menuItem.is('#browse')) {
+          loadBrowseScreen(collectionId, 'click');
+        } else if (menuItem.is('#create')) {
+          loadCreateScreen(Florence.globalVars.pagePath, collectionId);
+        } else if (menuItem.is('#edit')) {
+          Florence.globalVars.pagePath = getPathName();
+          loadPageDataIntoEditor(Florence.globalVars.pagePath, Florence.collection.id);
+        } else {
+          loadBrowseScreen(collectionId);
+        }
       }
-    }
 
-    $('#nav--workspace__welsh').on('click', function () {
-      Florence.globalVars.welsh = Florence.globalVars.welsh === false ? true : false;
-      createWorkspace(Florence.globalVars.pagePath, collectionId);
-    });  
+      $('#nav--workspace__welsh').on('click', function () {
+        Florence.globalVars.welsh = Florence.globalVars.welsh === false ? true : false;
+        createWorkspace(Florence.globalVars.pagePath, collectionId);
+      });
 
-    $('.workspace-menu').on('click', '.btn-browse-create', function () {
-      var dest = $('.tree-nav-holder ul').find('.selected').attr('data-url');
-      Florence.globalVars.pagePath = dest;
-      $('.nav--workspace li').removeClass('selected');
-      $("#create").addClass('selected');
-      loadCreateScreen(Florence.globalVars.pagePath, collectionId);
-    });
+      $('.workspace-menu').on('click', '.btn-browse-create', function () {
+        var dest = $('.tree-nav-holder ul').find('.selected').attr('data-url');
+        Florence.globalVars.pagePath = dest;
+        $('.nav--workspace li').removeClass('selected');
+        $("#create").addClass('selected');
+        loadCreateScreen(Florence.globalVars.pagePath, collectionId);
+      });
 
-    $('.workspace-menu').on('click', '.btn-browse-edit', function () {
-      var dest = $('.tree-nav-holder ul').find('.selected').attr('data-url');
-      Florence.globalVars.pagePath = dest;
-      $('.nav--workspace li').removeClass('selected');
-      $("#edit").addClass('selected');
-      loadPageDataIntoEditor(Florence.globalVars.pagePath, collectionId);
-    });
+      $('.workspace-menu').on('click', '.btn-browse-edit', function () {
+        var dest = $('.tree-nav-holder ul').find('.selected').attr('data-url');
+        Florence.globalVars.pagePath = dest;
+        $('.nav--workspace li').removeClass('selected');
+        $("#edit").addClass('selected');
+        loadPageDataIntoEditor(Florence.globalVars.pagePath, collectionId);
+      });
 
-    if (menu === 'edit') {
-      $('.nav--workspace li').removeClass('selected');
-      $("#edit").addClass('selected');
-      loadPageDataIntoEditor(Florence.globalVars.pagePath, collectionId);
-    } else {
-      //browse screen by default
-      $('.nav--workspace li').removeClass('selected');
-      $("#browse").addClass('selected');
-      loadBrowseScreen(collectionId, 'click');
-    }
+      if (menu === 'edit') {
+        $('.nav--workspace li').removeClass('selected');
+        $("#edit").addClass('selected');
+        loadPageDataIntoEditor(Florence.globalVars.pagePath, collectionId);
+      } else {
+        //browse screen by default
+        $('.nav--workspace li').removeClass('selected');
+        $("#browse").addClass('selected');
+        loadBrowseScreen(collectionId, 'click');
+      }
+    };
   }
 }
-
-
 
