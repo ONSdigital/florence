4aa558a8de8708a45da8d3815d0dbdcabbdf6486
pastor-methods <pastor.methods@gmail.com>
2015-09-09 15:43:38 +0100

    Add timeseries_dataset type in Florence

--- src/main/web/florence/js/functions/_loadCreator.js
+++ src/main/web/florence/js/functions/_loadCreator.js
@@ -36,7 +36,7 @@ function loadCreator (parentUrl, collectionId, type) {
       else if (pageType.match(/static_.+/)) {
         loadT7Creator(collectionId, releaseDate, pageType, parentUrl);
       }
-      else if (pageType === 'reference_tables' || pageType === 'dataset') {
+      else if (pageType === 'reference_tables' || pageType === 'dataset' || pageType === 'timeseries_dataset') {
         loadT8Creator(collectionId, releaseDate, pageType, parentUrl);
       }
       else if (pageType === 'release') {
