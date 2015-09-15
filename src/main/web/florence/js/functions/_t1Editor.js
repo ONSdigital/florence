e97ea173735c6885baa8f4e46e947e4858ecee74
pastor-methods <pastor.methods@gmail.com>
2015-09-15 11:49:12 +0100

    Change error behaviour

--- src/main/web/florence/js/functions/_t1Editor.js
+++ src/main/web/florence/js/functions/_t1Editor.js
@@ -157,9 +157,6 @@ function resolveTitleT1(collectionId, data, templateData, field) {
                       if (response.status === 400) {
                         alert("Cannot edit this page. It is already part of another collection.");
                       }
-                      else if (response.status === 401) {
-                        alert("You are not authorised to update content.");
-                      }
                       else {
                         handleApiError(response);
                       }
--- src/main/web/florence/js/functions/_t4ArticleEditor.js
+++ src/main/web/florence/js/functions/_t4ArticleEditor.js
@@ -149,30 +149,6 @@ function articleEditor(collectionId, data) {
     saveAndReviewContent(collectionId, data.uri, JSON.stringify(data));
   });
 
-
-  //function autoSaveMetadata () {
-  //  clearTimeout(timeoutId, collectionId, data);
-  //  timeoutId = setTimeout(function() {
-  //    // Runs 5 second (5000 ms) after the last change
-  //    postContent(collectionId, data.uri, JSON.stringify(data),
-  //      success = function () {
-  //        Florence.Editor.isDirty = false;
-  //      },
-  //      error = function (response) {
-  //        if (response.status === 400) {
-  //          alert("Cannot edit this page. It is already part of another collection.");
-  //        }
-  //        else if (response.status === 401) {
-  //          alert("You are not authorised to update content.");
-  //        }
-  //        else {
-  //          handleApiError(response);
-  //        }
-  //      }
-  //    );
-  //  }, 5000);
-  //}
-
   function save() {
     // Sections
     var orderSection = $("#sortable-section").sortable('toArray');
