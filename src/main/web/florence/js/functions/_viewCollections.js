ae5ddbd6fc622b42e7d82a8e16b651cfba1dedfe
CarlHembrough <carlhuk@gmail.com>
2015-10-06 15:38:52 +0100

    Remove console logs.

--- src/main/web/florence/js/functions/_viewCollections.js
+++ src/main/web/florence/js/functions/_viewCollections.js
@@ -96,8 +96,6 @@ function viewCollections(collectionId) {
         url: baseReleaseUri + '&page=' + i,
         type: "get",
         success: function (data) {
-          console.log('populating releases for page ' + i);
-          console.log(data);
           _(data.results).each(function (release) {
             releases.push(release);
           });
@@ -121,9 +119,6 @@ function viewCollections(collectionId) {
             releases.push(release);
           });
 
-          console.log('populating releases!');
-          console.log(data);
-
           // if there are more results than the existing page size, go get them.
           if (data.numberOfResults > pageSize) {
 
@@ -131,13 +126,11 @@ function viewCollections(collectionId) {
             var pageDataRequests = []; // list of promises - one for each ajax request to load page data.
 
             for (var i = 2; i < pagesToGet + 2; i++) {
-              console.log("Calling release list for page " + baseReleaseUri + '&page=' + i);
               var dfd = getReleasesPage(i, releases);
               pageDataRequests.push(dfd);
             }
 
             $.when.apply($, pageDataRequests).then(function () {
-              console.log('Finished getting all release pages');
               populateReleasesDropdown(releases);
             });
           }
@@ -149,12 +142,8 @@ function viewCollections(collectionId) {
     );
 
     function populateReleasesDropdown(releases) {
-
       var releaseSelect = $('#collection-release');
-
       _(_.sortBy(releases, 'uri')).each(function (release) {
-        //console.log('release: ' + release.uri);
-        //console.log(release);
         releaseSelect.append(new Option(release.description.title, release.uri));
       });
     }
