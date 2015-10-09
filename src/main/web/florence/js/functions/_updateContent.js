1c8123f5a9fbe956d2302b997876ff2414f4381f
CarlHembrough <carlhuk@gmail.com>
2015-10-09 14:24:54 +0100

    Fixed response code check when a page is being edited as part of another collection.

--- src/main/web/florence/js/functions/_updateContent.js
+++ src/main/web/florence/js/functions/_updateContent.js
@@ -13,7 +13,7 @@ function updateContent(collectionId, path, content, redirectToPath) {
       }
     },
     error = function (response) {
-      if (response.status === 400) {
+      if (response.status === 409) {
         alert("Cannot edit this page. It is already part of another collection.");
       } else {
         handleApiError(response);
