9e459974bc0532ab28b1cc405e8c7a21c49c4d5f
pastor-methods <pastor.methods@gmail.com>
2015-10-13 17:42:01 +0100

    Changes for uploading images
    Former-commit-id: fd4e59e6fb87143021ec426f990bc4868e59a8d6

--- src/main/web/florence/js/functions/_loadImagesList.js
+++ src/main/web/florence/js/functions/_loadImagesList.js
@@ -14,56 +14,52 @@ function initialiseImagesList(data, collectionId) {
 
   $(data.images).each(function (index, image) {
     var basePath = data.uri;
-    var imagePath = basePath + '/' + image.filename;
-    var imageJson = imagePath;
+    var noExtension = image.uri.match(/^(.+?)(\.[^.]*$|$)/);
+    var imageJson = noExtension[1] + '.json';
 
-    $("#image-edit_" + image.filename).click(function () {
-      getPageData(collectionId, imageJson,
+    $("#image-edit_" + index).click(function () {
+      getPageResource(collectionId, imageJson,
         onSuccess = function (imageData) {
           loadImageBuilder(data, function () {
             Florence.Editor.isDirty = false;
-            refreshPreview();
+            //refreshPreview();
             refreshImagesList(data, collectionId);
           }, imageData);
         })
     });
 
-    $("#image-delete_" + image.filename).click(function () {
+    $("#image-delete_" + index).click(function () {
       var result = confirm("Are you sure you want to delete this image?");
       if (result === true) {
         $("#image_" + index).remove();
         // delete any files associated with the image.
-        var extraFiles = [image.filename + '.jpg']; //restrict this to one file type jpg
-        _(extraFiles).each(function (file) {
-          var fileToDelete = basePath + '/' + file;
-          deleteContent(collectionId, fileToDelete,
-            onSuccess = function () {
-            },
-            onError = function (error) {
-              handleApiError(error);
+        deleteContent(collectionId, image.uri,
+          onSuccess = function () {
+          },
+          onError = function (error) {
+            handleApiError(error);
+          });
+
+        // delete the image json file
+        deleteContent(collectionId, imageJson,
+          onSuccess = function () {
+            // remove the image from the page json when its deleted
+            data.images = _(data.images).filter(function (item) {
+              return item.filename !== image.filename;
             });
 
-          // delete the image json file
-          deleteContent(collectionId, imageJson + '.json',
-            onSuccess = function () {
-              // remove the image from the page json when its deleted
-              data.images = _(data.images).filter(function (item) {
-                return item.filename !== image.filename;
-              });
-
-              // save the updated page json
-              postContent(collectionId, basePath, JSON.stringify(data),
-                success = function () {
-                  Florence.Editor.isDirty = false;
-                  refreshImagesList(data, collectionId);
-                },
-                error = function (response) {
-                  handleApiError(response);
-                }
-              );
-            }
-          );
-        });
+            // save the updated page json
+            postContent(collectionId, basePath, JSON.stringify(data),
+              success = function () {
+                Florence.Editor.isDirty = false;
+                refreshImagesList(data, collectionId);
+              },
+              error = function (response) {
+                handleApiError(response);
+              }
+            );
+          }
+        );
       }
     });
   });
