0ceaf85d7f160841e2d0216ae9ee78cc6e08432d
pastor-methods <pastor.methods@gmail.com>
2015-10-21 09:48:15 +0100

    Add corrections to bulletins

--- src/main/web/florence/js/functions/_makeEditSections.js
+++ src/main/web/florence/js/functions/_makeEditSections.js
@@ -57,6 +57,7 @@ function makeEditSections(collectionId, pageData, isPageComplete) {
     editRelated (collectionId, pageData, templateData, 'relatedMethodology', 'methodology');
     editTopics (collectionId, pageData, templateData, 'topics', 'topics');
     editLink (collectionId, pageData, 'links', 'link');
+    editCorrection(collectionId, data, templateData, 'corrections', 'correction');
     accordion();
     bulletinEditor(collectionId, pageData);
   }
