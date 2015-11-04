f849da3f79944a3bcf33a9e8a9f2e0c6354f8742
pastor-methods <pastor.methods@gmail.com>
2015-11-04 17:27:34 +0000

    Add reserved words
    Former-commit-id: 81cab940a08afead2f9bc135c226699dc8e91bf8

---
+++ src/main/web/florence/js/functions/_t4StaticArticleEditor.js
@@ -0,0 +1,223 @@
+function staticArticleEditor(collectionId, data) {
+
+//  var index = data.release;
+  var newSections = [], newTabs = [], newFiles = [], newDatasets = [], newLinks = [], newRelatedMethodology = [], newDocuments = [];
+  var setActiveTab, getActiveTab;
+  var timeoutId;
+
+  $(".edit-accordion").on('accordionactivate', function (event, ui) {
+    setActiveTab = $(".edit-accordion").accordion("option", "active");
+    if (setActiveTab !== false) {
+      Florence.globalVars.activeTab = setActiveTab;
+    }
+  });
+
+  getActiveTab = Florence.globalVars.activeTab;
+  accordion(getActiveTab);
+  getLastPosition();
+
+  // Metadata load, edition and saving
+  $("#title").on('input', function () {
+    $(this).textareaAutoSize();
+    data.description.title = $(this).val();
+    clearTimeout(timeoutId);
+    timeoutId = setTimeout(function () {
+      autoSaveMetadata(collectionId, data);
+    }, 3000);
+  });
+  $("#edition").on('input', function () {
+    $(this).textareaAutoSize();
+    data.description.edition = $(this).val();
+    clearTimeout(timeoutId);
+    timeoutId = setTimeout(function () {
+      autoSaveMetadata(collectionId, data);
+    }, 3000);
+  });
+  //if (!Florence.collection.date) {                        //overwrite scheduled collection date
+  if (!data.description.releaseDate) {
+    $('#releaseDate').datepicker({dateFormat: 'dd MM yy'}).on('change', function () {
+      data.description.releaseDate = new Date($(this).datepicker({dateFormat: 'dd MM yy'})[0].value).toISOString();
+      clearTimeout(timeoutId);
+      timeoutId = setTimeout(function () {
+        autoSaveMetadata(collectionId, data);
+      }, 3000);
+    });
+  } else {
+    //dateTmp = $('#releaseDate').val();
+    dateTmp = data.description.releaseDate;
+    var dateTmpFormatted = $.datepicker.formatDate('dd MM yy', new Date(dateTmp));
+    $('#releaseDate').val(dateTmpFormatted).datepicker({dateFormat: 'dd MM yy'}).on('change', function () {
+      data.description.releaseDate = new Date($('#releaseDate').datepicker('getDate')).toISOString();
+      clearTimeout(timeoutId);
+      timeoutId = setTimeout(function () {
+        autoSaveMetadata(collectionId, data);
+      }, 3000);
+    });
+  }
+  //} else {
+  //    $('.release-date').hide();
+  //}
+  $("#nextRelease").on('input', function () {
+    $(this).textareaAutoSize();
+    data.description.nextRelease = $(this).val();
+    clearTimeout(timeoutId);
+    timeoutId = setTimeout(function () {
+      autoSaveMetadata(collectionId, data);
+    }, 3000);
+  });
+  if (!data.description.contact) {
+    data.description.contact = {};
+  }
+  $("#contactName").on('input', function () {
+    $(this).textareaAutoSize();
+    data.description.contact.name = $(this).val();
+    clearTimeout(timeoutId);
+    timeoutId = setTimeout(function () {
+      autoSaveMetadata(collectionId, data);
+    }, 3000);
+  });
+  $("#contactEmail").on('input', function () {
+    $(this).textareaAutoSize();
+    data.description.contact.email = $(this).val();
+    clearTimeout(timeoutId);
+    timeoutId = setTimeout(function () {
+      autoSaveMetadata(collectionId, data);
+    }, 3000);
+  });
+  $("#contactTelephone").on('input', function () {
+    $(this).textareaAutoSize();
+    data.description.contact.telephone = $(this).val();
+    clearTimeout(timeoutId);
+    timeoutId = setTimeout(function () {
+      autoSaveMetadata(collectionId, data);
+    }, 3000);
+  });
+  $("#abstract").on('input', function () {
+    $(this).textareaAutoSize();
+    data.description._abstract = $(this).val();
+    clearTimeout(timeoutId);
+    timeoutId = setTimeout(function () {
+      autoSaveMetadata(collectionId, data);
+    }, 3000);
+  });
+  $("#keywordsTag").tagit({
+    availableTags: data.description.keywords,
+    singleField: true,
+    allowSpaces: true,
+    singleFieldNode: $('#keywords')
+  });
+  $('#keywords').on('change', function () {
+    data.description.keywords = $('#keywords').val().split(',');
+    clearTimeout(timeoutId);
+    timeoutId = setTimeout(function () {
+      autoSaveMetadata(collectionId, data);
+    }, 3000);
+  });
+  $("#metaDescription").on('input', function () {
+    $(this).textareaAutoSize();
+    data.description.metaDescription = $(this).val();
+    clearTimeout(timeoutId);
+    timeoutId = setTimeout(function () {
+      autoSaveMetadata(collectionId, data);
+    }, 3000);
+  });
+
+  /* The checked attribute is a boolean attribute, which means the corresponding property is true if the attribute
+   is present at all—even if, for example, the attribute has no value or is set to empty string value or even "false" */
+  var checkBoxStatus = function () {
+    if (data.description.nationalStatistic === "false" || data.description.nationalStatistic === false) {
+      return false;
+    }
+    return true;
+  };
+
+  $("#metadata-list input[type='checkbox']").prop('checked', checkBoxStatus).click(function () {
+    data.description.nationalStatistic = $("#metadata-list input[type='checkbox']").prop('checked') ? true : false;
+    clearTimeout(timeoutId);
+    timeoutId = setTimeout(function () {
+      autoSaveMetadata(collectionId, data);
+    }, 3000);
+  });
+
+  // Save
+  var editNav = $('.edit-nav');
+  editNav.off(); // remove any existing event handlers.
+
+  editNav.on('click', '.btn-edit-save', function () {
+    save();
+    updateContent(collectionId, data.uri, JSON.stringify(data));
+  });
+
+  // completed to review
+  editNav.on('click', '.btn-edit-save-and-submit-for-review', function () {
+    save();
+    saveAndCompleteContent(collectionId, data.uri, JSON.stringify(data));
+  });
+
+  // reviewed to approve
+  editNav.on('click', '.btn-edit-save-and-submit-for-approval', function () {
+    save();
+    saveAndReviewContent(collectionId, data.uri, JSON.stringify(data));
+  });
+
+  function save() {
+    // Sections
+    var orderSection = $("#sortable-section").sortable('toArray');
+    $(orderSection).each(function (indexS, nameS) {
+      var markdown = data.sections[parseInt(nameS)].markdown;
+      var title = $('#section-title_' + nameS).val();
+      newSections[indexS] = {title: title, markdown: markdown};
+    });
+    data.sections = newSections;
+    // Tabs
+    var orderTab = $("#sortable-tab").sortable('toArray');
+    $(orderTab).each(function (indexT, nameT) {
+      var markdown = data.accordion[parseInt(nameT)].markdown;
+      var title = $('#tab-title_' + nameT).val();
+      newTabs[indexT] = {title: title, markdown: markdown};
+    });
+    data.accordion = newTabs;
+    // Related documents
+    var orderDocument = $("#sortable-document").sortable('toArray');
+    $(orderDocument).each(function (indexD, nameD) {
+      var uri = data.relatedDocuments[parseInt(nameD)].uri;
+      var safeUri = checkPathSlashes(uri);
+      newDocuments[indexD] = {uri: safeUri};
+    });
+    data.relatedDocuments = newDocuments;
+    // Related data
+    var orderData = $("#sortable-dataset").sortable('toArray');
+    $(orderData).each(function (indexD, nameD) {
+      var uri = data.relatedDatasets[parseInt(nameD)].uri;
+      var safeUri = checkPathSlashes(uri);
+      newDatasets[indexD] = {uri: safeUri};
+    });
+    data.relatedDatasets = newDatasets;
+    // Files are uploaded. Save metadata
+    var orderFile = $("#sortable-file").sortable('toArray');
+    $(orderFile).each(function (indexF, nameF) {
+      var title = $('#file-title_' + nameF).val();
+      var file = data.downloads[parseInt(nameF)].file;
+      newFiles[indexF] = {title: title, file: file};
+    });
+    data.downloads = newFiles;
+    // External links
+    var orderLink = $("#sortable-link").sortable('toArray');
+    $(orderLink).each(function (indexL, nameL) {
+      var displayText = $('#link-markdown_' + nameL).val();
+      var link = $('#link-uri_' + nameL).val();
+      newLinks[indexL] = {uri: link, title: displayText};
+    });
+    data.links = newLinks;
+    // Related methodology
+    var orderRelatedMethodology = $("#sortable-methodology").sortable('toArray');
+    $(orderRelatedMethodology).each(function (indexM, nameM) {
+      var uri = data.relatedMethodology[parseInt(nameM)].uri;
+      var safeUri = checkPathSlashes(uri);
+      newRelatedMethodology[indexM] = {uri: safeUri};
+    });
+    data.relatedMethodology = newRelatedMethodology;
+  }
+}
+
+
