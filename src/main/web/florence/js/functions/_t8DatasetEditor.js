76e780ddbf152056aa8a11d4482f91d4e5c3d503
Crispioso <crispin.merriman@gmail.com>
2015-11-09 17:29:31 +0000

    Add 'back to parent' functionality in datasets
    Former-commit-id: e9527076c7c19f08454d115f87e770594599d0cd

--- src/main/web/florence/js/functions/_t8DatasetEditor.js
+++ src/main/web/florence/js/functions/_t8DatasetEditor.js
@@ -18,6 +18,51 @@ function datasetEditor(collectionId, data) {
 
   //resolveVersionCorrectionTitleT8(collectionId, data, 'versions');
 
+  //Add parent link onto page
+  var parentUri = getParentPage(data.uri);
+  $.ajax ({
+    url: parentUri + "/data",
+    dataType: 'json',
+    crossDomain: true,
+    success: function (data) {
+      //Add title to parent link area
+      var parentTitle = data.description.title;
+      $('.child-page__title').append(parentTitle);
+    },
+    error: function() {
+      sweetAlert("Error", "Could not find parent that this is a sub page of", "error");
+    }
+  });
+
+  //Add link back to parent page
+  $('.child-page').append("<a class='child-page__link'>Back to parent page</a>");
+
+  //Take user to parent edit screen on link click
+  $('.child-page__link').click(function(){
+    //If there are edits check whether user wants to continue
+    if (Florence.Editor.isDirty) {
+      swal ({
+        title: "Warning",
+        text: "You have unsaved changes. Are you sure you want to continue?",
+        type: "warning",
+        showCancelButton: true,
+        confirmButtonText: "Continue",
+        cancelButtonText: "Cancel"
+      }, function (result) {
+        if (result === true) {
+          Florence.Editor.isDirty = false;
+          //Return to parent if user confirms it
+          updateContent(collectionId, data.uri, JSON.stringify(data), parentUri);
+          return true;
+        }
+      });
+    } else {
+      //Return to parent
+      updateContent(collectionId, data.uri, JSON.stringify(data), parentUri);
+    }
+  });
+
+
   // Save
   var editNav = $('.edit-nav');
   editNav.off(); // remove any existing event handlers.
