5efb57be9339470cc01045e246fb6cd5b16571ed
pastor-methods <pastor.methods@gmail.com>
2015-12-09 16:40:21 +0000

    Add search functionality
    Former-commit-id: 606bfe30d0313c10ac833d4e980e5d651b0c6809

--- src/main/web/florence/js/functions/_viewTeamDetails.js
+++ src/main/web/florence/js/functions/_viewTeamDetails.js
@@ -54,50 +54,83 @@ function viewTeamDetails(teamName) {
 }
 
 function populateMembers(team) {
+  var userArray, userNotInTeam;
   getUsers(
-    function (users) {
-      var teamsHtml = templates.teamEdit({team: team, user: users});
+  function (users) {
+      userArray = _.pluck(users, "email");
+      userNotInTeam = _.difference(userArray, team.members);
+      var teamsHtml = templates.teamEdit({team: team, user: userNotInTeam});
       $('.section').append(teamsHtml);
+
+      $('#team-search-input').on('input', function () {
+        var searchText = $(this).val();
+        populateUsersList(userNotInTeam, searchText);
+      });
 
       $('.btn-team-selector-cancel').click(function () {
         $('.team-select').stop().fadeOut(200).remove();
+        viewTeamDetails(team.name);
       });
 
-      $(function () {
-        $('.user-list > li').draggable({
-          appendTo: 'document',
-          helper: 'clone',
-          cursor: 'move'
-        });
-
-        $('.team-list > li').draggable({
-          appendTo: 'document',
-          cursor: 'move'
-        });
-
-        $('.user-list').droppable({
-          accept: ".team-list > li",
-          drop: function (event, ui) {
-            var targetElem = $(this).attr("id");
-            $(this).addClass("ui-state-highlight");
-            $(ui.draggable).appendTo(this);
-            deleteTeamMember(team.name, ui.draggable[0].firstChild.textContent);
-          }
-        });
-
-        $('.team-list').droppable({
-          accept: ".user-list > li",
-          drop: function (event, ui) {
-            var targetElem = $(this).attr("id");
-            $(this).addClass("ui-state-highlight");
-            $(ui.draggable).appendTo(this);
-            postTeamMember(team.name, ui.draggable[0].firstChild.textContent);
-          }
-        });
-      });
+      dragAndDrop();
     },
     function (jqxhr) {
       handleApiError(jqxhr);
     }
   );
+
+  /**
+   * Populate the users list from the given array of users.
+   * @param users
+   */
+  function populateUsersList(users, filter) {
+    var userList = $('.user-list');
+    userList.find('li').remove(); // remove existing table entries
+
+    _(_.sortBy(users, function (user) {
+      return user;
+    }))
+      .each(function (user) {
+        if (!filter || (user.toUpperCase().indexOf(filter.toUpperCase()) > -1)) {
+          userList.append('<li class="ui-draggable ui-draggable-handle">' + user + '</li>');
+        }
+      });
+    dragAndDrop();
+  }
+
+  function dragAndDrop () {
+    $('.user-list > li').draggable({
+      appendTo: 'document',
+      helper: 'clone',
+      cursor: 'move'
+    });
+
+    $('.team-list > li').draggable({
+      appendTo: 'document',
+      helper: 'clone',
+      cursor: 'move'
+    });
+
+    $('.user-list').droppable({
+      accept: ".team-list > li",
+      drop: function (event, ui) {
+        var targetElem = $(this).attr("id");
+        $(this).addClass("ui-state-highlight");
+        $(ui.draggable).appendTo(this);
+        deleteTeamMember(team.name, ui.draggable[0].firstChild.textContent);
+        userNotInTeam.push(ui.draggable[0].firstChild.textContent);
+      }
+    });
+
+    $('.team-list').droppable({
+      accept: ".user-list > li",
+      drop: function (event, ui) {
+        var targetElem = $(this).attr("id");
+        $(this).addClass("ui-state-highlight");
+        $(ui.draggable).appendTo(this);
+        postTeamMember(team.name, ui.draggable[0].firstChild.textContent);
+        userNotInTeam = _.difference(userNotInTeam, [ui.draggable[0].firstChild.textContent]);
+      }
+    });
+  };
 }
