ded3f78d8a36de95d2d65adc10e3dac6b43c52ea
pastor-methods <pastor.methods@gmail.com>
2016-01-27 12:57:34 +0000

    Refactored external links saving
    Former-commit-id: 93cc9e913a43871a65976cd048144db3d439533d

--- src/main/web/florence/js/functions/_t7StaticLandingPageEditor.js
+++ src/main/web/florence/js/functions/_t7StaticLandingPageEditor.js
@@ -212,7 +212,7 @@ function staticLandingPageEditor(collectionId, data) {
     // External links
     var orderLink = $("#sortable-link").sortable('toArray');
     $(orderLink).each(function (indexL, nameL) {
-      var displayText = $('#link-markdown_' + nameL).val();
+      var displayText = data.links[parseInt(nameL)].title;
       var link = $('#link-uri_' + nameL).val();
       newLinks[indexL] = {uri: link, title: displayText};
     });
