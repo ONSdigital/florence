1c9a06bdc48a001b157aaf2bd6a48cac06db4e3f
Crispioso <crispin.merriman@gmail.com>
2016-03-09 13:45:29 +0000

    Allow collections to be scheduled on today

--- src/main/web/florence/js/functions/_viewCollections.js
+++ src/main/web/florence/js/functions/_viewCollections.js
@@ -1,126 +1,118 @@
 function viewCollections(collectionId) {
 
-  var result = {};
-  var pageDataRequests = []; // list of promises - one for each ajax request.
-  pageDataRequests.push(
-    $.ajax({
-      url: "/zebedee/collections",
-      type: "get",
-      success: function (data) {
-        result.data = data;
-      },
-      error: function (jqxhr) {
-        handleApiError(jqxhr);
-      }
-    })
-  );
-  pageDataRequests.push(
-    getTeams(
-      success = function (team) {
-        result.team = team;
-      },
-      error = function (response) {
-        handleApiError(response);
-      }
-    )
-  );
+    var result = {};
+    var pageDataRequests = []; // list of promises - one for each ajax request.
+    pageDataRequests.push(
+        $.ajax({
+            url: "/zebedee/collections",
+            type: "get",
+            success: function (data) {
+                result.data = data;
+            },
+            error: function (jqxhr) {
+                handleApiError(jqxhr);
+            }
+        })
+    );
+    pageDataRequests.push(
+        getTeams(
+            success = function (team) {
+                result.team = team;
+            },
+            error = function (response) {
+                handleApiError(response);
+            }
+        )
+    );
 
-  $.when.apply($, pageDataRequests).then(function () {
+    $.when.apply($, pageDataRequests).then(function () {
 
-    var response = [], teams = [];
+        var response = [], teams = [];
 
-    $.each(result.data, function (i, collection) {
-      if (!collection.approvedStatus) {
-        if (!collection.publishDate) {
-          date = '[manual collection]';
-          response.push({id: collection.id, name: collection.name, date: date});
-        } else if (collection.publishDate && collection.type === 'manual') {
-          var formattedDate = StringUtils.formatIsoDateString(collection.publishDate) + ' [rolled back]';
-          response.push({id: collection.id, name: collection.name, date: formattedDate});
-        } else {
-          var formattedDate = StringUtils.formatIsoDateString(collection.publishDate);
-          response.push({id: collection.id, name: collection.name, date: formattedDate});
+        $.each(result.data, function (i, collection) {
+            if (!collection.approvedStatus) {
+                if (!collection.publishDate) {
+                    date = '[manual collection]';
+                    response.push({id: collection.id, name: collection.name, date: date});
+                } else if (collection.publishDate && collection.type === 'manual') {
+                    var formattedDate = StringUtils.formatIsoDateString(collection.publishDate) + ' [rolled back]';
+                    response.push({id: collection.id, name: collection.name, date: formattedDate});
+                } else {
+                    var formattedDate = StringUtils.formatIsoDateString(collection.publishDate);
+                    response.push({id: collection.id, name: collection.name, date: formattedDate});
+                }
+            }
+        });
+
+        var collectionsHtml = templates.collectionList({response: response, teams: result.team.teams});
+        $('.section').html(collectionsHtml);
+
+        if (collectionId) {
+            $('.collections-select-table tr[data-id="' + collectionId + '"]')
+                .addClass('selected');
+            viewCollectionDetails(collectionId);
         }
-      }
+
+        $('.collections-select-table tbody tr').click(function () {
+            $('.collections-select-table tbody tr').removeClass('selected');
+            $(this).addClass('selected');
+            var collectionId = $(this).attr('data-id');
+            viewCollectionDetails(collectionId);
+        });
+
+        $("#team-tag").tagit({
+            singleField: true,
+            singleFieldNode: $('#team-input')
+        });
+
+        $('.ui-autocomplete-input').hide();
+
+        $('select#team').change(function () {
+            $('#team-tag').tagit('createTag', $("#team option:selected").text());
+        });
+
+        $('#team-input').change(function () {
+            teams = $('#team-input').val().split(',');
+            //After creating the array tagit leaves an empty string if all elements are removed
+            if (teams.length === 1 && teams[0] === "") {
+                teams = [];
+            }
+        });
+
+        $('form input[type=radio]').click(function () {
+
+            if ($(this).val() === 'manual') {
+                $('#scheduledPublishOptions').hide();
+            } else if ($(this).val() === 'scheduled') {
+                $('#scheduledPublishOptions').show();
+            } else if ($(this).val() === 'custom') {
+                $('#customScheduleOptions').show();
+                $('#releaseScheduleOptions').hide();
+            } else if ($(this).val() === 'release') {
+                $('#customScheduleOptions').hide();
+                $('#releaseScheduleOptions').show();
+            }
+        });
+
+
+        $(function () {
+            var today = new Date();
+            $('#date').datepicker({
+                minDate: today,
+                dateFormat: 'dd/mm/yy',
+                constrainInput: true
+            });
+        });
+
+
+        $('.btn-select-release').on("click", function (e) {
+            e.preventDefault();
+            viewReleaseSelector();
+        });
+
+        $('.form-create-collection').submit(function (e) {
+            e.preventDefault();
+            createCollection(teams);
+        });
     });
-
-    var collectionsHtml = templates.collectionList({response: response, teams: result.team.teams});
-    $('.section').html(collectionsHtml);
-
-    if (collectionId) {
-      $('.collections-select-table tr[data-id="' + collectionId + '"]')
-        .addClass('selected');
-      viewCollectionDetails(collectionId);
-    }
-
-    $('.collections-select-table tbody tr').click(function () {
-      $('.collections-select-table tbody tr').removeClass('selected');
-      $(this).addClass('selected');
-      var collectionId = $(this).attr('data-id');
-      viewCollectionDetails(collectionId);
-    });
-
-    $("#team-tag").tagit({
-      singleField: true,
-      singleFieldNode: $('#team-input')
-    });
-
-    $('.ui-autocomplete-input').hide();
-
-    $('select#team').change(function () {
-      $('#team-tag').tagit('createTag', $("#team option:selected").text());
-    });
-
-    $('#team-input').change(function () {
-      teams = $('#team-input').val().split(',');
-      //After creating the array tagit leaves an empty string if all elements are removed
-      if (teams.length === 1 && teams[0] === "") {
-        teams = [];
-      }
-    });
-
-    $('form input[type=radio]').click(function () {
-
-      if ($(this).val() === 'manual') {
-        $('#scheduledPublishOptions').hide();
-      } else if ($(this).val() === 'scheduled') {
-        $('#scheduledPublishOptions').show();
-      } else if ($(this).val() === 'custom') {
-        $('#customScheduleOptions').show();
-        $('#releaseScheduleOptions').hide();
-      } else if ($(this).val() === 'release') {
-        $('#customScheduleOptions').hide();
-        $('#releaseScheduleOptions').show();
-      }
-    });
-
-    var noBefore = function (date) {
-      if (date < new Date()) {
-        return [false];
-      }
-      return [true];
-    };
-
-
-    $(function () {
-      var today = new Date();
-      $('#date').datepicker({
-        minDate: today,
-        dateFormat: 'dd/mm/yy',
-        constrainInput: true,
-        beforeShowDay: noBefore
-      });
-    });
-
-
-    $('.btn-select-release').on("click", function (e) {
-      e.preventDefault();
-      viewReleaseSelector();
-    });
-
-    $('.form-create-collection').submit(function (e) {
-      e.preventDefault();
-      createCollection(teams);
-    });
-  });
 }
