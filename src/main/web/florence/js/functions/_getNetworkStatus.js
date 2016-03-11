2963879b5a6daf8d71095750f7cb9219e3d632f7
Crispioso <crispin.merriman@gmail.com>
2016-03-11 14:41:06 +0000

    Add network status function

---
+++ src/main/web/florence/js/functions/_getNetworkStatus.js
@@ -0,0 +1,20 @@
+/**
+ *  Get the network speed from zebedee/ping endpoint and output the network health
+ **/
+
+function networkStatus(ping) {
+    if (ping > 0 && ping < 100) {
+        $('.icon-status div').css({"opacity": "1.0"});
+    } else if (ping >= 100 && ping < 200) {
+        $('.icon-status--ok').css({"opacity": "1.0"});
+        $('.icon-status--poor').css({"opacity": "1.0"});
+        $('.icon-status--very-poor').css({"opacity": "1.0"});
+    } else if (ping >= 200 && ping < 300) {
+        $('.icon-status--poor').css({"opacity": "1.0"});
+        $('.icon-status--very-poor').css({"opacity": "1.0"});
+    } else if (ping >= 300) {
+        $('.icon-status--very-poor').css({"opacity": "1.0"});
+    }
+}
+
+networkStatus();
