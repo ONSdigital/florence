0091eeb95b1da05957774323f46d8191e25a5fe7
CarlHembrough <carlhuk@gmail.com>
2016-03-11 14:56:04 +0000

    Added additional state changes
    Former-commit-id: c1f77945cc78dc7c28bb2ea407bec7097a0feb03

--- src/main/web/florence/js/functions/_getNetworkStatus.js
+++ src/main/web/florence/js/functions/_getNetworkStatus.js
@@ -6,15 +6,19 @@ function networkStatus(ping) {
     if (ping > 0 && ping < 100) {
         $('.icon-status div').css({"opacity": "1.0"});
     } else if (ping >= 100 && ping < 200) {
+        $('.icon-status--good').css({"opacity": "0.1"});
         $('.icon-status--ok').css({"opacity": "1.0"});
         $('.icon-status--poor').css({"opacity": "1.0"});
         $('.icon-status--very-poor').css({"opacity": "1.0"});
     } else if (ping >= 200 && ping < 300) {
+        $('.icon-status--good').css({"opacity": "0.1"});
+        $('.icon-status--ok').css({"opacity": "0.1"});
         $('.icon-status--poor').css({"opacity": "1.0"});
         $('.icon-status--very-poor').css({"opacity": "1.0"});
     } else if (ping >= 300) {
+        $('.icon-status--good').css({"opacity": "0.1"});
+        $('.icon-status--ok').css({"opacity": "0.1"});
+        $('.icon-status--poor').css({"opacity": "0.1"});
         $('.icon-status--very-poor').css({"opacity": "1.0"});
     }
 }
-
-networkStatus();
