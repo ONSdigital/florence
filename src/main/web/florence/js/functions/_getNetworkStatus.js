e4d925bcfd4543eb9dc309091b0098b3c8e6ccd5
Crispioso <crispin.merriman@gmail.com>
2016-03-14 08:06:07 +0000

    Store variables for signal status

--- src/main/web/florence/js/functions/_getNetworkStatus.js
+++ src/main/web/florence/js/functions/_getNetworkStatus.js
@@ -3,22 +3,27 @@
  **/
 
 function networkStatus(ping) {
+    var $good = $('.icon-status--good'),
+        $ok = $('.icon-status--ok'),
+        $poor = $('.icon-status--poor'),
+        $veryPoor = $('.icon-status--very-poor');
+
     if (ping > 0 && ping < 100) {
         $('.icon-status div').css({"opacity": "1.0"});
     } else if (ping >= 100 && ping < 200) {
-        $('.icon-status--good').css({"opacity": "0.2"});
-        $('.icon-status--ok').css({"opacity": "1.0"});
-        $('.icon-status--poor').css({"opacity": "1.0"});
-        $('.icon-status--very-poor').css({"opacity": "1.0"});
+        $good.css({"opacity": "0.2"});
+        $ok.css({"opacity": "1.0"});
+        $poor.css({"opacity": "1.0"});
+        $veryPoor.css({"opacity": "1.0"});
     } else if (ping >= 200 && ping < 300) {
-        $('.icon-status--good').css({"opacity": "0.2"});
-        $('.icon-status--ok').css({"opacity": "0.2"});
-        $('.icon-status--poor').css({"opacity": "1.0"});
-        $('.icon-status--very-poor').css({"opacity": "1.0"});
+        $good.css({"opacity": "0.2"});
+        $ok.css({"opacity": "0.2"});
+        $poor.css({"opacity": "1.0"});
+        $veryPoor.css({"opacity": "1.0"});
     } else if (ping >= 300) {
-        $('.icon-status--good').css({"opacity": "0.2"});
-        $('.icon-status--ok').css({"opacity": "0.2"});
-        $('.icon-status--poor').css({"opacity": "0.2"});
-        $('.icon-status--very-poor').css({"opacity": "1.0"});
+        $good.css({"opacity": "0.2"});
+        $ok.css({"opacity": "0.2"});
+        $poor.css({"opacity": "0.2"});
+        $veryPoor.css({"opacity": "1.0"});
     }
 }
