4d03c7fcb767cf38e92a5d0cca5b2d66465e817d
Jon Dewi Jones <jondewijones@gmail.com>
2016-04-20 10:00:30 +0100

    Add if equals (if_eq) helper
    Former-commit-id: fb55ac3f79b198ba8882af2b5227b4005abc8863

--- src/main/web/florence/js/functions/_setupFlorence.js
+++ src/main/web/florence/js/functions/_setupFlorence.js
@@ -64,6 +64,12 @@ function setupFlorence() {
     }
     return "";
   });
+  Handlebars.registerHelper('if_eq', function(a, b, opts) {
+    if(a == b)
+      return opts.fn(this);
+    else
+      return opts.inverse(this);
+  });
 
 
 
