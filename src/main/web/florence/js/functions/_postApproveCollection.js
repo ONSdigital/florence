d6893ca5aa01416614d227e5575c2f72ce8485f1
Crispioso <crispin.merriman@gmail.com>
2016-04-25 18:12:37 +0100

    Fix animation on collection approval

--- src/main/web/florence/js/functions/_postApproveCollection.js
+++ src/main/web/florence/js/functions/_postApproveCollection.js
@@ -1,29 +1,25 @@
 function postApproveCollection(collectionId) {
-  $.ajax({
-    url: "/zebedee/approve/" + collectionId,
-    dataType: 'json',
-    contentType: 'application/json',
-    crossDomain: true,
-    type: 'POST',
-    success: function (response) {
-      //console.log(response);
-      //console.log(collectionId + ' collection is now approved');
-      $('.over').remove();
-      $('.collection-selected').stop().animate({right: "-50%"}, 500);
-      $('.collections-select-table tbody tr').removeClass('selected');
-      // Wait until the animation ends
-      setTimeout(function(){
-        viewController('collections');
-      }, 500);
-    },
-    error: function (response) {
-      $('.over').remove();
-      if (response.status === 409) {
-        sweetAlert("Cannot approve this collection", "It contains files that have not been approved.");
-      }
-      else {
-        handleApiError(response);
-      }
-    }
-  });
+    $.ajax({
+        url: "/zebedee/approve/" + collectionId,
+        dataType: 'json',
+        contentType: 'application/json',
+        crossDomain: true,
+        type: 'POST',
+        success: function (response) {
+            $('.over').remove();
+            $('.collections-select-table tbody tr').removeClass('selected');
+            $('.collection-selected').stop().animate({right: "-50%"}, 500, function() {
+                viewController('publish');
+            });
+        },
+        error: function (response) {
+            $('.over').remove();
+            if (response.status === 409) {
+                sweetAlert("Cannot approve this collection", "It contains files that have not been approved.");
+            }
+            else {
+                handleApiError(response);
+            }
+        }
+    });
 }
