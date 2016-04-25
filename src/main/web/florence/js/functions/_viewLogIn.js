8ad089b866785e51763bb1e2749fd1c957ef9c1e
Crispioso <crispin.merriman@gmail.com>
2016-04-25 15:34:05 +0100

    Add loading icon to login screen
    Former-commit-id: 90d450bf4183942e81865a20f91780d27f208994

--- src/main/web/florence/js/functions/_viewLogIn.js
+++ src/main/web/florence/js/functions/_viewLogIn.js
@@ -1,13 +1,14 @@
 function viewLogIn() {
 
-  var login_form = templates.login;
-  $('.section').html(login_form);
+    var login_form = templates.login;
+    $('.section').html(login_form);
 
-  $('.form-login').submit(function (e) {
-    e.preventDefault();
-    var email = $('.fl-user-and-access__email').val();
-    var password = $('.fl-user-and-access__password').val();
-    postLogin(email, password);
-  });
+    $('.form-login').submit(function (e) {
+        e.preventDefault();
+        loadingBtn($('#login'));
+        var email = $('.fl-user-and-access__email').val();
+        var password = $('.fl-user-and-access__password').val();
+        postLogin(email, password);
+    });
 }
 
