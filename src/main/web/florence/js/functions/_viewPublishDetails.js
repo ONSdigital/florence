948304e87b6f28328eb76c546482ee0615915b13
Crispioso <crispin.merriman@gmail.com>
2016-04-26 10:50:53 +0100

    Correct indentation of publish queue JS

--- src/main/web/florence/js/functions/_viewPublishDetails.js
+++ src/main/web/florence/js/functions/_viewPublishDetails.js
@@ -1,93 +1,96 @@
 function viewPublishDetails(collections) {
 
-  var manual = '[manual collection]';
-  var result = {
-    date: Florence.collectionToPublish.publishDate,
-    subtitle: '',
-    collectionDetails: [],
-  }
-  var pageDataRequests = []; // list of promises - one for each ajax request to load page data.
-  var onlyOne = 0;
+    var manual = '[manual collection]';
+    var result = {
+        date: Florence.collectionToPublish.publishDate,
+        subtitle: '',
+        collectionDetails: []
+    };
+    var pageDataRequests = []; // list of promises - one for each ajax request to load page data.
+    var onlyOne = 0;
 
-  $.each(collections, function (i, collectionId) {
-    onlyOne += 1;
-    pageDataRequests.push(
-      getCollectionDetails(collectionId,
-        success = function (response) {
-          if (result.date === manual) {
-            result.collectionDetails.push({
-              id: response.id,
-              name: response.name,
-              pageDetails: response.reviewed,
-              pageType: 'manual'
-            });
-          } else {
-            result.collectionDetails.push({id: response.id, name: response.name, pageDetails: response.reviewed});
-          }
-        },
-        error = function (response) {
-          handleApiError(response);
-        }
-      )
-    );
-  });
-
-  if (onlyOne < 2) {
-    result.subtitle = 'The following collection has been approved';
-  } else {
-    result.subtitle = 'The following collections have been approved';
-  }
-
-  $.when.apply($, pageDataRequests).then(function () {
-//  console.log(result)
-    var publishDetails = templates.publishDetails(result);
-    $('.publish-selected').html(publishDetails);
-    $('.collections-accordion').accordion({
-      header: '.collections-section__head',
-      heightStyle: "content",
-      active: false,
-      collapsible: true
+    $.each(collections, function (i, collectionId) {
+        onlyOne += 1;
+        pageDataRequests.push(
+            getCollectionDetails(collectionId,
+                success = function (response) {
+                    if (result.date === manual) {
+                        result.collectionDetails.push({
+                            id: response.id,
+                            name: response.name,
+                            pageDetails: response.reviewed,
+                            pageType: 'manual'
+                        });
+                    } else {
+                        result.collectionDetails.push({
+                            id: response.id,
+                            name: response.name,
+                            pageDetails: response.reviewed
+                        });
+                    }
+                },
+                error = function (response) {
+                    handleApiError(response);
+                }
+            )
+        );
     });
 
-    $('.btn-collection-publish').click(function () {
-      var collection = $(this).closest('.collections-section').find('.collection-name').attr('data-id');
-      publish(collection);
-    });
+    if (onlyOne < 2) {
+        result.subtitle = 'The following collection has been approved';
+    } else {
+        result.subtitle = 'The following collections have been approved';
+    }
 
-    $('.btn-collection-unlock').click(function () {
-      var collection = $(this).closest('.collections-section').find('.collection-name').attr('data-id');
+    $.when.apply($, pageDataRequests).then(function () {
+        var publishDetails = templates.publishDetails(result);
+        $('.publish-selected').html(publishDetails);
+        $('.collections-accordion').accordion({
+            header: '.collections-section__head',
+            heightStyle: "content",
+            active: false,
+            collapsible: true
+        });
 
-      if (result.date !== manual) {
-        swal({
-            title: "Are you sure?",
-            text: "If unlocked, this collection will not be published on " + result.date + " unless it is approved" +
-            " again",
-            type: "warning",
-            showCancelButton: true,
-            confirmButtonColor: "#6d272b",
-            confirmButtonText: "Yes, unlock it!",
-            closeOnConfirm: false
-          },
-          function () {
-            unlock(collection);
-          });
-      } else {
-        unlock(collection);
-      }
-    });
+        $('.btn-collection-publish').click(function () {
+            var collection = $(this).closest('.collections-section').find('.collection-name').attr('data-id');
+            publish(collection);
+        });
 
-    //page-list
-    $('.page-item').click(function () {
-      $('.page-list li').removeClass('selected');
-      $('.page-options').hide();
+        $('.btn-collection-unlock').click(function () {
+            var collection = $(this).closest('.collections-section').find('.collection-name').attr('data-id');
 
-      $(this).parent('li').addClass('selected');
-      $(this).next('.page-options').show();
+            if (result.date !== manual) {
+                swal({
+                        title: "Are you sure?",
+                        text: "If unlocked, this collection will not be published on " + result.date + " unless it is approved" +
+                        " again",
+                        type: "warning",
+                        showCancelButton: true,
+                        confirmButtonColor: "#6d272b",
+                        confirmButtonText: "Yes, unlock it!",
+                        closeOnConfirm: false
+                    },
+                    function () {
+                        unlock(collection);
+                    });
+            } else {
+                unlock(collection);
+            }
+        });
+
+        //page-list
+        $('.page-item').click(function () {
+            $('.page-list li').removeClass('selected');
+            $('.page-options').hide();
+
+            $(this).parent('li').addClass('selected');
+            $(this).next('.page-options').show();
+        });
+        $('.publish-selected .btn-collection-cancel').click(function () {
+            $('.publish-selected').animate({right: "-50%"}, 500);
+            $('.publish-select').animate({marginLeft: "25%"}, 800);
+            $('.publish-select-table tbody tr').removeClass('selected');
+        });
     });
-    $('.publish-selected .btn-collection-cancel').click(function () {
-      $('.publish-selected').animate({right: "-50%"}, 500);
-      $('.publish-select').animate({marginLeft: "25%"}, 800);
-      $('.publish-select-table tbody tr').removeClass('selected');
-    });
-  });
 }
