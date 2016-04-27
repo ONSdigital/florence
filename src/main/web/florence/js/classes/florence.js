51a1f65b36d6dd1219ee763ef6a1347e21595a8e
Crispioso <crispin.merriman@gmail.com>
2016-04-27 12:04:42 +0100

    Switch browse tree on iframe load instead of click

--- src/main/web/florence/js/classes/florence.js
+++ src/main/web/florence/js/classes/florence.js
@@ -1,28 +1,34 @@
 // The florence object is used for storing application state.
 var Florence = Florence || {
-    tredegarBaseUrl: window.location.origin,
-    refreshAdminMenu: function () {
-      var mainNavHtml = templates.mainNav(Florence);
-      $('.admin-nav').html(mainNavHtml);
-    },
-    setActiveCollection: function (collection) {
-      document.cookie = "collection=" + collection.id + ";path=/";
-      if (!collection.publishDate) {
-        var formattedDate = null;
-      } else {
-        var formattedDate = StringUtils.formatIsoDateString(collection.publishDate);
-      }
-      Florence.collection = {id: collection.id, name: collection.name, date: formattedDate, publishDate: collection.publishDate, type: collection.type};
-    }
-  };
+        tredegarBaseUrl: window.location.origin,
+        refreshAdminMenu: function () {
+            var mainNavHtml = templates.mainNav(Florence);
+            $('.admin-nav').html(mainNavHtml);
+        },
+        setActiveCollection: function (collection) {
+            document.cookie = "collection=" + collection.id + ";path=/";
+            if (!collection.publishDate) {
+                var formattedDate = null;
+            } else {
+                var formattedDate = StringUtils.formatIsoDateString(collection.publishDate);
+            }
+            Florence.collection = {
+                id: collection.id,
+                name: collection.name,
+                date: formattedDate,
+                publishDate: collection.publishDate,
+                type: collection.type
+            };
+        }
+    };
 
 Florence.Editor = {
-  isDirty: false,
-  data: {}
+    isDirty: false,
+    data: {}
 };
 
 Florence.CreateCollection = {
-  selectedRelease:""
+    selectedRelease: ""
 };
 
 Florence.collection = {};
@@ -32,51 +38,53 @@ Florence.collectionToPublish = {};
 Florence.globalVars = {pagePath: '', activeTab: false, pagePos: '', welsh: false};
 
 Florence.Authentication = {
-  accessToken: function () {
-    return CookieUtils.getCookieValue("access_token");
-  },
-  isAuthenticated: function () {
-    return Florence.Authentication.accessToken() !== '';
-  },
-  loggedInEmail: function () {
-    return localStorage.getItem("loggedInAs");
-  }
+    accessToken: function () {
+        return CookieUtils.getCookieValue("access_token");
+    },
+    isAuthenticated: function () {
+        return Florence.Authentication.accessToken() !== '';
+    },
+    loggedInEmail: function () {
+        return localStorage.getItem("loggedInAs");
+    }
 };
 
 Florence.Handler = function (e) {
-  if (Florence.Editor.isDirty) {
-    var result = confirm("You have unsaved changes. Are you sure you want to continue");
-    if (result === true) {
-      Florence.Editor.isDirty = false;
-      processPreviewClick(this);
-      return true;
-    } else {
-      e.preventDefault();
-      return false;
-    }
-  } else {
-    processPreviewClick(this);
-  }
+    var noIframe = true;
 
-  function processPreviewClick() {
-    setTimeout(function () {
-      checkForPageChanged(function (newUrl) {
-        var safeUrl = checkPathSlashes(newUrl);
-        Florence.globalVars.pagePath = safeUrl;
-        if ($('.workspace-edit').length) {
-          loadPageDataIntoEditor(safeUrl, Florence.collection.id, 'click');
+    if (Florence.Editor.isDirty) {
+        var result = confirm("You have unsaved changes. Are you sure you want to continue");
+        if (result === true) {
+            Florence.Editor.isDirty = false;
+            processPreviewClick(this);
+            return true;
+        } else {
+            e.preventDefault();
+            return false;
         }
-        else if ($('.workspace-browse').length) {
-          treeNodeSelect(safeUrl);
-        }
-      });
-    }, 200);
-  }
+    } else {
+        processPreviewClick(this);
+    }
+
+    function processPreviewClick() {
+        setTimeout(function () {
+            checkForPageChanged(function (newUrl) {
+                var safeUrl = checkPathSlashes(newUrl);
+                Florence.globalVars.pagePath = safeUrl;
+                if ($('.workspace-edit').length) {
+                    loadPageDataIntoEditor(safeUrl, Florence.collection.id, 'click');
+                }
+                else if ($('.workspace-browse').length) {
+                    treeNodeSelect(safeUrl);
+                }
+            });
+        }, 200);
+    }
 };
 
 // if running in a node environment export this as a module.
 if (typeof module !== 'undefined') {
-  module.exports = Florence;
+    module.exports = Florence;
 }
 
 
--- src/main/web/florence/js/functions/_loadBrowseScreen.js
+++ src/main/web/florence/js/functions/_loadBrowseScreen.js
@@ -1,63 +1,107 @@
 function loadBrowseScreen(collectionId, click) {
 
+    // Check if browse tree data already in local storage
+    // if (localStorage.getItem("browseTree") === null) {
+    //     // Request browse tree from zebedee
+    //     var request = $.ajax({
+    //         url: "/zebedee/collectionBrowseTree/" + collectionId, // url: "/navigation",
+    //         dataType: 'json',
+    //         type: 'GET',
+    //         success: function (response) {
+    //             console.log(request);
+    //             bindBrowseEvents(response, click);
+    //         },
+    //         error: function (response) {
+    //             handleApiError(response);
+    //         }
+    //     });
+    //     localStorage.setItem('browseTree', request);
+    //     return request;
+    // } else {
+    //     console.log('In local storage');
+    //     // Browse tree is available in local storage, return that instead
+    //     var browseTree = localStorage.getItem('browseTree');
+    //     return bindBrowseEvents(browseTree, click);
+    // }
+
+    console.log(collectionId);
+
+    // Request browse tree from zebedee
+    console.time('Get tree');
     return $.ajax({
         url: "/zebedee/collectionBrowseTree/" + collectionId, // url: "/navigation",
         dataType: 'json',
         type: 'GET',
         success: function (response) {
-            var browserContent = $('#iframe')[0].contentWindow;
-            var baseURL = Florence.tredegarBaseUrl;
-            var html = templates.workBrowse(response);
-            $('.workspace-menu').html(html);
-            $('.workspace-browse').css("overflow", "scroll");
-
-            //page-list
-            $('.page-item').click(function () {
-
-                var uri = $(this).closest('li').attr('data-url');
-
-                if (uri) {
-                    var newURL = baseURL + uri;
-
-                    $('.page-list li').removeClass('selected');
-                    $(this).parent('li').addClass('selected');
-
-                    $('.page-options').hide();
-                    $(this).next('.page-options').show();
-
-                    //change iframe location
-                    browserContent.location.href = newURL;
-                    Florence.globalVars.pagePath = uri;
-                    $('.browser-location').val(newURL);
-                }
-
-                //page-list-tree
-                $('.tree-nav-holder ul').removeClass('active');
-                $(this).parents('ul').addClass('active');
-                $(this).closest('li').children('ul').addClass('active');
-
-                $(this).closest('li').find('.page-item--directory').removeClass('page-item--directory--selected');
-                if ($(this).hasClass('page-item--directory')) {
-                    $('.page-item--directory').removeClass('page-item--directory--selected');
-                    $(this).addClass('page-item--directory--selected');
-                }
-            });
-
-            if (click) {
-                var url = getPathName();
-                if (url === "/blank") {
-                    setTimeout(treeNodeSelect('/'), 500);
-                } else {
-                    treeNodeSelect(url);
-                }
-            } else {
-                treeNodeSelect('/');
-            }
-
+            console.timeEnd(response);
+            bindBrowseEvents(response, click);
         },
         error: function (response) {
             handleApiError(response);
         }
     });
+}
+
+function bindBrowseEvents(data, click) {
+    console.time('Build browse');
+    var browserContent = $('#iframe')[0].contentWindow;
+    var baseURL = Florence.tredegarBaseUrl;
+    console.time('Handlebars');
+    var html = templates.workBrowse(data);
+    console.timeEnd('Handlebars');
+    console.time('Append html');
+    $('.workspace-menu').html(html);
+    console.timeEnd('Append html');
+    $('.workspace-browse').css("overflow", "scroll");
+
+    console.time('Item binding');
+    //page-list
+    $('.page-item').click(function () {
+        var $this = $(this),
+            uri = $this.closest('li').attr('data-url');
+
+
+        if (uri) {
+            var newURL = baseURL + uri;
+
+            $('.page-list li').removeClass('selected');
+            $this.parent('li').addClass('selected');
+
+            $('.page-options').hide();
+            $this.next('.page-options').show();
+
+            //change iframe location
+            browserContent.location.href = newURL;
+            Florence.globalVars.pagePath = uri;
+            $('.browser-location').val(newURL);
+        }
+
+        //page-list-tree
+        $('.tree-nav-holder ul').removeClass('active');
+        $this.parents('ul').addClass('active');
+        $this.closest('li').children('ul').addClass('active');
+
+        $this.closest('li').find('.page-item--directory').removeClass('page-item--directory--selected');
+        if ($this.hasClass('page-item--directory')) {
+            $('.page-item--directory').removeClass('page-item--directory--selected');
+            $this.addClass('page-item--directory--selected');
+        }
+    });
+    console.timeEnd('Item binding');
+
+    if (click) {
+        // var url = getPathName();
+        // if (url === "/blank") {
+        //     setTimeout(treeNodeSelect('/'), 500);
+        // } else {
+        //     treeNodeSelect(url);
+        // }
+        var url = getPathName();
+        treeNodeSelect(url);
+    } else {
+        treeNodeSelect('/');
+    }
+
+    console.timeEnd('Build browse');
 }
 
