a266e40eb4383dea8f69b6db376b376206696089
Crispioso <crispin.merriman@gmail.com>
2016-05-18 08:47:27 +0100

    Edit/submit zip file added

--- src/main/web/florence/js/functions/_checkRenameUri.js
+++ src/main/web/florence/js/functions/_checkRenameUri.js
@@ -7,138 +7,138 @@
  */
 function checkRenameUri(collectionId, data, renameUri, onSave) {
 
-  if (renameUri && !data.description.language && !data.description.edition) {   // It will not change welsh url + do not rename content with edition.
-    doRename();// askUserIfTheyAreSure();
-  } else {
-    onSave(collectionId, data.uri, JSON.stringify(data));
-  }
-
-  function askUserIfTheyAreSure() {
-    swal({
-      title: "Warning",
-      text: "You have changed the title or edition and this could change the uri. Are you sure you want to proceed?",
-      type: "warning",
-      showCancelButton: true,
-      confirmButtonText: "Change url",
-      cancelButtonText: "Cancel",
-      closeOnConfirm: true
-    }, function (result) {
-      if (result === true) {
-        doRename();
-      } else {
-        refreshPreview(data.uri);
-        loadPageDataIntoEditor(data.uri, collectionId);
-      }
-    });
-  }
-
-  function doRename() {
-    // Does it have edition?
-    if (data.description.edition) {
-      // CH 29/04/2016 disabling the URI change of content with an edition as it breaks the link of previous editions
-      //moveContentWithEditionInUri();
-    } else if (data.type === 'static_adhoc') {
-      moveAdHoc();
+    if (renameUri && !data.description.language && !data.description.edition) {   // It will not change welsh url + do not rename content with edition.
+        doRename();// askUserIfTheyAreSure();
     } else {
-      moveContentWithoutEdition();
-    }
-  }
-
-  function moveAdHoc() {
-    var titleNoSpace = data.description.title.replace(/[^A-Z0-9]+/ig, "").toLowerCase();
-    var referenceNoSpace = data.description.reference.replace(/[^A-Z0-9]+/ig, "").toLowerCase();
-    var tmpNewUri = data.uri.split("/");
-    tmpNewUri.splice([tmpNewUri.length - 1], 1, referenceNoSpace + titleNoSpace);
-    var newUri = tmpNewUri.join("/");
-
-    putContent(collectionId, data.uri, JSON.stringify(data), function () {
-      moveContent(collectionId, data.uri, newUri, function () {
-        getPageData(collectionId, newUri, function (pageData) { // get the updated data after doing the move.
-            data = pageData;
-            Florence.globalVars.pagePath = newUri;
-            onSave(collectionId, newUri, JSON.stringify(data));
-          },
-          onError = function () {
-            onSave(collectionId, data.uri, JSON.stringify(data));
-          });
-      });
-    });
-
-    return titleNoSpace;
-  }
-
-  function moveContentWithEditionInUri() {
-    //Special case dataset editions. They have edition but not title
-    if (data.description.title) {
-      var titleNoSpace = data.description.title.replace(/[^A-Z0-9]+/ig, "").toLowerCase();
-    }
-    var editionNoSpace = data.description.edition.replace(/[^A-Z0-9]+/ig, "").toLowerCase();
-    var tmpNewUri = data.uri.split("/");
-    if (data.type === 'dataset') {
-      tmpNewUri.splice([tmpNewUri.length - 1], 1, editionNoSpace);
-    } else {
-      tmpNewUri.splice([tmpNewUri.length - 2], 2, titleNoSpace, editionNoSpace);
+        onSave(collectionId, data.uri, JSON.stringify(data));
     }
 
-    var newUri = tmpNewUri.join("/");
-
-    // save any changes before actually doing the move
-    putContent(collectionId, data.uri, JSON.stringify(data), function () {
-      moveContent(collectionId, data.uri, newUri, function () {
-          getPageData(collectionId, newUri, function (pageData) { // get the updated data after doing the move.
-            data = pageData;
-
-            Florence.globalVars.pagePath = newUri;
-            //is it a compendium? Rename children array
-            //take this out if moveContent in Zebedee works
-            if (data.type === 'compendium_landing_page') {
-              if (data.chapters) {
-                data.chapters = renameCompendiumChildren(data.chapters, titleNoSpace, editionNoSpace);
-              }
-              if (data.datasets) {
-                data.datasets = renameCompendiumChildren(data.datasets, titleNoSpace, editionNoSpace);
-              }
+    function askUserIfTheyAreSure() {
+        swal({
+            title: "Warning",
+            text: "You have changed the title or edition and this could change the uri. Are you sure you want to proceed?",
+            type: "warning",
+            showCancelButton: true,
+            confirmButtonText: "Change url",
+            cancelButtonText: "Cancel",
+            closeOnConfirm: true
+        }, function (result) {
+            if (result === true) {
+                doRename();
+            } else {
+                refreshPreview(data.uri);
+                loadPageDataIntoEditor(data.uri, collectionId);
             }
-            onSave(collectionId, newUri, JSON.stringify(data));
-          });
-        },
-        onError = function () {
-          onSave(collectionId, data.uri, JSON.stringify(data));
         });
-    });
-  }
-
-  function moveContentWithoutEdition() {
-    var titleNoSpace = data.description.title.replace(/[^A-Z0-9]+/ig, "").toLowerCase();
-    var tmpNewUri = data.uri.split("/");
-    //Articles with no edition. Add date as edition
-    if (data.type === 'article' || data.type === 'article_download') {
-      var editionDate = $.datepicker.formatDate('yy-mm-dd', new Date());
-      tmpNewUri.splice([tmpNewUri.length - 2], 2, titleNoSpace, editionDate);
-    } else {
-      tmpNewUri.splice([tmpNewUri.length - 1], 1, titleNoSpace);
     }
-    var newUri = tmpNewUri.join("/");
 
-    putContent(collectionId, data.uri, JSON.stringify(data), function () {
-      moveContent(collectionId, data.uri, newUri, function () {
-          getPageData(collectionId, newUri, function (pageData) { // get the updated data after doing the move.
-            data = pageData;
+    function doRename() {
+        // Does it have edition?
+        if (data.description.edition) {
+            // CH 29/04/2016 disabling the URI change of content with an edition as it breaks the link of previous editions
+            //moveContentWithEditionInUri();
+        } else if (data.type === 'static_adhoc') {
+            moveAdHoc();
+        } else {
+            moveContentWithoutEdition();
+        }
+    }
 
-            Florence.globalVars.pagePath = newUri;
-            //if it is a dataset rename children array
-            //take this out if moveContent in Zebedee works
-            if (data.type === 'dataset_landing_page') {
-              if (data.datasets) {
-                data.datasets = renameDatasetChildren(data.datasets, titleNoSpace);
-              }
-            }
-            onSave(collectionId, newUri, JSON.stringify(data));
-          });
-        },
-        onError = function () {
-          onSave(collectionId, data.uri, JSON.stringify(data));
+    function moveAdHoc() {
+        var titleNoSpace = data.description.title.replace(/[^A-Z0-9]+/ig, "").toLowerCase();
+        var referenceNoSpace = data.description.reference.replace(/[^A-Z0-9]+/ig, "").toLowerCase();
+        var tmpNewUri = data.uri.split("/");
+        tmpNewUri.splice([tmpNewUri.length - 1], 1, referenceNoSpace + titleNoSpace);
+        var newUri = tmpNewUri.join("/");
+
+        putContent(collectionId, data.uri, JSON.stringify(data), function () {
+            moveContent(collectionId, data.uri, newUri, function () {
+                getPageData(collectionId, newUri, function (pageData) { // get the updated data after doing the move.
+                        data = pageData;
+                        Florence.globalVars.pagePath = newUri;
+                        onSave(collectionId, newUri, JSON.stringify(data));
+                    },
+                    onError = function () {
+                        onSave(collectionId, data.uri, JSON.stringify(data));
+                    });
+            });
         });
-    });
-  }
+
+        return titleNoSpace;
+    }
+
+    function moveContentWithEditionInUri() {
+        //Special case dataset editions. They have edition but not title
+        if (data.description.title) {
+            var titleNoSpace = data.description.title.replace(/[^A-Z0-9]+/ig, "").toLowerCase();
+        }
+        var editionNoSpace = data.description.edition.replace(/[^A-Z0-9]+/ig, "").toLowerCase();
+        var tmpNewUri = data.uri.split("/");
+        if (data.type === 'dataset') {
+            tmpNewUri.splice([tmpNewUri.length - 1], 1, editionNoSpace);
+        } else {
+            tmpNewUri.splice([tmpNewUri.length - 2], 2, titleNoSpace, editionNoSpace);
+        }
+
+        var newUri = tmpNewUri.join("/");
+
+        // save any changes before actually doing the move
+        putContent(collectionId, data.uri, JSON.stringify(data), function () {
+            moveContent(collectionId, data.uri, newUri, function () {
+                    getPageData(collectionId, newUri, function (pageData) { // get the updated data after doing the move.
+                        data = pageData;
+
+                        Florence.globalVars.pagePath = newUri;
+                        //is it a compendium? Rename children array
+                        //take this out if moveContent in Zebedee works
+                        if (data.type === 'compendium_landing_page') {
+                            if (data.chapters) {
+                                data.chapters = renameCompendiumChildren(data.chapters, titleNoSpace, editionNoSpace);
+                            }
+                            if (data.datasets) {
+                                data.datasets = renameCompendiumChildren(data.datasets, titleNoSpace, editionNoSpace);
+                            }
+                        }
+                        onSave(collectionId, newUri, JSON.stringify(data));
+                    });
+                },
+                onError = function () {
+                    onSave(collectionId, data.uri, JSON.stringify(data));
+                });
+        });
+    }
+
+    function moveContentWithoutEdition() {
+        var titleNoSpace = data.description.title.replace(/[^A-Z0-9]+/ig, "").toLowerCase();
+        var tmpNewUri = data.uri.split("/");
+        //Articles with no edition. Add date as edition
+        if (data.type === 'article' || data.type === 'article_download') {
+            var editionDate = $.datepicker.formatDate('yy-mm-dd', new Date());
+            tmpNewUri.splice([tmpNewUri.length - 2], 2, titleNoSpace, editionDate);
+        } else {
+            tmpNewUri.splice([tmpNewUri.length - 1], 1, titleNoSpace);
+        }
+        var newUri = tmpNewUri.join("/");
+
+        putContent(collectionId, data.uri, JSON.stringify(data), function () {
+            moveContent(collectionId, data.uri, newUri, function () {
+                    getPageData(collectionId, newUri, function (pageData) { // get the updated data after doing the move.
+                        data = pageData;
+
+                        Florence.globalVars.pagePath = newUri;
+                        //if it is a dataset rename children array
+                        //take this out if moveContent in Zebedee works
+                        if (data.type === 'dataset_landing_page') {
+                            if (data.datasets) {
+                                data.datasets = renameDatasetChildren(data.datasets, titleNoSpace);
+                            }
+                        }
+                        onSave(collectionId, newUri, JSON.stringify(data));
+                    });
+                },
+                onError = function () {
+                    onSave(collectionId, data.uri, JSON.stringify(data));
+                });
+        });
+    }
 }
--- src/main/web/florence/js/functions/_visualisationEditor.js
+++ src/main/web/florence/js/functions/_visualisationEditor.js
@@ -5,10 +5,148 @@
  */
 
 function visualisationEditor(collectionId, data) {
-    // Build section for uploading single ZIP file (not using editAddFile function because we only want a single file with just the uri stored)
-    
+    var renameUri = false,
+        $title = $('#title'),
+        $uid = $('#uid'),
+        setActiveTab, getActiveTab;
 
-    // Bind submit and cancel click event
-    
+    // Active tab
+    $(".edit-accordion").on('accordionactivate', function() {
+        setActiveTab = $(".edit-accordion").accordion("option", "active");
+        if (setActiveTab !== false) {
+            Florence.globalVars.activeTab = setActiveTab;
+        }
+    });
+    getActiveTab = Florence.globalVars.activeTab;
+    accordion(getActiveTab);
+    getLastPosition();
 
+    // Submit new ZIP file
+    bindZipSubmit();
+
+    // Edit existing ZIP file
+    $('#edit-vis').on('submit', function(e) {
+        e.preventDefault();
+        e.stopImmediatePropagation();
+
+        // Refresh visualisations tab but show 'submit ZIP' option
+        var tempData = data;
+        tempData.fileUri = "";
+        var html = templates.workEditVisualisation(tempData);
+        $('.workspace-menu').html(html);
+        bindZipSubmit();
+
+        // Set visualisation tab to active
+        accordion(1);
+    });
+    
+    // Bind save buttons
+    var editNav = $('.edit-nav');
+    editNav.off(); // remove any existing event handlers.
+
+    editNav.on('click', '.btn-edit-save', function () {
+        save(updateContent);
+    });
+
+    editNav.on('click', '.btn-edit-save-and-submit-for-review', function () {
+        save(saveAndCompleteContent);
+    });
+
+    editNav.on('click', '.btn-edit-save-and-submit-for-approval', function () {
+        save(saveAndReviewContent);
+    });
+
+
+    function bindZipSubmit() {
+        // Upload ZIP file
+        $('#upload-vis').on('submit', function(e) {
+            e.preventDefault();
+            e.stopImmediatePropagation();
+
+            var formdata = new FormData($(this)[0]),
+                file = this[0].files[0];
+
+            if (!$('#input-vis')) {
+                sweetAlert("Please choose a file before submitting");
+                return false;
+            }
+
+            if (!file.name.match(/\.zip$/)) {
+                sweetAlert("Incorrect file format", "You are only allowed to upload ZIP files", "warning")
+            } else {
+                var fileNameNoSpace = file.name.replace(/[^a-zA-Z0-9\.]/g, "").toLowerCase();
+                var uniqueIdNoSpace = data.uid.replace(/[^a-zA-Z0-9\.]/g, "").toLowerCase();
+                var uriUpload = "/visualisations/" + uniqueIdNoSpace + "/" + fileNameNoSpace;
+                var safeUriUpload = checkPathSlashes(uriUpload);
+
+                uploadFile(
+                    safeUriUpload, formdata,
+                    success = function() {
+                        data.fileUri = uriUpload; // Update fileUri ready for save
+                        unpackZip(safeUriUpload);
+                        save(updateContent);
+                    }
+                )
+            }
+
+        });
+    }
+
+    function uploadFile(path, formData, success) {
+        // Send zip file to zebedee
+        $.ajax({
+            url: "/zebedee/content/" + Florence.collection.id + "?uri=" + path,
+            type: 'POST',
+            data: formData,
+            async: false,
+            cache: false,
+            contentType: false,
+            processData: false,
+            success: function (response) {
+                success(response);
+            },
+            error: function(response) {
+                handleApiError(response);
+            }
+        });
+    }
+
+    function unpackZip(zipPath) {
+        // Unpack contents of ZIP
+        console.log("Unpack: " + zipPath);
+        var url = "/zebedee/UnzipDataVisualisation/" + Florence.collection.id + "?zipPath=" + zipPath;
+
+        $.ajax({
+            url: url,
+            dataType: 'json',
+            contentType: 'application/json',
+            type: 'POST',
+            success: function (response) {
+                success(response);
+            },
+            error: function (response) {
+                if (error) {
+                    error(response);
+                } else {
+                    handleApiError(response);
+                }
+            }
+        });
+    }
+
+    function save(onSave) {
+        // Stop the unique ID being editable
+        if (data.uid != $uid.val()) {
+            sweetAlert("A unique ID can't be edited after creation");
+            loadPageDataIntoEditor(data.uri, collectionId);
+        }
+
+        // Stop title from being editable
+        if (data.description.title != $title.val()) {
+            sweetAlert("The title can't be edited after creation");
+            loadPageDataIntoEditor(data.uri, collectionId);
+        }
+
+        checkRenameUri(collectionId, data, renameUri, onSave);
+    }
 }
