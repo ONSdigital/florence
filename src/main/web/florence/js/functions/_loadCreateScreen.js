37aaafa425eb97ba92e69c151373e0b54dceb15f
Crispioso <crispin.merriman@gmail.com>
2016-05-18 11:39:29 +0100

    Move main JS and CSS into assets folder
    Former-commit-id: b1f08afa22b6867bb133d75dc469112ff19d9f07

--- src/main/web/florence/js/functions/_loadCreateScreen.js
+++ src/main/web/florence/js/functions/_loadCreateScreen.js
@@ -3,7 +3,6 @@ function loadCreateScreen(parentUrl, collectionId, type, collectionData) {
     // Load data vis creator or ordinary publisher creator
     if (collectionData && collectionData.collectionOwner == "DATA_VISUALISATION") {
         var html = templates.workCreate({"dataVis": true});
-        
     } else {
         var html = templates.workCreate();
     }
