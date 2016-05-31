3e9f2ca156febc7695eae4b1f30a505c4269eb13
Crispioso <crispin.merriman@gmail.com>
2016-05-31 12:23:15 +0100

    Replace accordion with lighter version
    Former-commit-id: 7c44faad6aa2e96c16e7b161364689d490f54eaf

--- src/main/web/florence/js/components/_accordion.js
+++ src/main/web/florence/js/components/_accordion.js
@@ -1,11 +1,31 @@
 /**
- *  A simple jQuery accordion (inspired by 'https://css-tricks.com/snippets/jquery/simple-jquery-accordion/')
+ *  A simple jQuery accordion
  *  Requires a container ('js-accordion'), a title ('js-accordion__title) and the content ('js-accordion__content')
  **/
 function bindAccordions() {
     var $title = $('.js-accordion .js-accordion__title');
 
-    $title.click(function(e) {
-        $(this).find('.js-accordion__content').addClass('.active');
+    $title.click(function() {
+        var $this = $(this),
+            $activeAccordions = $('.js-accordion .active'),
+            active = false;
+
+        // Remove class that disables animations on load
+        $this.next('.js-accordion__content').removeClass('disable-animation');
+
+        // Detect whether the accordion is active already or not
+        if ($this.hasClass('active')) {
+            active = true;
+        }
+
+        // Deselect any accordions already active
+        $activeAccordions.removeClass('active');
+
+        // Activate clicked accordion if it wasn't already
+        if (!active) {
+            $this.addClass('active');
+            $this.next('.js-accordion__content').addClass('active');
+        }
+
     });
 }
