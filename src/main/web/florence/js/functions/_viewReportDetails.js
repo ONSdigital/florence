e3620b617c3a824c0ea3379d83abee8300c635f6
Crispioso <crispin.merriman@gmail.com>
2016-06-01 09:48:52 +0100

    Show unpublished collections in reports screen

--- src/main/web/florence/js/functions/_viewReportDetails.js
+++ src/main/web/florence/js/functions/_viewReportDetails.js
@@ -1,75 +1,92 @@
 function viewReportDetails(collection) {
+    var details, reportDetails, published;
 
-    if (!collection.publishResults || collection.publishResults.length === 0) {
-        return;
+    // Detect if we're loading a published collection
+    if (collection.publishEndDate || collection.publishStartDate || collection.publishResults) {
+        published = true;
     }
 
-    var success = collection.publishResults[collection.publishResults.length - 1];
-    var duration = (function () {
 
-        if (collection.publishStartDate && collection.publishEndDate) {
-            var start = new Date(collection.publishStartDate);
-            var end = new Date(collection.publishEndDate);
-        } else {
-            var start = new Date(success.transaction.startDate);
-            var end = new Date(success.transaction.endDate);
+    if (published) {
+        // Load details with published data
+
+        if (!collection.publishResults || collection.publishResults.length === 0) {
+            return;
         }
-        return end - start;
-    })();
+
+        var success = collection.publishResults[collection.publishResults.length - 1];
+        var duration = (function () {
+
+            if (collection.publishStartDate && collection.publishEndDate) {
+                var start = new Date(collection.publishStartDate);
+                var end = new Date(collection.publishEndDate);
+            } else {
+                var start = new Date(success.transaction.startDate);
+                var end = new Date(success.transaction.endDate);
+            }
+            return end - start;
+        })();
 
 
-    if (collection.publishStartDate) {
-        var starting = StringUtils.formatIsoFullSec(collection.publishStartDate);
+        if (collection.publishStartDate) {
+            var starting = StringUtils.formatIsoFullSec(collection.publishStartDate);
+        } else {
+            var starting = StringUtils.formatIsoFullSec(success.transaction.startDate);
+        }
+
+        var verifiedCount = collection.verifiedCount;
+        var verifyFailedCount = collection.verifyFailedCount;
+        var verifyInprogressCount = collection.verifyInprogressCount;
+        details = {
+            name: collection.name,
+            verifiedCount: verifiedCount,
+            verifyInprogressCount: verifyInprogressCount,
+            verifyFailedCount: verifyFailedCount,
+            date: collection.formattedDate,
+            starting: starting,
+            duration: duration,
+            success: success
+        };
+
+        reportDetails = templates.reportPublishedDetails(details);
     } else {
-        var starting = StringUtils.formatIsoFullSec(success.transaction.startDate);
+        // Load details with unpublished data
+
+        details = {
+            name: collection.name
+        };
+        reportDetails = templates.reportUnpublishedDetails(details);
     }
 
-    var verifiedCount = collection.verifiedCount;
-    var verifyFailedCount = collection.verifyFailedCount;
-    var verifyInprogressCount = collection.verifyInprogressCount;
-    var details = {
-        name: collection.name,
-        verifiedCount: verifiedCount,
-        verifyInprogressCount: verifyInprogressCount,
-        verifyFailedCount: verifyFailedCount,
-        date: collection.formattedDate,
-        starting: starting,
-        duration: duration,
-        success: success
-    };
-
-    var reportDetails = templates.reportDetails(details);
+    // Load handlebars into page and bind accordion events
     $('.publish-selected').html(reportDetails);
-    // $('.collections-accordion').accordion({
-    //     header: '.collections-section__head',
-    //     heightStyle: "content",
-    //     active: false,
-    //     collapsible: true
-    // });
     bindAccordions();
 
-    // order table
-    $('th').click(function () {
-        var table = $(this).parents('table').eq(0);
-        var rows = table.find('tr:gt(0)').toArray().sort(comparer($(this).index()));
-        this.asc = !this.asc;
-        if (!this.asc) {
-            rows = rows.reverse();
-        }
-        for (var i = 0; i < rows.length; i++) {
-            table.append(rows[i]);
-        }
-    });
+    // TODO fix the table ordering
+    // Bind table ordering functionality to publish times
+    if (published) {
+        $('.publish-times-table th').click(function () {
+            var table = $(this).parents('table').eq(0);
+            var rows = table.find('tr:gt(0)').toArray().sort(comparer($(this).index()));
+            this.asc = !this.asc;
+            if (!this.asc) {
+                rows = rows.reverse();
+            }
+            for (var i = 0; i < rows.length; i++) {
+                table.append(rows[i]);
+            }
+        });
 
-    function comparer(index) {
-        return function (a, b) {
-            var valA = getCellValue(a, index), valB = getCellValue(b, index);
-            return $.isNumeric(valA) && $.isNumeric(valB) ? valA - valB : valA.localeCompare(valB);
+        function comparer(index) {
+            return function (a, b) {
+                var valA = getCellValue(a, index), valB = getCellValue(b, index);
+                return $.isNumeric(valA) && $.isNumeric(valB) ? valA - valB : valA.localeCompare(valB);
+            }
         }
-    }
 
-    function getCellValue(row, index) {
-        return $(row).children('td').eq(index).html()
+        function getCellValue(row, index) {
+            return $(row).children('td').eq(index).html()
+        }
     }
 
     $('.publish-selected .btn-collection-cancel').click(function () {
