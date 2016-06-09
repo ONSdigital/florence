0e17c76ebeabc6c08f0883f8c4e1011f4ac01d7a
CarlHembrough <carlhuk@gmail.com>
2016-06-09 10:58:24 +0100

    Remove auto save from editor pages.

--- src/main/web/florence/js/functions/_t7QmiEditor.js
+++ src/main/web/florence/js/functions/_t7QmiEditor.js
@@ -3,7 +3,6 @@ function qmiEditor(collectionId, data) {
   var newFiles = [], newDocument = [], newDataset = [];
   var setActiveTab, getActiveTab;
   var renameUri = false;
-  var timeoutId;
 
   $(".edit-accordion").on('accordionactivate', function (event, ui) {
     setActiveTab = $(".edit-accordion").accordion("option", "active");
@@ -36,84 +35,44 @@ function qmiEditor(collectionId, data) {
   $("#contactName").on('input', function () {
     $(this).textareaAutoSize();
     data.description.contact.name = $(this).val();
-    clearTimeout(timeoutId);
-    timeoutId = setTimeout(function () {
-      autoSaveMetadata(collectionId, data);
-    }, 3000);
   });
   $("#contactEmail").on('input', function () {
     $(this).textareaAutoSize();
     data.description.contact.email = $(this).val();
-    clearTimeout(timeoutId);
-    timeoutId = setTimeout(function () {
-      autoSaveMetadata(collectionId, data);
-    }, 3000);
   });
   $("#contactTelephone").on('input', function () {
     $(this).textareaAutoSize();
     data.description.contact.telephone = $(this).val();
-    clearTimeout(timeoutId);
-    timeoutId = setTimeout(function () {
-      autoSaveMetadata(collectionId, data);
-    }, 3000);
   });
   $("#survey").on('input', function () {
     $(this).textareaAutoSize();
     data.description.surveyName = $(this).val();
-    clearTimeout(timeoutId);
-    timeoutId = setTimeout(function () {
-      autoSaveMetadata(collectionId, data);
-    }, 3000);
   });
   $("#frequency").on('input', function () {
     $(this).textareaAutoSize();
     data.description.frequency = $(this).val();
-    clearTimeout(timeoutId);
-    timeoutId = setTimeout(function () {
-      autoSaveMetadata(collectionId, data);
-    }, 3000);
   });
   $("#compilation").on('input', function () {
     $(this).textareaAutoSize();
     data.description.compilation = $(this).val();
-    clearTimeout(timeoutId);
-    timeoutId = setTimeout(function () {
-      autoSaveMetadata(collectionId, data);
-    }, 3000);
   });
   $("#geoCoverage").on('input', function () {
     $(this).textareaAutoSize();
     data.description.geographicCoverage = $(this).val();
-    clearTimeout(timeoutId);
-    timeoutId = setTimeout(function () {
-      autoSaveMetadata(collectionId, data);
-    }, 3000);
   });
   $("#sampleSize").on('input', function () {
     $(this).textareaAutoSize();
     data.description.sampleSize = $(this).val();
-    clearTimeout(timeoutId);
-    timeoutId = setTimeout(function () {
-      autoSaveMetadata(collectionId, data);
-    }, 3000);
   });
   if (!data.description.lastRevised) {
     $('#lastRevised').datepicker({dateFormat: 'dd MM yy'}).on('change', function () {
       data.description.lastRevised = new Date($(this).datepicker({dateFormat: 'dd MM yy'})[0].value).toISOString();
-      clearTimeout(timeoutId);
-      timeoutId = setTimeout(function () {
-        autoSaveMetadata(collectionId, data);
-      }, 3000);
     });
   } else {
     dateTmp = data.description.lastRevised;
     var dateTmpFormatted = $.datepicker.formatDate('dd MM yy', new Date(dateTmp));
     $('#lastRevised').val(dateTmpFormatted).datepicker({dateFormat: 'dd MM yy'}).on('change', function () {
       data.description.lastRevised = new Date($('#lastRevised').datepicker('getDate')).toISOString();
-      clearTimeout(timeoutId);
-      timeoutId = setTimeout(function () {
-        autoSaveMetadata(collectionId, data);
-      }, 3000);
     });
   }
   $("#keywordsTag").tagit({
@@ -124,18 +83,10 @@ function qmiEditor(collectionId, data) {
   });
   $('#keywords').on('change', function () {
     data.description.keywords = $('#keywords').val().split(',');
-    clearTimeout(timeoutId);
-    timeoutId = setTimeout(function () {
-      autoSaveMetadata(collectionId, data);
-    }, 3000);
   });
   $("#metaDescription").on('input', function () {
     $(this).textareaAutoSize();
     data.description.metaDescription = $(this).val();
-    clearTimeout(timeoutId);
-    timeoutId = setTimeout(function () {
-      autoSaveMetadata(collectionId, data);
-    }, 3000);
   });
 
   /* The checked attribute is a boolean attribute, which means the corresponding property is true if the attribute
@@ -149,10 +100,6 @@ function qmiEditor(collectionId, data) {
 
   $("#metadata-list input[type='checkbox']").prop('checked', checkBoxStatus).click(function () {
     data.description.nationalStatistic = $("#metadata-list input[type='checkbox']").prop('checked') ? true : false;
-    clearTimeout(timeoutId);
-    timeoutId = setTimeout(function () {
-      autoSaveMetadata(collectionId, data);
-    }, 3000);
   });
 
   // Save
@@ -174,7 +121,6 @@ function qmiEditor(collectionId, data) {
   });
 
   function save(onSave) {
-    clearTimeout(timeoutId);
     // Sections
     data.markdown = [$('#content-markdown').val()];
     // Files are uploaded. Save metadata
--- src/main/web/florence/js/functions/_t7StaticArticleEditor.js
+++ src/main/web/florence/js/functions/_t7StaticArticleEditor.js
@@ -3,7 +3,6 @@ function staticArticleEditor(collectionId, data) {
   var newSections = [], newChart = [], newTable = [], newImage = [], newLinks = [], newFiles = [];
   var setActiveTab, getActiveTab;
   var renameUri = false;
-  var timeoutId;
 
   $(".edit-accordion").on('accordionactivate', function (event, ui) {
     setActiveTab = $(".edit-accordion").accordion("option", "active");
@@ -26,10 +25,6 @@ function staticArticleEditor(collectionId, data) {
   if (!data.description.releaseDate) {
     $('#releaseDate').datepicker({dateFormat: 'dd MM yy'}).on('change', function () {
       data.description.releaseDate = new Date($(this).datepicker({dateFormat: 'dd MM yy'})[0].value).toISOString();
-      clearTimeout(timeoutId);
-      timeoutId = setTimeout(function () {
-        autoSaveMetadata(collectionId, data);
-      }, 3000);
     });
   } else {
     //dateTmp = $('#releaseDate').val();
@@ -37,10 +32,6 @@ function staticArticleEditor(collectionId, data) {
     var dateTmpFormatted = $.datepicker.formatDate('dd MM yy', new Date(dateTmp));
     $('#releaseDate').val(dateTmpFormatted).datepicker({dateFormat: 'dd MM yy'}).on('change', function () {
       data.description.releaseDate = new Date($('#releaseDate').datepicker('getDate')).toISOString();
-      clearTimeout(timeoutId);
-      timeoutId = setTimeout(function () {
-        autoSaveMetadata(collectionId, data);
-      }, 3000);
     });
   }
   //} else {
@@ -49,10 +40,6 @@ function staticArticleEditor(collectionId, data) {
   $("#summary").on('input', function () {
     $(this).textareaAutoSize();
     data.description.summary = $(this).val();
-    clearTimeout(timeoutId);
-    timeoutId = setTimeout(function () {
-      autoSaveMetadata(collectionId, data);
-    }, 3000);
   });
   $("#keywordsTag").tagit({
     availableTags: data.description.keywords,
@@ -62,18 +49,10 @@ function staticArticleEditor(collectionId, data) {
   });
   $('#keywords').on('change', function () {
     data.description.keywords = $('#keywords').val().split(',');
-    clearTimeout(timeoutId);
-    timeoutId = setTimeout(function () {
-      autoSaveMetadata(collectionId, data);
-    }, 3000);
   });
   $("#metaDescription").on('input', function () {
     $(this).textareaAutoSize();
     data.description.metaDescription = $(this).val();
-    clearTimeout(timeoutId);
-    timeoutId = setTimeout(function () {
-      autoSaveMetadata(collectionId, data);
-    }, 3000);
   });
 
   // Save
@@ -95,7 +74,6 @@ function staticArticleEditor(collectionId, data) {
   });
 
   function save(onSave) {
-    clearTimeout(timeoutId);
     // Sections
     var orderSection = $("#sortable-section").sortable('toArray');
     $(orderSection).each(function (indexS, nameS) {
