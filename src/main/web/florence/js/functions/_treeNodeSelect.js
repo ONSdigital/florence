38883765308babd66b6291e6e4dfa0104333dcc6
Crispioso <crispin.merriman@gmail.com>
2016-07-26 09:50:28 +0100

    Initial commit of decoupled js and CSS classes and removed silent classing
    Former-commit-id: 290b3c20599043da5f332b0ac5fbfa99b9aa5f27

--- src/main/web/florence/js/functions/_treeNodeSelect.js
+++ src/main/web/florence/js/functions/_treeNodeSelect.js
@@ -1,8 +1,18 @@
 function treeNodeSelect(url) {
     var urlPart = url.replace(Florence.babbageBaseUrl, '');
     var $selectedListItem = $('[data-url="' + urlPart + '"]'); //get first li with data-url with url
-    $('.page-list li').removeClass('selected');
+    $('.js-browse__item.selected').removeClass('selected');
     $selectedListItem.addClass('selected');
+
+    // Hide container for item and buttons for previous and show selected one
+    $('.page-container.selected').removeClass('selected');
+    $selectedListItem.find('.page-container:first').addClass('selected');
+
+    // Hide previous displayed page buttons and show selected one
+    if ($selectedListItem.find('.page-options:first')) {
+        $('.page-options.selected').removeClass('selected');
+        $selectedListItem.find('.page-options:first').addClass('selected');
+    }
 
     //page-list-tree
     $('.tree-nav-holder ul').removeClass('active');
