428678c2aac22a472cb237f0f73564c874d00b96
Crispioso <crispin.merriman@gmail.com>
2016-07-26 13:19:10 +0100

    Fix indentation for children items in browse tree

--- src/main/web/florence/js/functions/_treeNodeSelect.js
+++ src/main/web/florence/js/functions/_treeNodeSelect.js
@@ -5,13 +5,13 @@ function treeNodeSelect(url) {
     $selectedListItem.addClass('selected');
 
     // Hide container for item and buttons for previous and show selected one
-    $('.page-container.selected').removeClass('selected');
-    $selectedListItem.find('.page-container:first').addClass('selected');
+    $('.page__container.selected').removeClass('selected');
+    $selectedListItem.find('.page__container:first').addClass('selected');
 
     // Hide previous displayed page buttons and show selected one
-    if ($selectedListItem.find('.page-options:first')) {
-        $('.page-options.selected').removeClass('selected');
-        $selectedListItem.find('.page-options:first').addClass('selected');
+    if ($selectedListItem.find('.page__buttons:first')) {
+        $('.page__buttons.selected').removeClass('selected');
+        $selectedListItem.find('.page__buttons:first').addClass('selected');
     }
 
     //page-list-tree
