08f08bea9a340125796c00bc6ccb4693a1e2c33c
Jon Dewi Jones <jondewijones@gmail.com>
2016-09-22 11:36:21 +0100

    Render team details on creation of new team

--- src/main/web/florence/js/functions/_postTeam.js
+++ src/main/web/florence/js/functions/_postTeam.js
@@ -1,68 +1,77 @@
 function postTeam(name) {
 
-  var encodedName = encodeURIComponent(name);
-  $.ajax({
-    url: "/zebedee/teams/" + encodedName,
-    dataType: 'json',
-    contentType: 'application/json',
-    type: 'POST',
-    data: JSON.stringify({
-      name: encodedName
-    }),
-    success: function () {
-      viewTeams();
-    },
-    error: function (response) {
-      handleUserPostError(response);
-    }
-  });
+    var encodedName = encodeURIComponent(name);
+    $.ajax({
+        url: "/zebedee/teams/" + encodedName,
+        dataType: 'json',
+        contentType: 'application/json',
+        type: 'POST',
+        data: JSON.stringify({
+            name: encodedName
+        }),
+        success: function () {
+            viewTeams();
 
-  /**
-   * Handle error response from creating the team.
-   * @param response
-   */
-  function handleUserPostError(response) {
-    if (response.status === 403 || response.status === 401) {
-      sweetAlert("You are not permitted to create teams.");
+            // use time out to give browser time to render. this needs a fix
+            setTimeout(function(){
+                // find the new team in teams list and add select class
+                var select = $("table").find("[data-id='" + name + "']");
+
+                viewTeamDetails(name, select);
+             }, 5);
+
+        },
+        error: function (response) {
+            handleUserPostError(response);
+        }
+    });
+
+    /**
+     * Handle error response from creating the team.
+     * @param response
+     */
+    function handleUserPostError(response) {
+        if (response.status === 403 || response.status === 401) {
+            sweetAlert("You are not permitted to create teams.");
+        }
+        else if (response.status === 409) {
+            sweetAlert("Error", response.responseJSON.message, "error");
+        } else {
+            handleApiError(response);
+        }
     }
-    else if (response.status === 409) {
-      sweetAlert("Error", response.responseJSON.message, "error");
-    } else {
-      handleApiError(response);
-    }
-  }
 }
 
 function postTeamMember(name, email) {
-  var encodedName = encodeURIComponent(name);
-  $.ajax({
-    url: "/zebedee/teams/" + encodedName + "?email=" + email,
-    dataType: 'json',
-    contentType: 'application/json',
-    type: 'POST',
-    data: JSON.stringify({
-      name: email
-    }),
-    success: function () {
-      console.log('Team member added: ' + email);
-    },
-    error: function (response) {
-      handleUserPostError(response);
-    }
-  });
+    var encodedName = encodeURIComponent(name);
+    $.ajax({
+        url: "/zebedee/teams/" + encodedName + "?email=" + email,
+        dataType: 'json',
+        contentType: 'application/json',
+        type: 'POST',
+        data: JSON.stringify({
+            name: email
+        }),
+        success: function () {
+            console.log('Team member added: ' + email);
+        },
+        error: function (response) {
+            handleUserPostError(response);
+        }
+    });
 
-  /**
-   * Handle error response from creating the team.
-   * @param response
-   */
-  function handleUserPostError(response) {
-    if (response.status === 403 || response.status === 401) {
-      sweetAlert("You are not permitted to add users.");
+    /**
+     * Handle error response from creating the team.
+     * @param response
+     */
+    function handleUserPostError(response) {
+        if (response.status === 403 || response.status === 401) {
+            sweetAlert("You are not permitted to add users.");
+        }
+        else if (response.status === 409) {
+            sweetAlert("Error", response.responseJSON.message, "error");
+        } else {
+            handleApiError(response);
+        }
     }
-    else if (response.status === 409) {
-      sweetAlert("Error", response.responseJSON.message, "error");
-    } else {
-      handleApiError(response);
-    }
-  }
 }
