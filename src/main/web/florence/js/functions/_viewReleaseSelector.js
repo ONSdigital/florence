47d1d8a97e22f1bb72dcfdcffc9ad73896257154
Crispioso <crispin.merriman@gmail.com>
2016-10-06 11:24:03 +0100

    Remove stubbed data and redundant release selector template

--- src/main/web/florence/js/functions/_viewReleaseSelector.js
+++ src/main/web/florence/js/functions/_viewReleaseSelector.js
@@ -3,8 +3,6 @@
  */
 function viewReleaseSelector() {
 
-    var stubbedData = {"type":"list","listType":"releasecalendar","uri":"/releasecalendar/data","result":{"numberOfResults":489,"took":25,"results":[{"_type":"release","description":{"summary":"Inputs, output and productivity estimates for public services in the UK between 1997 and 2012 and new estimates for 2013.","nextRelease":"","releaseDate":"2016-02-18T09:30:00.000Z","finalised":true,"source":"","published":false,"title":"UK total public services, productivity estimates: 2013","nationalStatistic":true,"unit":"","contact":{"name":"General and statistical enquiries","telephone":"0845 601 3034","email":"info@ons.gsi.gov.uk"},"provisionalDate":"","cancelled":false,"preUnit":"","cancellationNotice":[]},"searchBoost":[],"type":"release","uri":"/releases/uktotalpublicservicesproductivityestimates2013"},{"_type":"release","description":{"summary":"","nextRelease":"","releaseDate":"2016-03-15T09:30:00.000Z","finalised":true,"source":"","published":false,"title":"Consumer price inflation basket of goods and services: 2016","nationalStatistic":true,"unit":"","contact":{"name":"","telephone":"","email":""},"provisionalDate":"","cancelled":false,"preUnit":"","cancellationNotice":[]},"searchBoost":[],"type":"release","uri":"/releases/consumerpriceinflationbasketofgoodsandservices2016"},{"_type":"release","description":{"summary":"The number of deaths registered in England and Wales in the latest eight weeks for which data are available.","nextRelease":"","releaseDate":"2016-03-15T09:30:00.000Z","finalised":true,"source":"","published":false,"title":"Deaths registered in England and Wales, weekly provisional: ending 4 March 2016","nationalStatistic":true,"unit":"","contact":{"name":"General and statistical enquiries","telephone":"0845 601 3034","email":"info@ons.gsi.gov.uk"},"provisionalDate":"","cancelled":false,"preUnit":"","cancellationNotice":[]},"searchBoost":[],"type":"release","uri":"/releases/deathsregisteredinenglandandwalesweeklyprovisionalending4march2016"},{"_type":"release","description":{"summary":"The value of UK exports and imports of goods, grouped by product.","nextRelease":"","releaseDate":"2016-03-16T09:30:00.000Z","finalised":true,"source":"","published":false,"title":"UK trade in goods by classification of product by activity (CPA 08): Oct to Dec 2015","nationalStatistic":false,"unit":"","contact":{"name":"General and statistical enquiries","telephone":"0845 601 3034","email":"info@ons.gsi.gov.uk"},"provisionalDate":"","cancelled":false,"preUnit":"","cancellationNotice":[]},"searchBoost":[],"type":"release","uri":"/releases/uktradeingoodsbyclassificationofproductbyactivitycpa08octtodec2015"},{"_type":"release","description":{"summary":"Employment, unemployment, inactivity, jobs and the Claimant Count for regions, local authorities and parliamentary constituencies.","nextRelease":"","releaseDate":"2016-03-16T09:30:00.000Z","finalised":true,"source":"","published":false,"title":"Regional labour market statistics: March 2016","nationalStatistic":true,"unit":"","contact":{"name":"General and statistical enquiries","telephone":"0845 601 3034","email":"info@ons.gsi.gov.uk"},"provisionalDate":"","cancelled":false,"preUnit":"","cancellationNotice":[]},"searchBoost":[],"type":"release","uri":"/releases/regionallabourmarketstatisticsmarch2016"},{"_type":"release","description":{"summary":"The release includes employment, unemployment, economic inactivity, claimant count, average earnings, vacancies and labour disputes statistics.","nextRelease":"","releaseDate":"2016-03-16T09:30:00.000Z","finalised":true,"source":"","published":false,"title":"Labour market statistics: March 2016","nationalStatistic":true,"unit":"","contact":{"name":"General and statistical enquiries","telephone":"0845 601 3034","email":"info@ons.gsi.gov.uk"},"provisionalDate":"","cancelled":false,"preUnit":"","cancellationNotice":[]},"searchBoost":[],"type":"release","uri":"/releases/labourmarketstatisticsmarch2016"},{"_type":"release","description":{"summary":"Quarterly comparisons on a headcount and full-time equivalent basis, by sector, industry and region. Made up of central and local government and public corporations, such as the NHS and civil service.","nextRelease":"","releaseDate":"2016-03-16T09:30:00.000Z","finalised":true,"source":"","published":false,"title":"Public sector employment, UK: Oct to Dec 2015","nationalStatistic":true,"unit":"","contact":{"name":"General and statistical enquiries","telephone":"0845 601 3034","email":"info@ons.gsi.gov.uk"},"provisionalDate":"","cancelled":false,"preUnit":"","cancellationNotice":[]},"searchBoost":[],"type":"release","uri":"/releases/publicsectoremploymentukocttodec2015"},{"_type":"release","description":{"summary":"Investment choices of insurance companies, self-administered pension funds, investment trusts, unit trusts and property unit trusts. Includes quarterly balance sheet data for short-term assets and liabilities, quarterly income and expenditure data for insurance companies and self-administered pension funds.","nextRelease":"","releaseDate":"2016-03-17T09:30:00.000Z","finalised":true,"source":"","published":false,"title":"Investment by insurance companies, pension funds and trusts in the UK (MQ5): Oct to Dec 2015","nationalStatistic":true,"unit":"","contact":{"name":"General and statistical enquiries","telephone":"0845 601 3034","email":"info@ons.gsi.gov.uk"},"provisionalDate":"","cancelled":false,"preUnit":"","cancellationNotice":[]},"searchBoost":[],"type":"release","uri":"/releases/investmentbyinsurancecompaniespensionfundsandtrustsintheukmq5octtodec2015"},{"_type":"release","description":{"summary":"","nextRelease":"","releaseDate":"2016-03-17T09:30:00.000Z","finalised":true,"source":"","published":false,"title":"Analysis of the UK labour market - estimates of skills mismatch using measures of over and under education: 2015","nationalStatistic":false,"unit":"","contact":{"name":"General and statistical enquiries","telephone":"0845 601 3034","email":"info@ons.gsi.gov.uk"},"provisionalDate":"","cancelled":false,"preUnit":"","cancellationNotice":[]},"searchBoost":[],"type":"release","uri":"/releases/analysisoftheuklabourmarketestimatesofskillsmismatchusingmeasuresofoverandundereducation2015"},{"_type":"release","description":{"summary":"","nextRelease":"","releaseDate":"2016-03-18T09:30:00.000Z","finalised":true,"source":"","published":false,"title":"Towns and Cities Analysis","nationalStatistic":false,"unit":"","contact":{"name":"","telephone":"","email":""},"provisionalDate":"","cancelled":false,"preUnit":"","cancellationNotice":[]},"searchBoost":[],"type":"release","uri":"/releases/townsandcitiesanalysis"}],"suggestions":[],"docCounts":{},"paginator":{"numberOfPages":49,"currentPage":1,"start":1,"end":5,"pages":[1,2,3,4,5]},"sortBy":"release_date_asc"}};
-
     var templateData = {
         columns: ["Calendar entry", "Calendar entry date"],
         title: "Select a calendar entry"
@@ -18,27 +16,19 @@ function viewReleaseSelector() {
     var releases = [];
     PopulateReleasesForUri("/releasecalendar/data?view=upcoming", releases);
 
-    // var $searchInput = $('#release-search-input');
-    // $searchInput.focus();
-    // $searchInput.on('input', function () {
-    //     var searchText = $(this).val();
-    //     populateReleasesList(releases, searchText);
-    // });
-
     function PopulateReleasesForUri(baseReleaseUri, releases) {
-        //console.log("populating release for uri " + baseReleaseUri);
-        // $.ajax({
-        //         url: baseReleaseUri,
-        //         type: "get",
-        //         success: function (data) {
-        //             populateRemainingReleasePages(data, releases, baseReleaseUri);
-        //         },
-        //         error: function (response) {
-        //             handleApiError(response);
-        //         }
-        //     }
-        // );
-        populateRemainingReleasePages(stubbedData, releases, baseReleaseUri)
+        console.log("populating release for uri " + baseReleaseUri);
+        $.ajax({
+                url: baseReleaseUri,
+                type: "get",
+                success: function (data) {
+                    populateRemainingReleasePages(data, releases, baseReleaseUri);
+                },
+                error: function (response) {
+                    handleApiError(response);
+                }
+            }
+        );
     }
 
     /**
