02da1f07a2712730dd7fd536160c6aadfc02181f
Crispioso <crispin.merriman@gmail.com>
2016-10-06 10:44:31 +0100

    Enable search on select modal

--- src/main/web/florence/js/components/_viewSelectorModal.js
+++ src/main/web/florence/js/components/_viewSelectorModal.js
@@ -3,11 +3,10 @@
  */
 
 
-function viewSelectModal(templateData, tableBodyData, onItemSelect) {
+function viewSelectModal(templateData, onSearch) {
     var modalHtml = templates.selectorModal(templateData);
     $('body').append(modalHtml);
     bindSelectModalEvents();
-    buildTableBody();
 
     function bindSelectModalEvents() {
         var $modal = $('#js-modal-select');
@@ -16,37 +15,41 @@ function viewSelectModal(templateData, tableBodyData, onItemSelect) {
             $modal.remove();
         });
 
-        $modal.find('#js-modal-select__body').on('click', 'tr', function() {
-            onItemSelect($(this).index());
-            $modal.remove();
+        $modal.find('#js-modal-select__search').on('input', function() {
+            onSearch($(this).val());
         });
 
+        // $modal.find('#js-modal-select__body').on('click', 'tr', function() {
+        //     onItemSelect($(this).index());
+        //     $modal.remove();
+        // });
+
     }
 
-    function buildTableBody() {
-        var tableBodyHtml = function() {
-            var i,
-                tableBodyDataLength = tableBodyData.length,
-                html = [];
-
-            // Loop through each row object
-            for (i = 0; i < tableBodyDataLength; i++) {
-                var tableRow = [];
-
-                // Loop through each property in row object and wrap in <td> element
-                for (var property in tableBodyData[i]) {
-                    if(!tableBodyData[i].hasOwnProperty(property)) continue;
-                    tableRow.push("<td>" + tableBodyData[i][property] + "</td>");
-                }
-
-                // Join together <td>s, wrap in <tr> and push into HTML variable
-                html.push("<tr>" + tableRow.join() + "</tr>");
-            }
-
-            // Return string of table body HTML
-            return html.join();
-        };
-
-        $('#js-modal-select__body').html(tableBodyHtml());
-    }
+    // function buildTableBody() {
+    //     var tableBodyHtml = function() {
+    //         var i,
+    //             tableBodyDataLength = tableBodyData.length,
+    //             html = [];
+    //
+    //         // Loop through each row object
+    //         for (i = 0; i < tableBodyDataLength; i++) {
+    //             var tableRow = [];
+    //
+    //             // Loop through each property in row object and wrap in <td> element
+    //             for (var property in tableBodyData[i]) {
+    //                 if(!tableBodyData[i].hasOwnProperty(property)) continue;
+    //                 tableRow.push("<td>" + tableBodyData[i][property] + "</td>");
+    //             }
+    //
+    //             // Join together <td>s, wrap in <tr> and push into HTML variable
+    //             html.push("<tr>" + tableRow.join() + "</tr>");
+    //         }
+    //
+    //         // Return string of table body HTML
+    //         return html.join();
+    //     };
+    //
+    //     $('#js-modal-select__body').html(tableBodyHtml());
+    // }
 }
