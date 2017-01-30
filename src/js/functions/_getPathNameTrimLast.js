2f0a24d3b2eadf1201721385d49793a11271397a
Crispioso <crispin.merriman@gmail.com>
2017-01-30 13:58:19 +0000

    Remove legacy files and use CDN assets instead of local

--- src/js/functions/_getPathNameTrimLast.js
+++
@@ -1,9 +0,0 @@
-function getPathNameTrimLast() {
-  var parsedUrl = document.getElementById('iframe').contentWindow.location.pathname;
-
-  if (parsedUrl.charAt(parsedUrl.length-1) === '/') {
-    parsedUrl = parsedUrl.slice(0, -1);
-  }
-  return parsedUrl;
-}
-
