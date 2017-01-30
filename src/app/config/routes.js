aa024888dd08d7771fd12968a47549d0d5e34d46
Jon Dewi Jones <jondewijones@gmail.com>
2017-01-30 17:07:44 +0000

    Get react app working and using simple routing

---
+++ src/app/config/routes.js
@@ -0,0 +1,8 @@
+import Collections from '../collections/Collections';
+
+export default [
+    { path: "/florence", component: Collections },
+    // { path: 'login', component: Login },
+    // { path: '/collections', component: Collections },
+    // { path: '/collections/:id', component: CollectionDetails }
+]
