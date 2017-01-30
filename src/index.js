7159a7dfbe98e950459fa34d13edb7deed4a64f7
Crispioso <crispin.merriman@gmail.com>
2017-01-30 17:11:43 +0000

    Update build to include CSS and other files
    Former-commit-id: d3b5b8add91a8d8313e53b162a1c2760d14372f8
    Former-commit-id: 4c4d96dae810fe7538204b165ff716e350e5e6a8
    Former-commit-id: ffd69207ce93224014b04353c96e66a9e7cc45d5

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM, { render } from 'react-dom';
 
+import './scss/main.scss';
+
 import App from './app/App';
 
 ReactDOM.render(React.createElement(App), document.getElementById('app'));
