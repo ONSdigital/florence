9c11d125edcecffe9cd46c479dc0332829faf74d
Crispioso <crispin.merriman@gmail.com>
2017-01-30 17:11:43 +0000

    Update build to include CSS and other files

--- src/webpack.config.js
+++ src/webpack.config.js
@@ -1,5 +1,7 @@
 const path = require('path');
 const webpack = require('webpack');
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = {
     context: path.resolve(__dirname),
@@ -8,27 +10,46 @@ module.exports = {
     },
     output: {
         path: path.resolve(__dirname, './dist'),
-        filename: 'florence.bundle.js',
+        filename: 'js/florence.bundle.js',
     },
     module: {
-        loaders: [{
-            test: /\.js$/,
-            exclude: /node_modules/,
-            loader: 'babel-loader',
-            query: {
-                presets: ['es2015']
+        loaders: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                loader: 'babel-loader',
+                query: {
+                    presets: ['es2015']
+                }
+            },
+            {
+                test: /\.jsx$/,
+                exclude: /node_modules/,
+                loader: 'babel-loader',
+                query: {
+                    presets: ['react', 'es2015']
+                }
+            },
+            {
+                test: /\.scss$/,
+                loader: ExtractTextPlugin.extract({ fallbackLoader: "style-loader", loader: "css-loader!sass-loader"})
+            },
+            {
+                test: /\.(png|woff|woff2|eot|ttf|svg)$/,
+                loader: 'url-loader?limit=100000'
             }
-        },{
-            test: /\.jsx$/,
-            exclude: /node_modules/,
-            loader: 'babel-loader',
-            query: {
-                presets: ['react', 'es2015']
-            }
-        }]
+        ]
     },
     resolve: {
         // implicitly tell babel to load jsx
         extensions: ['.js', '.jsx']
-    }
+    },
+    plugins: [
+        new CopyWebpackPlugin([
+            { from: 'manifest.json', to: 'manifest.json' },
+            { from: 'service-worker.js', to: 'service-worker.js' },
+            { from: 'img', to: 'img' }
+        ]),
+        new ExtractTextPlugin("css/main.css")
+    ]
 };
