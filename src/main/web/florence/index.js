0a2c76a4580783603c3151eecc52d023a49ec9ba
Jon Dewi Jones <jondewijones@gmail.com>
2017-01-30 12:32:22 +0000

    Get webpack working

---
+++ src/main/web/florence/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM, { render } from 'react-dom';
+
+import App from './src/App';
+
+ReactDOM.render(React.createElement(App), document.getElementById('app'));
+
+
