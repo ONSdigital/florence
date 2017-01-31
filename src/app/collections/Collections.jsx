faf7df676ac99681f830105c6870ead5af52b3f2
Jon Dewi Jones <jondewijones@gmail.com>
2017-01-31 11:49:55 +0000

    Add global layout element for main nav bar
    Former-commit-id: 5485da77d30fcf0789c4e7ed68dcf9fa3cec8dfb
    Former-commit-id: fdf5bf8054577c24f7275ea662ecc178aba02bec
    Former-commit-id: 3fbe4901e2ecb6188084abbad4a73ab2ab1b40cd

--- src/app/collections/Collections.jsx
+++ src/app/collections/Collections.jsx
@@ -1,13 +1,20 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux'
 
-export default class Collections extends Component {
+import Layout from '../global/Layout'
+
+class Collections extends Component {
     constructor(props) {
         super(props);
     }
 
     render () {
         return (
-            <div>This is collections list</div>
+            <Layout >
+                <div>This is collections list</div>
+            </Layout>
         )
     }
 }
+
+export default connect()(Collections);
