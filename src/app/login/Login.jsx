fb67901b2f8e65f8c1809e9e7915717693046eb7
Jon Dewi Jones <jondewijones@gmail.com>
2017-02-02 10:34:31 +0000

    Add login form and set cookie functionality
    Former-commit-id: fef22bf27286c177022e74155b6dcb5b4ad5091f
    Former-commit-id: db0c2838cf6268459a99f3313db3f7f4b37d40bb
    Former-commit-id: 239ee8594ae5280a24b54f7a066207ed023dae5b

--- src/app/login/Login.jsx
+++ src/app/login/Login.jsx
@@ -1,20 +1,73 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux'
 
-import Layout from '../global/Layout';
+import { post } from '../utilities/post'
 
 class Login extends Component {
     constructor(props) {
         super(props);
+
+        this.state = {
+            email: "",
+            password: ""
+        };
+
+        this.handleSubmit = this.handleSubmit.bind(this);
+        this.handleEmailChange = this.handleEmailChange.bind(this);
+        this.handlePasswordChange = this.handlePasswordChange.bind(this);
+
+    }
+
+    handleSubmit(event) {
+        event.preventDefault();
+
+        const postBody = {
+            email: this.state.email,
+            password: this.state.password
+        };
+
+        function setAccessTokenCookie(accessToken) {
+            document.cookie = "access_token=" + accessToken+ ";path=/";
+            console.log(`Cookie: access_token: ${accessToken} set`);
+            // TODO update state authentication property
+        }
+
+        return post('/zebedee/login', postBody)
+            .then(function(accessToken) {
+                setAccessTokenCookie(accessToken);
+            }).catch(function(error) {
+                return error;
+            });
+
+
+    }
+
+
+    handleEmailChange(event) {
+        this.setState({email: event.target.value});
+    }
+
+    handlePasswordChange(event) {
+        this.setState({password: event.target.value});
     }
 
     render() {
         return (
-            <Layout >
-                <div>This is login</div>
-            </Layout>
+            <div className="col--4 login-wrapper">
+                <h1>Login</h1>
+
+                <form onSubmit={this.handleSubmit}>
+                    <label htmlFor="email">Email:</label>
+                    <input id="email" type="email" className="fl-user-and-access__email" name="email" cols="40" rows="1" onChange={this.handleEmailChange}/>
+
+                    <label htmlFor="password">Password:</label>
+                    <input id="password" type="password" className="fl-user-and-access__password" name="password" cols="40" rows="1" onChange={this.handlePasswordChange}/>
+
+                    <button type="submit" id="login" className="btn btn--primary margin-left--0 btn-florence-login fl-panel--user-and-access__login ">Log in</button>
+                </form>
+            </div>
         )
     }
 }
 
-export default connect()(Layout);
+export default connect()(Login);
