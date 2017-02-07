8b7cccc9bff9659b106b14dea04811a7f446556c
Crispioso <crispin.merriman@gmail.com>
2017-02-07 12:56:01 +0000

    Redirect on failed authentication to login page

--- src/app/config/store.js
+++ src/app/config/store.js
@@ -1,13 +1,11 @@
-import { createStore } from 'redux';
+// const initialState = {
+//     hello: "hello"
+// };
+//
+// export default function reducer(state = initialState, action) {
+//
+// }
 
-const initialState = {
-    hello: "hello"
-};
-
-function reducer(state = initialState, action) {
-
-}
-
-let store = createStore(reducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
-
-export default store;
+// let store = createStore(reducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+//
+// export default store;
