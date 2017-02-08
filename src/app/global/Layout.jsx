a23509c2f0b39d4c303dfdeeb24fbe15bd2744f7
Crispioso <crispin.merriman@gmail.com>
2017-02-08 15:37:51 +0000

    Redirect on successful login

--- src/app/global/Layout.jsx
+++ src/app/global/Layout.jsx
@@ -3,10 +3,6 @@ import React, { Component } from 'react';
 import NavBar from './NavBar';
 
 export default class Layout extends Component {
-    constructor(props) {
-        super(props)
-    }
-
     render() {
         return (
             <div>
