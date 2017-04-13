2582a5c8cb6b28fa0f96064c00faa8aa10470b29
Crispioso <crispin.merriman@gmail.com>
2017-04-13 14:40:54 +0100

    Remove rogue extra JS file from webpack build
    Former-commit-id: db17f7cfbf1018918ae5b0c62affa0ab2a095028
    Former-commit-id: 5239b897768d151993e8106273bb8b64d6da0704
    Former-commit-id: 54a1f51b209288d86f83a0e34f1a139d5535a8fd

--- src/webpack.config.js
+++ src/webpack.config.js
@@ -10,7 +10,7 @@ module.exports = {
     },
     output: {
         path: path.resolve(__dirname, '../dist'),
-        filename: 'js/[name].bundle.js',
+        filename: 'js/florence.bundle.js',
     },
     module: {
         loaders: [
