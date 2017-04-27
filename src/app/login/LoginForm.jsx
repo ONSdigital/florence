1d25dac35d0262b4c8c3ac7144c191ff98920f4f
Jon Dewi Jones <jondewijones@gmail.com>
2017-04-27 15:24:00 +0100

    Add login form renderer
    Former-commit-id: 25742e7eb2f155289b320270f1cb66347848b5c0
    Former-commit-id: 78142f79f479dbe3278720deaf6cebec2bc35ea3
    Former-commit-id: 1e8ba4f8fb119e32a6c6ae2e68e9aab3c6b6735d

---
+++ src/app/login/LoginForm.jsx
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+import Input from '../components/Input'
+
+
+export default class LoginForm extends Component {
+
+    constructor(props) {
+        super(props);
+
+    }
+
+    render() {
+        const inputs = this.props.formData.inputs;
+
+        return (
+            <div className="grid grid--justify-center">
+                <div className="grid__col-4">
+                    <h1>Login</h1>
+
+                    <form className="form" onSubmit={this.props.formData.onSubmit}>
+
+                        {
+                            inputs.map((input, index) => {
+                                let error = this.props.formData.error.inputID === input.id ? this.props.formData.error.message : false;
+                                return <Input key={index} {...input} error={error} />
+                            })
+                        }
+
+                        <button type="submit" className="btn btn--primary margin-top--1">Log in</button>
+                    </form>
+                </div>
+            </div>
+        )
+    }
+}
