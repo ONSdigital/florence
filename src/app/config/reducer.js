c0f482906fe415f8bc14e370ace444d5a088325b
Crispioso <crispin.merriman@gmail.com>
2017-05-02 08:41:34 +0100

    Fix initialState import syntax

--- src/app/config/reducer.js
+++ src/app/config/reducer.js
@@ -1,4 +1,4 @@
-import initialState from './initialState';
+import { initialState } from './initialState';
 
 export default function reducer(state = initialState, action) {
     switch (action.type) {
