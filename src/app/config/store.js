bfa45e305b433eb7a7e48ce2512176bc9b81183d
Crispioso <crispin.merriman@gmail.com>
2017-05-02 11:15:47 +0100

    Add eslint and fix various breaking errors

--- src/app/config/store.js
+++ src/app/config/store.js
@@ -1,11 +1,24 @@
-// const initialState = {
-//     hello: "hello"
-// };
-//
-// export default function reducer(state = initialState, action) {
-//
-// }
+import { createStore, combineReducers, applyMiddleware, compose } from 'redux'
+import { browserHistory } from 'react-router';
+import { syncHistoryWithStore, routerReducer, routerMiddleware } from 'react-router-redux';
+import thunkMiddleware from 'redux-thunk';
 
-// let store = createStore(reducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
-//
-// export default store;
+import reducer from './reducer';
+
+export const baseHistory = browserHistory;
+const routingMiddleware = routerMiddleware(baseHistory);
+
+const enhancer = compose(
+    applyMiddleware(thunkMiddleware, routingMiddleware),
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
+
+export const store = createStore(
+    combineReducers({
+        state: reducer,
+        routing: routerReducer
+    }),
+    enhancer
+);
+
+export const history = syncHistoryWithStore(baseHistory, store);
