09bbea93710f09cfb2e12bf5fcd2847b0572c3e7
Crispioso <crispin.merriman@gmail.com>
2017-05-02 11:15:47 +0100

    Add eslint and fix various breaking errors
    Former-commit-id: bfa45e305b433eb7a7e48ce2512176bc9b81183d
    Former-commit-id: 13048c930e0c3af3bb4b553457571b75f030a013
    Former-commit-id: bbfb9a4e6b48e4ec317df78a93f107be3265ebac

--- src/webpack.config.js
+++ src/webpack.config.js
@@ -23,9 +23,19 @@ module.exports = {
                 }
             },
             {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                loader: 'eslint-loader'
+            },
+            {
                 test: /\.jsx$/,
                 exclude: /node_modules/,
                 loader: 'babel-loader'
+            },
+            {
+                test: /\.jsx$/,
+                exclude: /node_modules/,
+                loader: 'eslint-loader'
             },
             {
                 test: /\.scss$/,
