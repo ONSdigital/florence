6e9a6b4ab0b4ef20091a19829f4410af74968fb3
Crispioso <crispin.merriman@gmail.com>
2017-05-03 20:46:05 +0100

    Add selectable box component and begin team page

---
+++ src/app/components/selectable-box/SelectableBoxController.jsx
@@ -0,0 +1,55 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+import SelectableBoxItem from './SelectableBoxItem';
+
+const propTypes = {
+    heading: PropTypes.string.isRequired,
+    items: PropTypes.array,
+    handleItemClick: PropTypes.func,
+    isUpdating: PropTypes.bool
+}
+
+export default class SelectableBoxController extends Component {
+    constructor(props) {
+        super(props);
+
+        this.bindItemClick = this.bindItemClick.bind(this);
+    }
+
+    bindItemClick(itemProps) {
+        this.props.handleItemClick(itemProps);
+    }
+
+    renderList() {
+        return (
+            <ul className="selectable-box__list">
+                {
+                    this.props.items.map((item, index) => {
+                        return (
+                            <SelectableBoxItem 
+                                key={index} 
+                                {...item} 
+                                handleClick={this.bindItemClick}
+                            />
+                        )
+                    })
+                }
+            </ul>
+        )
+    }
+
+    render() {
+        return (
+           <div className="selectable-box">
+                <h2 className="selectable-box__heading">
+                    Name
+                    { this.props.isUpdating ? "<span>Updating...</span>" : "" }
+                </h2>
+                { this.renderList() }
+            </div> 
+        )
+    }
+}
+
+SelectableBoxController.propTypes = propTypes;
