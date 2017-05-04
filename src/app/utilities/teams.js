be3cc19fbdd1a9fb116633175834d7b4242f1929
Crispioso <crispin.merriman@gmail.com>
2017-05-04 08:16:51 +0100

    Allow routing to specific team details

--- src/app/utilities/teams.js
+++ src/app/utilities/teams.js
@@ -5,7 +5,7 @@ export default class teams {
     static getAll() {
         return get(`/zebedee/teams`)
             .then(response => {
-                return response;
+                return response.teams;
             }).catch(error => {
                 console.log(`Error getting all teams \n${error}`);
         });
