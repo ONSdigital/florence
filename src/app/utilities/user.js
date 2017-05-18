13009ada4305a8988c9455851081e02617f167bf
Jon Dewi Jones <jondewijones@gmail.com>
2017-05-18 16:40:22 +0100

    Add get, getAll and setUserState methods

--- src/app/utilities/user.js
+++ src/app/utilities/user.js
@@ -1,14 +1,47 @@
 import { get } from '../utilities/get';
+import { store } from '../config/store';
+import { userLoggedIn } from '../config/actions';
 
 export default class user {
 
     static get(email) {
+        return get(`zebedee/users?email=${email}`)
+            .then(response => {
+                return response;
+            }).catch(error => {
+                console.error(`Error getting user details for ${email} \n${error}`);
+            })
+    }
+
+    static getAll() {
+        return get(`zebedee/users`)
+            .then(response => {
+                return response;
+            }).catch(error => {
+                console.error(`Error getting all users \n${error}`)
+            })
+    }
+
+    static getPermissions(email) {
         return get(`/zebedee/permission?email=${email}`)
             .then(response => {
                 return response;
             }).catch(error => {
-                console.log(`Error getting user details for ${email} \n${error}`);
+                console.error(`Error getting user permissions for ${email} \n${error}`);
         });
+    }
+
+    static setUserState(user) {
+        const email = user.email;
+        let userType = '';
+        if (user.editor) {
+            userType = 'EDITOR'
+        } else {
+            userType = 'DATA-VIS'
+        }
+        const isAdmin = !!user.admin;
+        store.dispatch(userLoggedIn(email, userType, isAdmin));
+        localStorage.setItem("loggedInAs", email);
     }
 
 }
