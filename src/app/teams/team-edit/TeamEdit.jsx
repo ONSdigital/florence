b9f3a2455d6689571a35f1a4b4dd7f94d8270ea9
Crispioso <crispin.merriman@gmail.com>
2017-05-24 11:09:09 +0100

    Fix teams tests
    Former-commit-id: 99a1647af493c081b6d2181eac3295d6e617874e
    Former-commit-id: 44540bbae8d8a6910150e0deab35b4be0c548625
    Former-commit-id: df11d12e00bc8b5507c4e929ef0f94dce39b91bd

--- src/app/teams/team-edit/TeamEdit.jsx
+++ src/app/teams/team-edit/TeamEdit.jsx
@@ -6,7 +6,8 @@ const propTypes = {
     members: PropTypes.arrayOf(PropTypes.string),
     users: PropTypes.arrayOf(PropTypes.object),
     updatingAllUsers: PropTypes.bool.isRequired,
-    showingLoader: PropTypes.bool,
+    updatingMembers: PropTypes.bool.isRequired,
+    showingLoaders: PropTypes.bool,
     onMembersChange: PropTypes.func.isRequired,
     onDone: PropTypes.func.isRequired
 }
@@ -43,55 +44,62 @@ class TeamEdit extends Component {
                 <div className="grid__col-6">
                     <div className="add-remove__col">
                         <h2 className="add-remove__col-heading">All users</h2>
-                        {!this.props.updatingAllUsers &&
-                            <ul className="add-remove__list list list--neutral">
-                                {this.props.users.map((user, index) => {
-                                    return (
-                                        <li key={index} className="add-remove__item">
-                                            <span className="add-remove__item-title">{user.email}</span>
-                                            <button 
-                                                onClick={this.handleMembersChange} 
-                                                className="btn btn--positive" 
-                                                data-email={user.email}
-                                                data-action="add"
-                                            >
-                                                Add
-                                            </button>
-                                        </li>
-                                    )
-                                })}
-                            </ul>
-                        }
-
-                            {this.props.showingLoader && 
+                        <div className="add-remove__body ">
+                            {!this.props.updatingAllUsers &&
+                                <ul className="list list--neutral">
+                                    {this.props.users.map((user, index) => {
+                                        return (
+                                            <li key={index} className="add-remove__item">
+                                                <span className="add-remove__item-title">{user.email}</span>
+                                                <button 
+                                                    onClick={this.handleMembersChange} 
+                                                    className="btn btn--positive" 
+                                                    data-email={user.email}
+                                                    data-action="add"
+                                                >
+                                                    Add
+                                                </button>
+                                            </li>
+                                        )
+                                    })}
+                                </ul>
+                            }
+                            {this.props.showingLoaders && 
                                 <div className="add-remove__loader loader loader--dark"></div>
                             }
+                        </div>
                     </div>
                 </div>
                 <div className="grid__col-6">
                     <div className="add-remove__col add-remove__col--border-left">
                         <h2 className="add-remove__col-heading">Team members</h2>
-                        <ul className="add-remove__list list list--neutral">
-                        {this.props.members.length > 0 ?
-                            this.props.members.map((member, index) => {
-                                return (
-                                    <li key={index} className="add-remove__item">
-                                        <span className="add-remove__item-title">{member}</span>
-                                        <button 
-                                            onClick={this.handleMembersChange} 
-                                            className="btn"  
-                                            data-email={member}
-                                            data-action="remove"
-                                        >
-                                            Remove
-                                        </button>
-                                    </li>
-                                )
-                            })
-                            :
-                            <p>This team has no members</p>
-                        }
-                        </ul>
+                        <div className="add-remove__body">
+                            {!this.props.updatingAllUsers &&
+                                this.props.members.length > 0 ?
+                                <ul className="list list--neutral">
+                                    {this.props.members.map((member, index) => {
+                                        return (
+                                            <li key={index} className="add-remove__item">
+                                                <span className="add-remove__item-title">{member}</span>
+                                                <button 
+                                                    onClick={this.handleMembersChange} 
+                                                    className="btn"  
+                                                    data-email={member}
+                                                    data-action="remove"
+                                                >
+                                                    Remove
+                                                </button>
+                                            </li>
+                                        )
+                                    })}
+                                </ul>
+                                :
+                                <p>This team has no members</p>
+                            }
+                            {this.props.showingLoaders && 
+                                <div className="add-remove__loader loader loader--dark"></div>
+                            }
+                        </div>
                     </div>
                 </div>
                 <div className="grid__col-12">
