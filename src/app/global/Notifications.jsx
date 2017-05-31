3fa30c55fd4d89b02c27718cddcebd563a5ce2a8
Crispioso <crispin.merriman@gmail.com>
2017-05-31 15:23:48 +0100

    Add extra config to notification and move to abstracted API

--- src/app/global/Notifications.jsx
+++ src/app/global/Notifications.jsx
@@ -5,19 +5,41 @@ const propTypes = {
     notifications: PropTypes.arrayOf(PropTypes.shape({
         type: PropTypes.oneOf(["neutral", "warning"]).isRequired,
         message: PropTypes.string,
-        id: PropTypes.number.isRequired
+        id: PropTypes.number.isRequired,
+        buttons: PropTypes.arrayOf(PropTypes.shape({
+            onClick: PropTypes.func.isRequired,
+            text: PropTypes.string.isRequired
+        }))
     }))
 };
 
 class Notifications extends Component {
+
+    renderNotification(notification) {
+        return (
+            <li key={notification.id} className="notifications__item">
+                {notification.message}
+                {notification.buttons.map((button, index) => {
+                    return ( 
+                        <button 
+                            key={index} 
+                            className="btn notifications__button" 
+                            onClick={button.onClick}
+                        >
+                            {button.text}
+                        </button>
+                    )
+                })}
+            </li>
+        )
+    }
+
     render() {
         return (
-            <ul>
-                {
-                    this.props.notifications.map((notification, index) => {
-                        return <li key={index}>{notification.message}</li>
-                    })
-                }
+            <ul className="notifications">
+                {this.props.notifications.map(notification => {
+                    return this.renderNotification(notification)
+                })}
             </ul>
         )
     }
