66bee1371dc42a9db80ae789b6bad90965dfb24e
Crispioso <crispin.merriman@gmail.com>
2017-06-01 22:37:33 +0100

    Add tests for notification API layer

---
+++ src/app/utilities/notifications.test.js
@@ -0,0 +1,131 @@
+import notifications from './notifications';
+
+let mockNotificationsState = [];
+
+jest.mock('../config/store', () => (
+    {
+        store: {
+            dispatch: jest.fn().mockImplementation(updatedConfig => {
+                // nothing needed here
+            }),
+            getState: jest.fn().mockImplementation(() => {
+                return {
+                    state: {
+                        notifications: mockNotificationsState
+                    }
+                };
+            })
+        }
+    }
+))
+
+jest.mock('../config/actions', () => (
+    {
+        addNotification: jest.fn().mockImplementation(notification => {
+            mockNotificationsState.push(notification);
+        }),
+        removeNotification: jest.fn().mockImplementation(notificationID => {
+            mockNotificationsState = mockNotificationsState.filter(notification => {
+                return notification.id !== notificationID;
+            })
+        }),
+        toggleNotificationVisibility: jest.fn().mockImplementation(notificationID => {
+            mockNotificationsState = mockNotificationsState.map(notification => {
+                if (notification.id === notificationID) {
+                    notification.isVisible = !notification.isVisible;
+                }
+                return notification
+            })
+        })
+
+    }
+));
+
+test("Notifications are given unique ID on creation by default", () => {
+    const mockNotifications = [
+        {
+            message: "Message 1"
+        },
+        {
+            message: "Message 2"
+        }
+    ]
+    notifications.add(mockNotifications[0]);
+    notifications.add(mockNotifications[1]);
+    expect(mockNotificationsState[0].id).not.toBe(mockNotificationsState[1].id);
+    
+    // Reset mock variables
+    mockNotificationsState = [];
+});
+
+test("Auto dismiss configuration option automatically removes notification from state", () => {
+    const mockNotifications = [
+        {
+            message: "Message 1"
+        },
+        {
+            message: "Message 2",
+            autoDismiss: 1000
+        }
+    ]
+    jest.useFakeTimers();
+    notifications.add(mockNotifications[0]);
+    notifications.add(mockNotifications[1]);
+    expect(mockNotificationsState.length).toBe(2);
+    
+    setTimeout(() => {
+        expect(mockNotificationsState.length).toBe(1);
+    }, 3000);
+    jest.runAllTimers();
+
+    mockNotificationsState = [];
+});
+
+test("Button to dismiss notification is added to config correctly and included by default", () => {
+    const mockNotifications = [
+        {
+            message: "Message 1"
+        },
+        {
+            message: "Message 2",
+            isDismissable: false
+        },
+        {
+            message: "Message 2",
+            isDismissable: true
+        }
+    ]
+
+    notifications.add(mockNotifications[0]);
+    notifications.add(mockNotifications[1]);
+    notifications.add(mockNotifications[2]);
+    
+    expect(mockNotificationsState.length).toBe(3);
+    expect(mockNotificationsState[0].buttons.length).toBe(1);
+    expect(mockNotificationsState[1].buttons.length).toBe(0);
+    expect(mockNotificationsState[2].buttons.length).toBe(1);
+
+    mockNotificationsState = [];
+});
+
+test("Click 'dismiss' button removes that notfication from state", () => {
+    const mockNotifications = [
+        {
+            message: "Message 1",
+            id: 1,
+            isDismissable: true
+        }
+    ]
+    jest.useFakeTimers();
+
+    notifications.add(mockNotifications[0]);
+    mockNotificationsState[0].buttons[0].onClick();
+    setTimeout(() => {
+        expect(mockNotificationsState.length).toBe(0);
+    }, 1000)
+
+    jest.runAllTimers();
+
+    mockNotificationsState = [];
+});
+
