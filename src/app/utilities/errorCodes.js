5d002631bc460f24847b5249076b9379b5ed25d3
Jon Dewi Jones <jondewijones@gmail.com>
2017-06-01 11:39:56 +0100

    Add error type and error codes with messages

---
+++ src/app/utilities/errorCodes.js
@@ -0,0 +1,5 @@
+export const errCodes = {
+    FETCH_ERR: "Florence is unavailable",
+    RESPONSE_ERR: "Florence is experiencing difficulties",
+    UNEXPECTED_ERR: "Something has gone seriously wrong. Please restart Florence"
+};
