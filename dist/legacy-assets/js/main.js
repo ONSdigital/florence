d2992fab79174f8a29abe65543c73a4a6e95e205
Crispioso <crispin.merriman@gmail.com>
2017-06-13 14:03:18 +0100

    Add mock object and remove mentions of data vis collection type on collections screen
    Former-commit-id: fcb61d8f1b093969541480449734cf203c5665c9
    Former-commit-id: 2c6e51b8cc638b55fb5638325d5491dc81ff55b2
    Former-commit-id: d26f0b94e46311a77a5f4355a15bf1ff9688d680

--- dist/legacy-assets/js/main.js
+++ dist/legacy-assets/js/main.js
@@ -480,6 +480,60 @@ function getUsers(success, error, userId) {
     }
   });
 }
+var mock = {
+    collections: [
+        {
+            approvalStatus: "NOT_STARTED",
+            publishComplete: false,
+            isEncrypted: false,
+            timeseriesImportFiles: [ ],
+            id: "anewcollection-f4f70936e0c6131655a65c64b9691aafd5c5fdb1135a34278d3635ad0894a0da",
+            name: "A new collection",
+            type: "manual",
+            teams: [ ]
+        },
+        {
+            approvalStatus: "NOT_STARTED",
+            publishComplete: false,
+            isEncrypted: false,
+            timeseriesImportFiles: [ ],
+            id: "viscollection-52085dd2cf41acc7af3aec2220abacabd4cc63ad012c41fa1fc013a457dc66a8",
+            name: "vis collection",
+            type: "manual",
+            teams: [ ]
+        },
+        {
+            approvalStatus: "NOT_STARTED",
+            publishComplete: false,
+            isEncrypted: false,
+            timeseriesImportFiles: [ ],
+            id: "collection1-e8b1ecb3d5ea1bdc8e008981118e99e4a47875e18088f2217da232bfd3e74b2f",
+            name: "collection 1",
+            type: "manual",
+            teams: ["heeeerd"]
+        },
+        {
+            approvalStatus: "NOT_STARTED",
+            publishComplete: false,
+            isEncrypted: false,
+            timeseriesImportFiles: [ ],
+            id: "collection2-d04f8b47e2f9205fa6d5e085a0899428ef0a91aeca5e025c20f32edb1c191500",
+            name: "collection 2",
+            type: "manual",
+            teams: [ ]
+        }, 
+        {
+            approvalStatus: "NOT_STARTED",
+            publishComplete: false,
+            isEncrypted: false,
+            timeseriesImportFiles: [ ],
+            id: "compendiumdata-7c7fda8a70269f367097b2d94e14b75a550c5f5dd04905f6648fd41b028d9b22",
+            name: "compendium data",
+            type: "manual",
+            teams: [ ]
+        }
+    ]
+}
 function moveContent(collectionId, path, newPath, success, error) {
   $.ajax({
     url: "/zebedee/contentmove/" + collectionId + "?uri=" + checkPathSlashes(path) + "&toUri=" + checkPathSlashes(newPath),
@@ -1032,7 +1086,6 @@ function createCollection(teams) {
                 publishDate: collectionDate,
                 pendingDeletes: [],
                 teams: teams,
-                collectionOwner: Florence.Authentication.userType(),
                 releaseUri: releaseUri
             }),
             success: function (collection) {
@@ -15765,7 +15818,8 @@ function viewCollectionDetails(collectionId, $this) {
             url: "/zebedee/collections",
             type: "get",
             success: function (data) {
-                result.data = data;
+                // result.data = data;
+                result.data = mock.collections;
             },
             error: function (jqxhr) {
                 handleApiError(jqxhr);
@@ -15828,11 +15882,7 @@ function viewCollectionDetails(collectionId, $this) {
             }
         });
 
-        var isDataVis = false;
-        if (Florence.Authentication.userType() === "DATA_VISUALISATION") {
-            isDataVis = true;
-        }
-        var collectionsHtml = templates.collectionList({response: response, teams: result.team.teams, isDataVis: isDataVis});
+        var collectionsHtml = templates.collectionList({response: response, teams: result.team.teams});
         $('.section').html(collectionsHtml);
 
         if (collectionId) {
