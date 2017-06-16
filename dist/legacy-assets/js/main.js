c648045fc9f620fba761f39f1a0ec20c28043b9f
Crispioso <crispin.merriman@gmail.com>
2017-06-16 08:07:55 +0100

    Fix create screen for visualisation

--- dist/legacy-assets/js/main.js
+++ dist/legacy-assets/js/main.js
@@ -44764,14 +44764,7 @@ function initialiseChartList(collectionId, data) {
 
 
 function loadCreateScreen(parentUrl, collectionId, type, collectionData) {
-    var isDataVis = false; // Flag for template to show correct options in select
-
-    // Load data vis creator or ordinary publisher creator
-    if (collectionData && collectionData.collectionOwner == "DATA_VISUALISATION") {
-        isDataVis = true;
-        type = 'visualisation';
-    }
-
+    var isDataVis = type === "visualisation"; // Flag for template to show correct options in select
     var html = templates.workCreate({"dataVis": isDataVis});
 
     $('.workspace-menu').html(html);
