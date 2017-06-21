5afc94c8c5f8ea4e1fa60d568b5632b9875f2eac
Lloyd Griffiths <lloyd.griffiths@ons.gov.uk>
2017-06-21 09:19:22 +0100

    install node modules during build

--- ci/scripts/build.sh
+++ ci/scripts/build.sh
@@ -5,5 +5,5 @@ cwd=$(pwd)
 export GOPATH=$cwd/go
 
 pushd $GOPATH/src/github.com/ONSdigital/florence
-  make build && cp Dockerfile.concourse build/florence $cwd/build
+  make node-modules build && cp Dockerfile.concourse build/florence $cwd/build
 popd
