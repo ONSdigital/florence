70d02a996f7715d9499c9a6da204328f33912724
Crispioso <crispin.merriman@gmail.com>
2017-07-21 10:59:57 +0100

    Basic working functionality for inserting boxes into markdown - to be finished

--- dist/legacy-assets/js/main.js
+++ dist/legacy-assets/js/main.js
@@ -7337,6 +7337,33 @@ function loadMarkdownEditor(content, onSave, pageData, notEmpty) {
         });
     });
 
+    $("#js-editor--box").click(function() {
+        var $input = $('#wmd-input');
+        var selectionStart = $input[0].selectionStart;
+        var selectionEnd = $input[0].selectionEnd;
+        var selection;
+
+        if (selectionStart === selectionEnd) {
+            var eachLine = $input.val().split("\n");
+            var beforeCursor = $input.val().substring(0, selectionStart).split("\n");
+            var afterCursor = $input.val().substring(selectionStart).split("\n")[0];
+            selection = beforeCursor[beforeCursor.length-1] + afterCursor;
+
+            var wrappedSelection = "<box align=>\n" + selection + "\n</box>";
+
+            eachLine[beforeCursor.length-1] = wrappedSelection;
+
+            $input.val(eachLine.join("\n"));
+
+        } else {
+            selection = $input.val().substring(selectionStart, selectionEnd)
+            newInputValue = $input.val().slice(0, selectionStart) + "<box align=>\n" + selection + "\n</box>" + $input.val().slice(selectionEnd);
+            $input.val(newInputValue);
+        }
+
+        console.log(selection);
+    });
+
     $("#wmd-input").on('click', function () {
         markDownEditorSetLines();
     });
