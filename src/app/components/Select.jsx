cea7225ee5810d4232c9c32ef537fe6b020f5791
Jon Dewi Jones <jondewijones@gmail.com>
2017-09-18 20:02:58 +0100

    Add new select componeent and styling
    Former-commit-id: 818bcc47ec3cbd53cc140750e121e09489920f00
    Former-commit-id: d2d5bb0def05171f17129f02473b8591d32a1554
    Former-commit-id: f09cd8b44e67ae2f8484713f67d4b3f161cd3af6

--- src/app/components/Select.jsx
+++ src/app/components/Select.jsx
@@ -1,3 +1,50 @@
-/**
- * Created by jon on 18/09/2017.
- */
+import React, {Component} from 'react';
+import PropTypes from 'prop-types';
+
+const propTypes = {
+    id: PropTypes.string,
+    label: PropTypes.string,
+    contents: PropTypes.array.isRequired,
+    defaultOption: PropTypes.string,
+    onChange: PropTypes.func
+};
+
+class Select extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            isFocused: false
+        }
+
+        this.handleFocus = this.handleFocus.bind(this);
+    }
+
+    handleFocus() {
+        this.state.isFocused ? this.setState({isFocused: false}) : this.setState({isFocused: true})
+    }
+
+    render() {
+        return (
+            <div className="form__input">
+                <label className="form__label" htmlFor={this.props.id}>{this.props.label}</label>
+                <div className={"select-wrap " + (this.state.isFocused ? "select-wrap--focus" : "")}>
+                    <select
+                        className="select"
+                        id={this.props.id}
+                        onChange={this.props.onChange}
+                        onFocus={this.handleFocus}
+                        onBlur={this.handleFocus}>
+                        <option>{this.props.defaultOption || "Select an option"}</option>
+                        {this.props.contents.map((item, index) => {
+                            return <option key={index} value={item.id || ""}>{item.name || item}</option>
+                        })}
+                    </select>
+                </div>
+            </div>
+        )
+    }
+}
+
+Select.propTypes = propTypes;
+export default Select;
