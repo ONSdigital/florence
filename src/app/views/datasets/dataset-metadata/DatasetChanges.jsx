1f1a196299fd76d3ffcdc1dfdf44e383766ad096
Crispioso <crispin.merriman@gmail.com>
2017-09-25 14:10:47 +0100

    Add 'card' element to SCSS and example in DatasetChanges component

--- src/app/views/datasets/dataset-metadata/DatasetChanges.jsx
+++ src/app/views/datasets/dataset-metadata/DatasetChanges.jsx
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { Link } from 'react-router';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
+import isEmptyObject from 'is-empty-object';
 
 import datasets from '../../../utilities/api-clients/datasets'
 import {updateActiveInstance} from '../../../config/actions'
@@ -13,7 +14,12 @@ const propTypes = {
     params: PropTypes.shape({
         instance: PropTypes.string.isRequired
     }).isRequired,
-    instance: PropTypes.object,
+    instance: PropTypes.shape({
+        alerts: PropTypes.arrayOf(PropTypes.shape({
+            title: PropTypes.string,
+            description: PropTypes.string
+        }))
+    }),
     dispatch: PropTypes.func.isRequired,
     routes: PropTypes.arrayOf(PropTypes.shape({
         path: PropTypes.string
@@ -30,11 +36,55 @@ class DatasetChanges extends Component {
     }
 
     componentWillMount() {
-        if (!this.props.instance) {
+        if (isEmptyObject(this.props.instance)) {
+            this.setState({isFetchingInstance: true});
             datasets.getInstance(this.props.params.instance).then(instance => {
                 this.props.dispatch(updateActiveInstance(instance));
+                this.setState({isFetchingInstance: false});
             });
         }
+    }
+
+    handleAlertEdit(event) {
+        event.preventDefault()
+    }
+
+    handleAlertDelete(event) {
+        event.preventDefault()
+    }
+
+    renderAlerts() {
+        return (
+            <div className="margin-bottom--2">
+                <h2 className="margin-bottom--1">Alerts and corrections</h2>
+                <ul className="list--neutral margin-bottom--1">
+                    {this.props.instance.alerts.map((alert, index) => {
+                        return (
+                            <li key={index} className="card">
+                                <div className="card__body">
+                                    <div className="card__title">{alert.title}</div>
+                                    {alert.description}
+                                </div>
+                                <div className="card__actions">
+                                    <a href="" onClick={this.handleAlertEdit}>Edit</a>
+                                    <a className="margin-left--1" href="" onClick={this.handleAlertDelete}>Delete</a>
+                                </div>
+                            </li>
+                        )
+                    })}
+                </ul>
+                <Link to={`${location.pathname}/add-alert`}>Add an alert</Link>
+            </div>
+        )
+    }
+
+    renderChanges() {
+        return (
+            <div className="margin-bottom--2">
+                <h2>Summary of changes</h2>
+                <Link className="margin-bottom--2" to={`${location.pathname}/add-change`}>Add change</Link>
+            </div>
+        )
     }
 
     render() {
@@ -49,31 +99,25 @@ class DatasetChanges extends Component {
                         <p className="margin-bottom--2">
                             This information can change each time a new edition is published. <Link to={`${url.resolve("history")}`}>View history</Link>
                         </p>
-                        {this.props.instance ?
-                            <div>
-                                <div className="margin-bottom--2">
-                                    <h2>Alerts and corrections</h2>
-                                    <Link to={`${location.pathname}/add-alert`}>Add an alert</Link>
-                                </div>
-                                <div className="margin-bottom--2">
-                                    <h2>Summary of changes</h2>
-                                    <Link className="margin-bottom--2" to={`${location.pathname}/add-change`}>Add change</Link>
-                                </div>
-                            </div>
-                        :
+                        {this.state.isFetchingInstance ?
                             <div className="loader loader--dark"></div>
+                        :
+                            <div>
+                                {this.renderAlerts()}
+                                {this.renderChanges()}
+                            </div>
                         }
                     </div>
                 </div>
                 {this.props.routes[this.props.routes.length-1].path === "add-alert" &&
-                    <Modal sizeClass="grid__col-5">
+                    <Modal sizeClass="grid__col-4">
                         <h2 className="modal__header">Add alert</h2>
                         <div className="modal__body">
                             <p>Thing</p>
                         </div>
                         <div className="modal__footer">
                             <button className="btn btn--positive">Add</button>
-                            <Link to={`${url.parent()}`}>Cancel</Link>
+                            <Link className="margin-left--1" to={`${url.parent()}`}>Cancel</Link>
                         </div>
                     </Modal>
                 }
