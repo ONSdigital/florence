9b6187b81801120f167c531b4fea59120a349243
Matt Rout <matt_rout92@live.co.uk>
2017-09-25 14:35:39 +0100

    Further enhancements for related content pagr
    Former-commit-id: ccf6734b03c3fe8d40b7f4f733b18ea0c87ae161
    Former-commit-id: 48dea25e8bd575e15049dab450d708ae0da9ced7
    Former-commit-id: 35c8b2931de254e1e41de3986a53b5548f9a105b

--- src/app/views/datasets/dataset-metadata/DatasetRelated.jsx
+++ src/app/views/datasets/dataset-metadata/DatasetRelated.jsx
@@ -1,18 +1,77 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router';
 import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+
+import Modal from '../../../components/Modal';
+import RelatedDataController from './related-content/RelatedDataController';
 
 const propTypes = {
+    dispatch: PropTypes.func.isRequired,
+    rootPath: PropTypes.string.isRequired,
+    routes: PropTypes.arrayOf(PropTypes.object).isRequired,
 }
 
 class DatasetRelated extends Component {
     constructor(props) {
         super(props);
 
-        this.state = {};
+        this.state = {
+            showModal: false,
+            relatedTitles: [],
+            relatedURLs: [],
+            titleInput: {
+                value: "",
+                error: ""
+            },
+            urlInput: {
+                value: "",
+                error: ""
+            },
+        };
+
+        this.handleAddRelatedClick = this.handleAddRelatedClick.bind(this);
+        this.handleCancel = this.handleCancel.bind(this);
+        this.handleFormInput = this.handleFormInput.bind(this);
+        this.handleFormSubmit = this.handleFormSubmit.bind(this);
+    }
+
+    handleAddRelatedClick() {
+        this.setState({showModal: true});
+    }
+
+    handleCancel() {
+        this.setState({showModal: false});
+    }
+
+    handleFormInput(event) {
+        const input = Object.assign({}, this.state.input, {
+               value: event.target.value,
+               error: ""
+            });
+        if (event.target.name === "add-related-content-title") {
+            this.setState({titleInput: input});
+        } else if (event.target.name === "add-related-content-url") {
+            this.setState({urlInput: input});
+        }
+    }
+
+    handleFormSubmit(event) {
+        event.preventDefault()
+
+        var titles = this.state.relatedTitles.concat(this.state.titleInput.value);
+        this.setState({relatedTitles: titles});
+
+        var urls = this.state.relatedURLs.concat(this.state.urlInput.value);
+        this.setState({relatedURLs: urls});
+
+        this.setState({showModal: false});       
     }
 
     render() {
+
+        console.log("related titles on render: " + this.state.relatedTitles)
+        
         return (
             <div className="grid grid--justify-center">
                 <div className="grid__col-6">
@@ -26,7 +85,12 @@ class DatasetRelated extends Component {
                     </div>
                     <div className="margin-bottom--2">
                         <h3> Bulletins, articles and compendia </h3>
-                        <a href="#"> Add document </a>
+                        {
+                            this.state.relatedTitles.map((title) => {
+                                return (<p>Title: {title}</p>)
+                            })                            
+                        }
+                        <a href="#" onClick={this.handleAddRelatedClick}> Add document </a>
                     </div>
                     <div className="margin-bottom--2">
                         <h3> QMIs </h3>
@@ -48,6 +112,19 @@ class DatasetRelated extends Component {
                         <button className="btn btn--positive" type="submit">Save and Continue</button>
                     </div>
                 </div>
+                {   
+                    this.state.showModal ?             
+                    <Modal sizeClass="grid__col-3">
+                        <RelatedDataController 
+                            name="related-content-modal"
+                            onCancel={this.handleCancel}
+                            onFormInput={this.handleFormInput}
+                            onFormSubmit={this.handleFormSubmit}
+                        />
+                    </Modal>
+                    :
+                    ""
+                }
             </div>
         )
     }
