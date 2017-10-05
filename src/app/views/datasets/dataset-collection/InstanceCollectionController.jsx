6071c2479177a4949c27cef243d8baa5dd01f58b
Jon Dewi Jones <jondewijones@gmail.com>
2017-10-05 16:53:59 +0100

    Rename DatasetCollectionController to InstanceCollectionController and add extra logging and error checking

--- src/app/views/datasets/dataset-collection/InstanceCollectionController.jsx
+++ src/app/views/datasets/dataset-collection/InstanceCollectionController.jsx
@@ -3,8 +3,10 @@ import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 
 import dateFormat from 'dateformat';
+import notifications from '../../../utilities/notifications';
 import collections from '../../../utilities/api-clients/collections';
 import DatasetCollectionView from './DatasetCollectionView';
+
 
 const propTypes = {
     params: PropTypes.shape({
@@ -42,6 +44,15 @@ class DatasetCollectionController extends Component {
         this.setState({isGettingCollections: true});
         collections.getAll()
             .then(allCollections => {
+                if (allCollections.length === 0) {
+                    const notification = {
+                        "type": "warning",
+                        "message": "No collections were found. Try refreshing",
+                        isDismissable: true
+                    };
+                    notifications.add(notification);
+                    return
+                }
                 const collectionsSelectItems = [];
                 allCollections.map(item => {
                     collectionsSelectItems.push({id: item.id, name: item.name})
@@ -51,7 +62,65 @@ class DatasetCollectionController extends Component {
                     allCollections: allCollections,
                     isGettingCollections: false
                 })
-            })
+            }).catch(error => {
+            switch (error.status) {
+                case(403):{
+                    const notification = {
+                        "type": "neutral",
+                        "message": "You do not permission to view collections.",
+                        isDismissable: true
+                    };
+                    notifications.add(notification);
+                    break;
+                }
+                case(404):{
+                    const notification = {
+                        "type": "warning",
+                        "message": "No API route available to get collections.",
+                        isDismissable: true
+                    };
+                    notifications.add(notification);
+                    break;
+                }
+                case("RESPONSE_ERR"):{
+                    const notification = {
+                        "type": "warning",
+                        "message": "An error's occurred whilst trying to get the collections.",
+                        isDismissable: true
+                    };
+                    notifications.add(notification);
+                    break;
+                }
+                case("FETCH_ERR"): {
+                    const notification = {
+                        type: "warning",
+                        message: "There's been a network error whilst trying to get collections. Please check you internet connection and try again in a few moments.",
+                        isDismissable: true
+                    };
+                    notifications.add(notification);
+                    break;
+                }
+                case("UNEXPECTED_ERR"): {
+                    const notification = {
+                        type: "warning",
+                        message: "An unexpected error has occurred whilst trying to get collections.",
+                        isDismissable: true
+                    };
+                    notifications.add(notification);
+                    break
+                }
+                default: {
+                    const notification = {
+                        type: "warning",
+                        message: "An unexpected error's occurred whilst trying to get the submitted datasets.",
+                        isDismissable: true
+                    };
+                    notifications.add(notification);
+                    break;
+                }
+            }
+            this.setState({isFetchingData: false});
+        });
     }
 
     handleCollectionChange(event) {
@@ -90,18 +159,18 @@ class DatasetCollectionController extends Component {
             return
         }
 
-        const datasetToAddToCollection = {
+        const instanceToAddToCollection = {
             id: this.state.selectedCollection.id,
             name: this.state.selectedCollection.name
         };
 
         this.setState({ isSubmitting: true });
-        this.handleAddToCollection(datasetToAddToCollection);
+        this.handleAddToCollection(instanceToAddToCollection);
 
     }
 
     handleOnBackFromSuccess() {
-        collections.removeAPIDataset()
+        collections.removeInstance()
             .then(() => {
                 this.setState({hasChosen: false});
             })
@@ -110,7 +179,7 @@ class DatasetCollectionController extends Component {
     handleAddToCollection() {
         const instanceID = this.props.params.instance;
         const collectionID = this.state.selectedCollection.id;
-        collections.addAPIDataset(collectionID, instanceID)
+        collections.addInstance(collectionID, instanceID)
             .then(() => {
                 // possibly post "next release date" field to an api?
                 this.setState({hasChosen: true, isSubmitting: false });
--- src/index.js
+++ src/index.js
@@ -12,7 +12,7 @@ import TeamsController from './app/views/teams/TeamsController';
 import DatasetsController from './app/views/datasets/DatasetsController';
 import DatasetUploadsController from './app/views/datasets/dataset-upload/DatasetUploadsController';
 import DatasetOverviewController from './app/views/datasets/dataset-overview/DatasetOverviewController';
-import DatasetCollectionController from './app/views/datasets/dataset-collection/DatasetCollectionController';
+import InstanceCollectionController from './app/views/datasets/dataset-collection/InstanceCollectionController';
 import DatasetEdition from './app/views/datasets/dataset-metadata/DatasetEdition';
 import DatasetChangesController from './app/views/datasets/dataset-metadata/DatasetChangesController';
 import DatasetDetails from './app/views/datasets/dataset-metadata/DatasetDetails';
@@ -71,7 +71,7 @@ class Index extends Component {
                                 </Route>
                                 <Route path="uploads" component={ UserIsAuthenticated(DatasetUploadsController) } />
                                 <Route path="uploads/:job" component={ UserIsAuthenticated(DatasetOverviewController) } />
-                                <Route path="add-to-collection/:instance" component={ UserIsAuthenticated(DatasetCollectionController) } />
+                                <Route path="add-to-collection/:instance" component={ UserIsAuthenticated(InstanceCollectionController) } />
                             </Route>
                             <Route path={`${rootPath}/logs`} component={ UserIsAuthenticated(Logs) } />
                             <Route path={`${rootPath}/login`} component={ LoginController } />
