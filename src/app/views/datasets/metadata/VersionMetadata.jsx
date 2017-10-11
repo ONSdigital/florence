a6a642a35f9ec76413fd0fe244d49efeb44126b4
Crispioso <crispin.merriman@gmail.com>
2017-10-11 13:18:17 +0100

    Update to latest routes
    Former-commit-id: f92872d882a73f327fc38462ad071badbf4cc84c
    Former-commit-id: 219a5f7da0b64fc06afa75b789a770a9000b5766
    Former-commit-id: d545c43dc9c72407cd43b534b033c5a6542f703c

---
+++ src/app/views/datasets/metadata/VersionMetadata.jsx
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+
+export default class VersionMetadata extends Component {
+    render() {
+        return (
+            <div className="grid grid--justify-center">
+                <div className="grid__col-6">
+                    <h1>Placeholder for version details</h1>
+                    <p>This is the page for <a href="https://projects.invisionapp.com/share/CQCR545XV#/screens/255270448">instance details</a>, such as:</p>
+                    <ul className="margin-top--1 margin-left--1">
+                        <li>Edition confirmation</li>
+                        <li>Changes in this version</li>
+                        <li>Contact details</li>
+                        <li>etc...</li>
+                    </ul>
+                </div>
+            </div>
+        )
+    }
+}
