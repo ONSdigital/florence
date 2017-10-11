8ede2a9db617b85d53c4eb9b236bf58fd71d39da
Matt Rout <matt_rout92@live.co.uk>
2017-10-11 08:55:14 +0100

    Initial update of select a dataset screen
    Former-commit-id: 9195dfcf70b52da50819d784c32a335bf11a5fbd
    Former-commit-id: b6ee9cb633abafeb90ef96d4ec25e55c9c59bb79
    Former-commit-id: f971788719947d6bff2447deb119927f825ff4d8

---
+++ src/app/views/datasets/selectable-table/SelectableTableController.jsx
@@ -0,0 +1,115 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+const propTypes = {
+    values: PropTypes.arrayOf(PropTypes.object)
+}
+
+export default class SelectableTableController extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            table: []
+        }
+
+        this.handleDetailsClick = this.handleDetailsClick.bind(this);
+    }
+
+    handleDetailsClick(event, key) {
+        event.preventDefault();
+        var isSelected = true;
+        var array = this.state.table;
+
+        this.state.table.map((item, index) => {
+            if (item === key) {
+                array.splice(index, 1);
+                this.setState({table: array});
+                isSelected = false;
+                return;
+            }
+        })
+
+        if (isSelected) {
+            array.push(key)
+            this.setState({table: array});
+        }
+    }
+
+    render() {
+        return (
+            <div className="selectable-table">
+                    <div className="selectable-table__heading">
+                        <ul className="selectable-table__list">
+                            <li className="selectable-table__list-item grid__col-6">Title</li>
+                            <li className="selectable-table__list-item grid__col-6">Submission Date</li>
+                        </ul>
+                    </div>
+                    {this.props.values.map((item, index) => {
+                        return (
+                                <div key={index}>
+                                    <details onClick={(event) => {this.handleDetailsClick(event, index)}} className={this.state.table.includes(index) ? "selectable-table__selected" : ""} open={this.state.table.includes(index) ? true : false}>
+                                        <summary>
+                                            <ul className="selectable-table__list">
+                                                <li className="selectable-table__list-item grid__col-6">
+                                                    <strong>{item.title}</strong>
+                                                </li>
+                                                <li className="selectable-table__list-item grid__col-4">
+                                                    {item.date}
+                                                </li>
+                                                <li className="selectable-table__list-item grid__col-2">
+                                                    <div className={this.state.table.includes(index) ? "selectable-table__accordion-open" : "selectable-table__accordion-closed"}></div>
+                                                </li>
+                                            </ul>
+                                        </summary>
+                                        <div className="selectable-table__list">
+                                            <a href={item.datasetURL}> Edit dataset details </a>
+                                            <ul>
+                                                <li className="selectable-table__list-item grid__col-4">
+                                                    Date
+                                                </li>
+                                                <li className="selectable-table__list-item grid__col-2">
+                                                    Edition
+                                                </li>
+                                                <li className="selectable-table__list-item grid__col-2">
+                                                    Version
+                                                </li>
+                                                <hr />
+                                            </ul>
+                                            {item.instances.map((instance, i) => {
+                                                return (
+                                                    <div key={i}>
+                                                        <ul>
+                                                            <li className="selectable-table__list-item grid__col-4">
+                                                                {instance.date}
+                                                            </li>
+                                                            <li className="selectable-table__list-item grid__col-2">
+                                                                {instance.edition}
+                                                            </li>
+                                                            <li className="selectable-table__list-item grid__col-4">
+                                                                {instance.version}
+                                                            </li>
+                                                            <li className="selectable-table__list-item grid__col-2">
+                                                                <a href={instance.url}>View</a>
+                                                            </li>
+                                                            <hr />
+                                                        </ul>
+                                                    </div>
+                                                )
+                                            })}
+                                        </div>
+                                    </details>
+                                    {index != this.props.values.length - 1 ?
+                                        <hr className="selectable-table__table-divider" />
+                                    :
+                                        ""
+                                    }
+                                </div>
+                        )
+                    })}
+            </div>
+        )
+    }
+}
+
+SelectableTableController.propTypes = propTypes;
