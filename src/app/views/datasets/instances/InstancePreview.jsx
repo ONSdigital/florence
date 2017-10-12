6db53d4be6a2d76522a8a8dd1a5b9c3260ac46fe
Crispioso <crispin.merriman@gmail.com>
2017-10-12 11:07:20 +0100

    Add tests for instance preview component

--- src/app/views/datasets/instances/InstancePreview.jsx
+++ src/app/views/datasets/instances/InstancePreview.jsx
@@ -17,7 +17,7 @@ const propTypes = {
     }).isRequired
 }
 
-class InstancePreview extends Component {
+export class InstancePreview extends Component {
     constructor(props) {
         super(props);
 
