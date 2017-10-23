c7871ad5dc8952986f6ee740a0379e5717f67e46
boxadesign <info@boxadesign.co.uk>
2017-10-23 15:45:57 +0100

    Added put code

--- src/app/views/datasets/metadata/VersionMetadata.jsx
+++ src/app/views/datasets/metadata/VersionMetadata.jsx
@@ -10,7 +10,7 @@ import recipes from '../../../utilities/api-clients/recipes';
 import notifications from '../../../utilities/notifications';
 import Select from '../../../components/Select';
 import Input from '../../../components/Input';
-import {updateActiveInstance, updateAllRecipes} from '../../../config/actions';
+import {updateActiveInstance, updateActiveVersion, updateAllRecipes, updateActiveDataset} from '../../../config/actions';
 import url from '../../../utilities/url'
 
 const propTypes = {
@@ -28,10 +28,19 @@ const propTypes = {
         id: PropTypes.string
       })),
     })),
+    dataset: PropTypes.shape({
+      title: PropTypes.string.isRequired,
+      release_frequency: PropTypes.string.isRequired,
+    }),
     instance: PropTypes.shape({
       edition: PropTypes.string,
       version: PropTypes.number,
       id: PropTypes.string,
+      dimensions: PropTypes.arrayOf(PropTypes.object),
+    }),
+    version: PropTypes.shape({
+      edition: PropTypes.string,
+      version: PropTypes.number,
       dimensions: PropTypes.arrayOf(PropTypes.object),
     }),
     isInstance: PropTypes.string
@@ -45,11 +54,12 @@ class VersionMetadata extends Component {
             isFetchingData: false,
             isInstance: null,
             edition: null,
-            datasetTitle: null,
+            title: null,
+            release_frequency: null,
             dimensions: []
         }
 
-        this.handleEditionChange = this.handleEditionChange.bind(this);
+        this.handleSelectChange = this.handleSelectChange.bind(this);
         this.handleFormSubmit = this.handleFormSubmit.bind(this);
         this.handleInputChange = this.handleInputChange.bind(this);
     }
@@ -57,11 +67,8 @@ class VersionMetadata extends Component {
     componentWillMount() {
       this.setState({isFetchingData: true});
 
-      datasets.get(this.props.params.datasetID).then(dataset => {
-          this.setState({datasetTitle: dataset.current.title});
-      });
-
       const getMetadata = [
+          Promise.resolve(),
           Promise.resolve(),
           Promise.resolve()
       ];
@@ -78,16 +85,35 @@ class VersionMetadata extends Component {
         this.setState({isInstance: false});
       }
 
+      getMetadata[2] = datasets.get(this.props.params.datasetID);
+
       Promise.all(getMetadata).then(responses => {
+
         if (this.props.recipes.length === 0) {
             this.props.dispatch(updateAllRecipes(responses[0].items));
         }
 
-        this.props.dispatch(updateActiveInstance(responses[1]));
+        if (this.props.params.instanceID) {
+          this.props.dispatch(updateActiveInstance(responses[1]));
+          this.setState({
+            dimensions: this.props.instance.dimensions,
+            edition: this.props.instance.edition,
+          });
+        }
+
+        if (this.props.params.version) {
+          this.props.dispatch(updateActiveVersion(responses[1]));
+          this.setState({
+            dimensions: this.props.version.dimensions,
+            edition: this.props.version.edition,
+          });
+        }
+
+        this.props.dispatch(updateActiveDataset(responses[2].current));
 
         this.setState({
-          dimensions: this.props.instance.dimensions,
-          edition: this.props.instance.edition,
+          title: this.props.dataset.title,
+          release_frequency: this.props.dataset.release_frequency,
           isFetchingData: false
         });
 
@@ -135,6 +161,59 @@ class VersionMetadata extends Component {
         return true;
     }
 
+    postData(body) {
+      if(this.state.isInstance) {
+        return datasets.updateInstanceEdition(this.props.params.instanceID, this.state.selectedEdition, body);
+      } else {
+        return datasets.updateVersion(this.props.params.datasetID, this.props.params.edition, this.props.params.version, body);
+      }
+    }
+
+    updateInstanceVersion(metaData) {
+      return this.postData(metaData).then(() => {
+        if(this.state.isInstance) {
+          datasets.getInstance(this.props.params.instanceID).then(response => {
+            this.props.dispatch(push(`${this.props.rootPath}/datasets/${this.props.params.datasetID}/editions/${response.edition}/versions/${response.version}/collection`));
+          });
+        } else {
+          this.props.dispatch(push(url.resolve("collection")));
+        }
+
+      }).catch(error => {
+          switch (error.status) {
+              case(403):{
+                  const notification = {
+                      "type": "neutral",
+                      "message": "You do not permission to view the edition metadata for this dataset",
+                      isDismissable: true
+                  }
+                  notifications.add(notification);
+                  break;
+              }
+              case (404): {
+                  const notification = {
+                      "type": "neutral",
+                      "message": `Dataset ID '${this.props.params.datasetID}' was not recognised. You've been redirected to the datasets home screen`,
+                      isDismissable: true
+                  };
+                  notifications.add(notification);
+                  this.props.dispatch(push(url.parent(url.parent())));
+                  break;
+              }
+              default: {
+                  const notification = {
+                      type: "warning",
+                      message: "An unexpected error's occurred whilst trying to get this dataset",
+                      isDismissable: true
+                  }
+                  notifications.add(notification);
+                  break;
+              }
+          }
+          console.error("Error has occurred:\n", error);
+      });
+    }
+
     mapEditionsToSelectOptions() {
       const recipe = this.props.recipes.find(recipe => {
           return recipe.output_instances[0].dataset_id === this.props.params.datasetID;
@@ -162,17 +241,9 @@ class VersionMetadata extends Component {
       )
     }
 
-    handleEditionChange(event) {
-        event.preventDefault();
-        this.setState({
-            selectedEdition: event.target.value,
-            editionError: null
-        });
-    }
-
     mapReleaseFreqToSelectOptions() {
         const values = [
-          'Weekly', 'Monthly', 'Annually'
+          'Weekly', 'Monthly', 'Yearly'
         ];
         return values.map(value => {
             return {
@@ -191,30 +262,43 @@ class VersionMetadata extends Component {
        });
      }
 
+     handleSelectChange(event) {
+        const target = event.target;
+        const value = target.value;
+        const id = target.id;
+        this.setState({
+          [id]: value
+        });
+      }
 
     handleFormSubmit(event) {
         event.preventDefault();
 
-        if (!this.state.selectedEdition) {
-          this.setState({
-              editionError: "You must select an edition"
-          });
-          return;
+        if (!this.state.edition || !this.state.release_frequency) {
+          if (!this.state.edition) {
+            this.setState({
+                editionError: "You must select an edition"
+            });
+          }
+
+          if (!this.state.release_frequency) {
+            this.setState({
+                releaseError: "You must select a release frequency"
+            });
+          }
+          return
         }
 
-        if (this.state.selectedEdition) {
-          if(this.state.isInstance) {
-            datasets.updateInstanceEdition(this.props.params.instanceID, this.state.selectedEdition).then(() => {
-              datasets.getInstance(this.props.params.instanceID).then(response => {
-                this.props.dispatch(push(`${this.props.rootPath}/datasets/${this.props.params.datasetID}/editions/${response.edition}/versions/${response.version}/collection`));
-              });
-            });
-            return;
-          } else {
-            this.props.dispatch(push(url.resolve("collection")));
-          }
+        const metaData = {
+          release_frequency: this.state.release_frequency,
+          edition: this.state.edition
+        }
+
+        if (this.state.edition && this.state.release_frequency) {
+          this.updateInstanceVersion(metaData);
         }
     }
+
 
     render() {
 
@@ -222,7 +306,7 @@ class VersionMetadata extends Component {
             <div className="grid grid--justify-center">
                 <div className="grid__col-6">
                     <div className="margin-top--2">
-                        &#9664; <Link to={url.parent(url.parent())}>Back</Link>
+                      &#9664; <Link to={url.resolve("/datasets")}>Back</Link>
                     </div>
                     <h1 className="margin-top--1 margin-bottom--1">New data</h1>
                     <p>This information is specific to this new data and can be updated each time new data is added.</p>
@@ -230,26 +314,29 @@ class VersionMetadata extends Component {
                           <div className="loader loader--dark"></div>
                       :
                       <div>
-                        <h2 className="margin-top--1">{this.state.datasetTitle}</h2>
+                        <h2 className="margin-top--1">{this.state.title}</h2>
 
                         <form onSubmit={this.handleFormSubmit}>
                           <div className="margin-bottom--2">
                             <div className="grid__col-6">
                               <Select
+                                  id="edition"
                                   label="Edition"
                                   contents={this.mapEditionsToSelectOptions()}
-                                  onChange={this.handleEditionChange}
+                                  onChange={this.handleSelectChange}
                                   error={this.state.editionError}
-                                  defaultOption={this.state.edition}
+                                  selectedOption={this.state.edition}
                               />
                             </div>
                             <div className="grid__col-6 margin-bottom--1">
                               <h2 className="margin-top--1">Notes and information</h2>
                               <Select
+                                  id="release_frequency"
                                   contents={this.mapReleaseFreqToSelectOptions()}
                                   onChange={this.handleSelectChange}
-                                  error={this.state.error}
+                                  error={this.state.releaseError}
                                   label="Release frequency"
+                                  selectedOption={this.state.release_frequency}
                               />
                             </div>
                             {this.state.isInstance &&
@@ -272,7 +359,9 @@ function mapStateToProps(state) {
     return {
       rootPath: state.state.rootPath,
       instance: state.state.datasets.activeInstance,
-      recipes: state.state.datasets.recipes
+      version: state.state.datasets.activeVersion,
+      recipes: state.state.datasets.recipes,
+      dataset: state.state.datasets.activeDataset
     }
 }
 
