6434b1c5a598ca5b46fb904c765ca99cabacbed3
Jon Dewi Jones <jondewijones@gmail.com>
2017-10-23 14:15:21 +0100

    Remove next release date input

--- src/app/views/datasets/collection/CollectionView.jsx
+++ src/app/views/datasets/collection/CollectionView.jsx
@@ -4,7 +4,6 @@ import { Link } from 'react-router';
 import url from '../../../utilities/url'
 
 import Select from '../../../components/Select';
-import Input from '../../../components/Input';
 
 const propTypes = {
     hasChosen: PropTypes.bool.isRequired,
@@ -64,11 +63,6 @@ class CollectionView extends Component {
                         />
 
                         {selectedCollection.id ? this.renderSelectedCollectionDetails() : ""}
-
-                        <Input id="next-release-date"
-                               label="Next release date"
-                               onChange={this.props.handleNextReleaseChange}
-                        />
 
                         <button type="submit" className="btn btn--positive" disabled={isSubmitting}>
                             Save and continue
