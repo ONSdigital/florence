540929f6edb033823c76d69bd6fdff5129978870
Crispioso <crispin.merriman@gmail.com>
2017-11-23 11:40:24 +0000

    Add request ID to every ajax in legacy Florence

--- src/legacy/js/classes/log.js
+++ src/legacy/js/classes/log.js
@@ -8,6 +8,13 @@ var log = {
             timestamp: timestamp.getTime(),
             payload: payload || null
         }
+
+        // Add unrecognised log type to log event
+        if (!log.eventTypesMap[eventType]) {
+            console.log("Unrecognised log type: ", eventType, payload);
+            event.type = log.eventTypes.unrecognisedLogType;
+        }
+
         websocket.send("log:" + JSON.stringify(event));
     },
     eventTypes: {
@@ -22,6 +29,45 @@ var log = {
         socketBufferFull: "SOCKET_BUFFER_FULL",
         socketError: "SOCKET_ERROR",
         unexpectedRuntimeError: "UNEXPECTED_RUNTIME_ERROR",
-        runtimeWarning: "RUNTIME_WARNING"
-    }
+        runtimeWarning: "RUNTIME_WARNING",
+        unrecognisedLogType: "UNRECOGNISED_LOG_TYPE"
+    },
+    eventTypesMap: {}
 }
+
+// Create a map for event type values to property name 
+// so that we can detect when we're logging an unrecognised event
+for (var eventType in log.eventTypes) {
+    log.eventTypesMap[log.eventTypes[eventType]] = eventType;
+}
+
+$.ajaxSetup({
+    beforeSend: function(request) {
+        var requestID = (S4() + S4());
+        request.setRequestHeader("X-Request-ID", requestID);
+        request.uniqueID = requestID;
+        log.add(
+            log.eventTypes.requestSent, {
+                requestID
+            }
+        );
+    }
+});
+
+$(document).ajaxSuccess(function(event, request, settings) {
+    log.add(
+        log.eventTypes.requestReceived, {
+            requestID: request.uniqueID,
+            status: request.statusText
+        }
+    );
+});
+
+$(document).ajaxError(function(event, request, settings) {
+    log.add(
+        log.eventTypes.requestReceived, {
+            requestID: request.uniqueID,
+            status: request.statusText
+        }
+    );
+});
