3ac91525ca98ea782c09668b9db7563879e6e269
Jon Dewi Jones <jondewijones@gmail.com>
2017-12-12 09:30:52 +0000

    Add unit tests and snapshots for radio group and buttons
    Former-commit-id: b4d8246bce3618f2436c18022eea03594981e0bf
    Former-commit-id: 1a5e5f9052aa8b11e672caee2a64565d1e420c68
    Former-commit-id: 19842fa3682aa593e099f08410c3f765e8c775cb

---
+++ src/app/components/radio-buttons/RadioGroup.test.js
@@ -0,0 +1,52 @@
+import React from 'react';
+import RadioGroup from './RadioGroup';
+import renderer from 'react-test-renderer';
+import { shallow } from 'enzyme';
+
+const defaultProps = {
+    radioData: [
+        {
+            id: "test-radio1",
+            value: "test-value1",
+            label: "Test radio 1",
+        },
+        {
+            id: "test-radio2",
+            value: "test-value2",
+            label: "Test radio 2",
+        }
+    ],
+    legend: "Select a test radio",
+    groupName: "test-group",
+    onChange: ()=>{},
+    selectedValue: "test-value1",
+    inline: false
+};
+
+const mockedEvent = {id: "test-radio2", checked: true, value: "test-value2"};
+
+test("Radio button matches stored snapshot", () => {
+
+    const component = renderer.create(
+        <RadioGroup {...defaultProps} />
+    );
+    expect(component.toJSON()).toMatchSnapshot();
+});
+
+test("Handle change update states correctly and passes correct value caller onChange method", () => {
+    let passedToParent = {};
+    const mockedOnChange = (event) => {passedToParent = event;};
+    const component = shallow(
+        <RadioGroup {...defaultProps} checked={false} onChange={mockedOnChange}/>
+    );
+    expect(component.update().state().selectedValue).toBe("test-value1");
+    component.instance().handleChange(mockedEvent);
+    expect(component.update().state().selectedValue).toBe("test-value2");
+    expect(passedToParent).toMatchObject({
+        id: "test-radio2",
+        checked: true,
+        value: "test-value2"
+    });
+});
+
+
