fd0a9b64586e097a9fe0cf4598945737155aef1d
Crispioso <crispin.merriman@gmail.com>
2017-12-13 18:16:11 +0000

    Get full collection details on collection ID route and set default background-color on buttons

--- src/app/utilities/api-clients/collections.js
+++ src/app/utilities/api-clients/collections.js
@@ -2,6 +2,13 @@ import http from '../http';
 
 export default class collections {
 
+    static get(collectionID) {
+        return http.get(`/zebedee/collectionDetails/${collectionID}`)
+            .then(response => {
+                return response;
+            })
+    }
+    
     static getAll() {
         return http.get(`/zebedee/collections`)
             .then(response => {
--- src/app/views/collections/details/CollectionDetails.jsx
+++ src/app/views/collections/details/CollectionDetails.jsx
@@ -4,7 +4,11 @@ import PropTypes from 'prop-types';
 const propTypes = {
     collectionID: PropTypes.string.isRequired,
     name: PropTypes.string.isRequired,
-    onCancel: PropTypes.func.isRequired
+    onCancel: PropTypes.func.isRequired,
+    isLoadingDetails: PropTypes.bool,
+    inProgress: PropTypes.array,
+    complete: PropTypes.array,
+    reviewed: PropTypes.array
 };
 
 export class CollectionDetails extends Component {
@@ -14,12 +18,49 @@ export class CollectionDetails extends Component {
         this.state = {};
     }
 
+    renderInProgress() {
+        return this.props.inProgress.map(page => {
+            return (
+                <li key={page.uri}>{page.description.title}</li>
+            )
+        });
+    }
+    
+    renderWaitingReview() {
+        return this.props.complete.map(page => {
+            return (
+                <li key={page.uri}>{page.description.title}</li>
+            )
+        });
+    }
+    
+    renderReviewed() {
+        return this.props.reviewed.map(page => {
+            return (
+                <li key={page.uri}>{page.description.title}</li>
+            )
+        });
+    }
+
     render () {
         return (
             <div className="drawer__container">
                 <h2 className="drawer__heading">{this.props.name}</h2>
                 <div className="drawer__body">
-
+                    {this.props.isLoadingDetails ?
+                        <div className="grid grid--align-center margin-top--4">
+                            <div className="loader loader--large loader--dark"></div>
+                        </div>
+                        :
+                        <div>
+                            <h3>Pages in progress ({this.props.inProgress.length})</h3>
+                            {this.renderInProgress()}
+                            <h3>Pages waiting review ({this.props.complete.length})</h3>
+                            {this.renderWaitingReview()}
+                            <h3>Pages reviewed ({this.props.reviewed.length})</h3>
+                            {this.renderReviewed()}
+                        </div>
+                    }
                 </div>
                 <div className="drawer__footer">
                     <button className="btn" onClick={this.props.onCancel}>Cancel</button>
