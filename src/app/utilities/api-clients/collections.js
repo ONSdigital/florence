b6397f002b5b4eca13b6bc47220ff6718420da44
Jon Dewi Jones <jondewijones@gmail.com>
2017-12-13 15:30:50 +0000

    Add temporary list for triggering collection details
    Former-commit-id: c76fbe4e30fe55ce743a0c7737d0fc40968e566e
    Former-commit-id: 7e6b97a5086cf6a242cac4ad556912baa017e420
    Former-commit-id: 71a75b59078a740900c89288c0f7b936adfb2376

--- src/app/utilities/api-clients/collections.js
+++ src/app/utilities/api-clients/collections.js
@@ -2,6 +2,13 @@ import http from '../http';
 
 export default class collections {
 
+    static getAll() {
+        return http.get(`/zebedee/collections`)
+            .then(response => {
+                return response;
+            })
+    }
+
     static create(body) {
         return http.post(`/zebedee/collection`, body)
             .then(response => {
--- src/app/views/collections/CollectionsController.jsx
+++ src/app/views/collections/CollectionsController.jsx
@@ -1,16 +1,41 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux'
 
+import collections from '../../utilities/api-clients/collections';
+
 import { CollectionCreate } from './create/CollectionCreate';
 
 class Collections extends Component {
     constructor(props) {
         super(props);
+
+        this.state = {
+            collections: []
+        };
+
+        this.handleCollectionSelection = this.handleCollectionSelection.bind(this);
+
+    }
+
+    componentWillMount() {
+        this.fetchCollections();
+    }
+
+    fetchCollections() {
+        collections.getAll().then(collections => {
+            console.log(collections);
+            this.setState({collections: collections})
+        })
     }
 
     handleCollectionCreateSuccess() {
         // route to collection details pane for new collection
         // update list of collections
+    }
+
+    handleCollectionSelection() {
+        // trigger collection details view
+        console.log('clicked');
     }
 
     render () {
@@ -19,6 +44,13 @@ class Collections extends Component {
                 <div className="grid grid--justify-space-around">
                     <div className="grid__col-4">
                         <h1>Select a collection</h1>
+                        <ul>
+                        {this.state.collections ?
+                            this.state.collections.map((collection, index) => {
+                                return <li key={index} onClick={this.handleCollectionSelection}>{collection.name}</li>
+                            }) : ""
+                        }
+                        </ul>
 
                     </div>
                     <div className="grid__col-4">
