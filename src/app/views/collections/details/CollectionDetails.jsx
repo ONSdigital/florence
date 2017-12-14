e59f3ffb3f813c083ed2416554507c3c56116cb8
Crispioso <crispin.merriman@gmail.com>
2017-12-14 15:51:33 +0000

    Add page items to collection details and make them expandable on click
    Former-commit-id: 570162dd24a0827dff950664ed063a4d6993eaf1
    Former-commit-id: 8e026b2f4ba83c6473b724d43981527f661cea20
    Former-commit-id: e948dfd846e68b9f3237eaef120d40115a3107cf

--- src/app/views/collections/details/CollectionDetails.jsx
+++ src/app/views/collections/details/CollectionDetails.jsx
@@ -1,10 +1,15 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import url from '../../../utilities/url';
+
+import Page from '../../../components/page/Page';
 
 const propTypes = {
     collectionID: PropTypes.string.isRequired,
+    activePageID: PropTypes.string,
     name: PropTypes.string.isRequired,
     onCancel: PropTypes.func.isRequired,
+    onPageClick: PropTypes.func.isRequired,
     isLoadingDetails: PropTypes.bool,
     inProgress: PropTypes.array,
     complete: PropTypes.array,
@@ -14,38 +19,69 @@ const propTypes = {
 export class CollectionDetails extends Component {
     constructor(props) {
         super(props);
+    }
 
-        this.state = {};
+    renderPageItem(page) {
+        const pageID = url.slug(page.uri);
+        const handlePageClick = () => {
+            this.props.onPageClick(pageID);
+        }
+        return (
+            <li key={page.uri} onClick={handlePageClick} className={"list__item list__item--expandable" + (this.props.activePageID === pageID ? " active" : "")}>
+                <Page type={page.type} title={page.description.title} />
+            </li>
+        )
+    }
+
+    renderPagesList(pages) {
+        return (
+            <ul className="list list--neutral margin-bottom--2">
+                {pages}
+            </ul>
+        )
     }
 
     renderInProgress() {
-        return this.props.inProgress.map(page => {
-            return (
-                <li key={page.uri}>{page.description.title}</li>
-            )
+        if (this.props.inProgress.length === 0) {
+            return <p className="margin-bottom--2">No pages in progress</p>
+        }
+
+        const pages = this.props.inProgress.map(page => {
+            return this.renderPageItem(page);
         });
+        return this.renderPagesList(pages);
     }
     
     renderWaitingReview() {
-        return this.props.complete.map(page => {
-            return (
-                <li key={page.uri}>{page.description.title}</li>
-            )
+        if (this.props.complete.length === 0) {
+            return <p className="margin-bottom--2">No pages awaiting review</p>
+        }
+
+        const pages = this.props.complete.map(page => {
+            return this.renderPageItem(page);
         });
+        return this.renderPagesList(pages);
     }
     
     renderReviewed() {
-        return this.props.reviewed.map(page => {
-            return (
-                <li key={page.uri}>{page.description.title}</li>
-            )
+        if (this.props.reviewed.length === 0) {
+            return <p className="margin-bottom--2">No reviewed pages</p>
+        }
+
+        const pages = this.props.reviewed.map(page => {
+            return this.renderPageItem(page);
         });
+        return this.renderPagesList(pages);
     }
 
     render () {
         return (
             <div className="drawer__container">
                 <h2 className="drawer__heading">{this.props.name}</h2>
+                <div className="drawer__banner">
+                    <button className="btn btn--primary" disabled>Create/edit page</button>
+                    <button className="btn btn--margin-left" disabled>Restore page</button>
+                </div>
                 <div className="drawer__body">
                     {this.props.isLoadingDetails ?
                         <div className="grid grid--align-center margin-top--4">
@@ -53,11 +89,11 @@ export class CollectionDetails extends Component {
                         </div>
                         :
                         <div>
-                            <h3>Pages in progress ({this.props.inProgress.length})</h3>
+                            <h3 className="margin-bottom--1">{this.props.inProgress.length} page{this.props.inProgress.length > 1 && "s"} in progress</h3>
                             {this.renderInProgress()}
-                            <h3>Pages waiting review ({this.props.complete.length})</h3>
+                            <h3 className="margin-bottom--1">{this.props.complete.length} page{this.props.complete.length > 1 && "s"} awaiting review</h3>
                             {this.renderWaitingReview()}
-                            <h3>Pages reviewed ({this.props.reviewed.length})</h3>
+                            <h3 className="margin-bottom--1">{this.props.reviewed.length} reviewed page{this.props.reviewed.length > 1 && "s"}</h3>
                             {this.renderReviewed()}
                         </div>
                     }
