e95aa959a0066f4351aab96e20e75de408662131
boxadesign <info@boxadesign.co.uk>
2018-01-10 14:55:18 +0000

    Updated classes on controller view

--- src/app/views/datasets/collection/DatasetCollectionController.jsx
+++ src/app/views/datasets/collection/DatasetCollectionController.jsx
@@ -49,65 +49,64 @@ export class DatasetCollectionController extends Component {
             .then(dataset => {
                 this.setState(dataset.next.links.latest_version ? {hasVersion: "true"} : {hasVersion:"false"})
             }).catch(error => {
-                switch (error.status) {
-                    case(403):{
-                        const notification = {
-                            "type": "neutral",
-                            "message": "You do not have permission to preview this dataset.",
-                            isDismissable: true
-                        };
-                        notifications.add(notification);
-                        break;
-                    }
-                    case(404):{
-                        const notification = {
-                            "type": "warning",
-                            "message": "No API route available to get the dataset.",
-                            isDismissable: true
-                        };
-                        notifications.add(notification);
-                        break;
-                    }
-                    case("RESPONSE_ERR"):{
-                        const notification = {
-                            "type": "warning",
-                            "message": "An error's occurred whilst trying to get the dataset.",
-                            isDismissable: true
-                        };
-                        notifications.add(notification);
-                        break;
-                    }
-                    case("FETCH_ERR"): {
-                        const notification = {
-                            type: "warning",
-                            message: "There's been a network error whilst trying to get the dataset. Please check you internet connection and try again in a few moments.",
-                            isDismissable: true
-                        };
-                        notifications.add(notification);
-                        break;
-                    }
-                    case("UNEXPECTED_ERR"): {
-                        const notification = {
-                            type: "warning",
-                            message: "An unexpected error has occurred whilst trying to get the dataset.",
-                            isDismissable: true
-                        };
-                        notifications.add(notification);
-                        break
-                    }
-                    default: {
-                        const notification = {
-                            type: "warning",
-                            message: "An unexpected error's occurred whilst trying to get the submitted datasets.",
-                            isDismissable: true
-                        };
-                        notifications.add(notification);
-                        break;
-                    }
+            switch (error.status) {
+                case(403):{
+                    const notification = {
+                        "type": "neutral",
+                        "message": "You do not have permission to preview this dataset.",
+                        isDismissable: true
+                    };
+                    notifications.add(notification);
+                    break;
                 }
-            });
-            this.setState({isGettingDataset: false});
-
+                case(404):{
+                    const notification = {
+                        "type": "warning",
+                        "message": "No API route available to get the dataset.",
+                        isDismissable: true
+                    };
+                    notifications.add(notification);
+                    break;
+                }
+                case("RESPONSE_ERR"):{
+                    const notification = {
+                        "type": "warning",
+                        "message": "An error's occurred whilst trying to get the dataset.",
+                        isDismissable: true
+                    };
+                    notifications.add(notification);
+                    break;
+                }
+                case("FETCH_ERR"): {
+                    const notification = {
+                        type: "warning",
+                        message: "There's been a network error whilst trying to get the dataset. Please check you internet connection and try again in a few moments.",
+                        isDismissable: true
+                    };
+                    notifications.add(notification);
+                    break;
+                }
+                case("UNEXPECTED_ERR"): {
+                    const notification = {
+                        type: "warning",
+                        message: "An unexpected error has occurred whilst trying to get the dataset.",
+                        isDismissable: true
+                    };
+                    notifications.add(notification);
+                    break
+                }
+                default: {
+                    const notification = {
+                        type: "warning",
+                        message: "An unexpected error's occurred whilst trying to get the submitted datasets.",
+                        isDismissable: true
+                    };
+                    notifications.add(notification);
+                    break;
+                }
+            }
+        });
+        this.setState({isGettingDataset: false});
     }
 
     getCollections() {
