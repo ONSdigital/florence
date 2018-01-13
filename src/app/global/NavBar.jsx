de56ca609570f49089567aac1cb47123a1c08a1b
Crispioso <crispin.merriman@gmail.com>
2018-01-13 15:23:57 +0000

    Add logs route to nav bar

--- src/app/global/NavBar.jsx
+++ src/app/global/NavBar.jsx
@@ -32,50 +32,74 @@ class NavBar extends Component {
     }
 
     renderNavItems() {
-        if(!this.props.isAuthenticated) {
-            return (
-                <li className="global-nav__item">
-                    <Link to={`${this.props.rootPath}/login`} activeClassName="selected" className="global-nav__link">Login</Link>
-                </li>
-            )
-        }
-
         const route = this.props.location.pathname;
         const rootPath = this.props.rootPath;
         const isViewer = this.props.userType == 'VIEWER';
 
+        if(!this.props.isAuthenticated) {
+            return (
+                <ul>
+                    {route.includes(`${rootPath}/logs`) &&
+                        <li className="global-nav__item">
+                            <Link to={`${rootPath}/logs`} activeClassName="selected" className="global-nav__link">Logs</Link>
+                        </li>
+                    }
+                    <li className="global-nav__item">
+                        <Link to={`${this.props.rootPath}/login`} activeClassName="selected" className="global-nav__link">Login</Link>
+                    </li>
+                </ul>
+            )
+        }
+
         if (route.indexOf(`${rootPath}/collections`) >= 0 || route.indexOf(`${rootPath}/publishing-queue`) >= 0 || route.indexOf(`${rootPath}/reports`) >= 0 || route.indexOf(`${rootPath}/users-and-access`) >= 0 || route.indexOf(`${rootPath}/teams`) >= 0 || route.indexOf(`${rootPath}/not-authorised`) >= 0 ) {
             return (
-                <span>
-                    {!isViewer ?
-                        <span>
-                            <li className="global-nav__item">
-                                <a className="global-nav__link" href="/florence/collections">Collections</a>
-                            </li>
+                <nav>
+                    <ul>
+                        {!isViewer ?
+                            <span>
+                                <li className="global-nav__item">
+                                    <a className="global-nav__link" href="/florence/collections">Collections</a>
+                                </li>
 
-                            <li className="global-nav__item">
-                                <a className="global-nav__link" href="/florence/publishing-queue">Publishing queue</a>
-                            </li>
+                                <li className="global-nav__item">
+                                    <a className="global-nav__link" href="/florence/publishing-queue">Publishing queue</a>
+                                </li>
 
-                            <li className="global-nav__item">
-                                <a className="global-nav__link" href="/florence/reports">Reports</a>
-                            </li>
+                                <li className="global-nav__item">
+                                    <a className="global-nav__link" href="/florence/reports">Reports</a>
+                                </li>
 
-                            <li className="global-nav__item">
-                                <a className="global-nav__link" href="/florence/users-and-access">Users and access</a>
-                            </li>
+                                <li className="global-nav__item">
+                                    <a className="global-nav__link" href="/florence/users-and-access">Users and access</a>
+                                </li>
 
-                            <li className="global-nav__item">
-                                <Link to={`${rootPath}/teams`} activeClassName="selected" className="global-nav__link">Teams</Link>
-                            </li>
-                        </span>
-                    : "" }
+                                <li className="global-nav__item">
+                                    <Link to={`${rootPath}/teams`} activeClassName="selected" className="global-nav__link">Teams</Link>
+                                </li>
+                            </span>
+                        : "" }
 
-                    <li className="global-nav__item">
-                        <Link to={`${rootPath}/login`} onClick={this.handleLogoutClick} className="global-nav__link">Logout</Link>
-                    </li>
-                </span>
+                        <li className="global-nav__item">
+                            <Link to={`${rootPath}/login`} onClick={this.handleLogoutClick} className="global-nav__link">Logout</Link>
+                        </li>
+                    </ul>
+                </nav>
             )
+        }
+
+        if (route.includes(`${rootPath}/logs`)) {
+            return (
+                <nav>
+                    <ul>
+                        <li className="global-nav__item">
+                            <Link to={`${rootPath}/logs`} activeClassName="selected" className="global-nav__link">Logs</Link>
+                        </li>
+                        <li className="global-nav__item">
+                            <Link to={`${rootPath}/login`} onClick={this.handleLogoutClick} className="global-nav__link">Logout</Link>
+                        </li>
+                    </ul>
+                </nav>
+            )       
         }
     }
 
