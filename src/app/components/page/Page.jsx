ebb5c8adfbf9eeef664ae5d33d1f8e1010ceaa1b
Jon Dewi Jones <jondewijones@gmail.com>
2018-01-18 20:32:43 +0000

    Do not require pages to have a type. Deleted pages before Zebedee change wont have a type

--- src/app/components/page/Page.jsx
+++ src/app/components/page/Page.jsx
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
 const propTypes = {
-    type: PropTypes.string.isRequired,
+    type: PropTypes.string,
     title: PropTypes.string.isRequired,
     isActive: PropTypes.bool
 };
