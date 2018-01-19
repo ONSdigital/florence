17f0f0a149db04be132c6f06fbdf7e742102365c
Crispioso <crispin.merriman@gmail.com>
2018-01-19 10:43:30 +0000

    Fix collection edit form not submitting on enter consistently
    Former-commit-id: e49e9353a43aa14b6568a8351ff96520b9d30f9f
    Former-commit-id: 780ef6d51afa214f36a63693afd6acb1e0e1fbd3
    Former-commit-id: 590434129327abf7aa93f2ccb68c643e6ea0af56

--- src/app/views/collections/create/CollectionCreate.jsx
+++ src/app/views/collections/create/CollectionCreate.jsx
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux'
 import PropTypes from 'prop-types';
-import dateFormat from 'dateformat';
 
 import collections from '../../../utilities/api-clients/collections';
 import teams from '../../../utilities/api-clients/teams';
