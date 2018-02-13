c577173d9ea3b2db256ea844b25c80bb7cf330a4
Matthew Smith <matthew.smith@thinkinglogic.com>
2018-02-13 11:00:52 +0000

    [MS] Updated table builder styles

--- builder-app/src/index.js
+++ builder-app/src/index.js
@@ -3,6 +3,16 @@ import ReactDOM from 'react-dom';
 import GridContainer from 'dp-table-builder-ui';
 import 'dp-table-builder-ui/dist/assets/GridContainer.css';
 
+// tell es-lint not to bug us about sweetAlert not being defined:
+/* global sweetAlert */
+
  window.startTableBuilder = function(id, data, onSave, onCancel) {
-    ReactDOM.render(<GridContainer data={data} onSave={onSave} onCancel={onCancel} rendererUri={'/table'}/>, document.getElementById(id));
+     var onError = function (message) {
+         if (sweetAlert) {
+             sweetAlert(message);
+         } else {
+             console.log(message);
+         }
+     }
+    ReactDOM.render(<GridContainer data={data} onSave={onSave} onCancel={onCancel} onError={onError} rendererUri={'/table'}/>, document.getElementById(id));
 }
