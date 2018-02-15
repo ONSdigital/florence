d5efd88e9d93c6865feec397cfeb0bedd2650d93
Crispioso <crispin.merriman@gmail.com>
2018-02-15 16:59:21 +0000

    Allow tablebuilder React component to be unmounted properly

--- src/tablebuilder/tablebuilder.js
+++ src/tablebuilder/tablebuilder.js
@@ -3,9 +3,26 @@ import ReactDOM, { render } from 'react-dom';
 import TableBuilder from 'dp-table-builder-ui';
 
 window.startTableBuilder = (domID, data, onSave, onCancel, onError, path) => {
-    console.log({domID, data, onSave, onCancel, onError, path});
+    const node = document.getElementById(domID);
+
+    if (!node) {
+        onError(`Element with ID ${domID} doesn't exist`)
+        return;
+    }
+
     ReactDOM.render(
         <TableBuilder data={data} onSave={onSave} onCancel={onCancel} onError={onError} rendererUri={path} />,
-        document.getElementById(domID)
+        node
     )
+}
+
+window.closeTableBuilder = (domID, onError) => {
+    const node = document.getElementById(domID);
+
+    if (!node) {
+        onError(`Element with ID ${domID} doesn't exist`)
+        return;
+    }
+
+    ReactDOM.unmountComponentAtNode(node);
 }
