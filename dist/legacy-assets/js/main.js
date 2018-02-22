f5595c636bd352e3aac1d7ee19188d74bfc0ff87
Matthew Smith <matthew.smith@thinkinglogic.com>
2018-02-22 13:34:57 +0000

    Reverted to version 0.35.1 of handsontable

--- dist/legacy-assets/js/main.js
+++ dist/legacy-assets/js/main.js
@@ -48004,6 +48004,7 @@ function loadTableBuilderV2(pageData, onSave, table) {
 
   function closeModal() {
     closeTableBuilder("table-builder-app", onError)
+    $('.table-builder').stop().fadeOut(200).remove();
   }
 
   if (table && table.filename) {
