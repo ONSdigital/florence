dfe6c605a605241bd1c9a5fc3f72014511ef0244
Crispioso <crispin.merriman@gmail.com>
2018-03-19 16:43:13 +0000

    Fix broken unit tests for dataset metadata component

--- src/app/views/datasets/metadata/DatasetMetadata.test.js
+++ src/app/views/datasets/metadata/DatasetMetadata.test.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import {DatasetMetadata} from './DatasetMetadata.jsx';
-// import uuid from 'uuid/v4';
 import renderer from 'react-test-renderer';
 import { shallow } from 'enzyme';
 import handleMetadataSaveErrors from './datasetHandleMetadataSaveErrors';
@@ -248,10 +247,17 @@ test("Changing an input value updates the state to show a change has been made",
     const asyncComponent = await shallow(
         <DatasetMetadata {...defaultProps} />
     );
+    const event = {
+        ...mockEvent,
+        target: {
+            value: "A new description",
+            name: "description"
+        }
+    }
 
     await asyncComponent.update();
     expect(asyncComponent.state("hasChanges")).toBe(false);
-    asyncComponent.setState({description: "A new description"});
+    asyncComponent.instance().handleInputChange(event);
     await asyncComponent.update();
     expect(asyncComponent.state("description")).toBe("A new description");
     expect(asyncComponent.state("hasChanges")).toBe(true);
@@ -597,7 +603,7 @@ describe("Saving changes and submitting for review/approval", () => {
     });
 
     it("doesn't route to the current collection if there are errors", async () => {
-        handleMetadataSaveErrors.mockImplementationOnce(() => true);
+        collections.setDatasetStatusToReviewed.mockImplementationOnce(() => Promise.reject({status: 500}));
         dispatchedAction = null;
         await defaultComponent.instance().handleSave(mockEvent, false, true); 
         expect(dispatchedAction).toBeFalsy();
@@ -690,11 +696,14 @@ describe("Renders the correct buttons", () => {
         <DatasetMetadata {...reviewedProps} />
     )
     
-    it("disables the 'save' button loading data", () => {
+    it("disables the 'save' button loading data", async () => {
         expect(defaultComponent.state('isFetchingDataset')).toBeFalsy();
         expect(defaultComponent.state('isFetchingCollectionData')).toBeFalsy();
         defaultComponent.instance().componentWillMount();
         expect(defaultComponent.state('isFetchingDataset')).toBe(true);
+        expect(defaultComponent.find('#btn-save[disabled=true]').exists()).toBe(true);
+
+        await defaultComponent.update();
         expect(defaultComponent.state('isFetchingCollectionData')).toBe(true);
         expect(defaultComponent.find('#btn-save[disabled=true]').exists()).toBe(true);
     });
