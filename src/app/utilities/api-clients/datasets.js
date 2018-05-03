45c207c6c748c7668ea73501397f154ff6c83d49
Crispioso <crispin.merriman@gmail.com>
2018-05-03 16:28:34 +0100

    Auto populate version metadata with latest published version from that edition

--- src/app/utilities/api-clients/datasets.js
+++ src/app/utilities/api-clients/datasets.js
@@ -32,11 +32,30 @@ export default class datasets {
              });
     }
 
+    static getLatestVersion(datasetID, editionID) {
+        const editionURL = `/dataset/datasets/${datasetID}/editions/${editionID}`;
+
+        return new Promise(async (resolve, reject) => {
+            const edition = await http.get(editionURL).catch(error => reject(error));
+            if (edition && edition.current && edition.current.links.latest_version) {
+                const latestVersion = await http.get(`${editionURL}/versions/${edition.current.links.latest_version.id}`).catch(error => reject(error));
+                resolve(latestVersion);
+                return;
+            }
+            return;
+        });
+    }
+
     static getVersionDimensions(datasetID, edition, version) {
         return http.get(`/dataset/datasets/${datasetID}/editions/${edition}/versions/${version}/dimensions`)
              .then(response => {
                  return response;
              });
+    }
+
+    static updateInstanceDimensions(instanceID, dimensions) {
+        const body = {dimensions};
+        return http.put(`/instances/${instanceID}`, body);
     }
 
     static updateDimensionLabelAndDescription(instanceID, dimension, name, description) {
