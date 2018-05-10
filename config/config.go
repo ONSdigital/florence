f818eaab9022b6ebb87b5fbc45d9f1dc553ab1f1
mattrout92 <matt_rout92@live.co.uk>
2018-05-10 12:17:58 +0100

    Allow cookies to be accessible through subdomains
    Former-commit-id: cd0de341ac4dee90c0e4b99911d011c20f9acc7f
    Former-commit-id: 316e49a42072206ae626e33476bcbf55e9310c4f
    Former-commit-id: c71d3d0da53468aeb3ec3c94f16b8a1256fc58b8

--- config/config.go
+++ config/config.go
@@ -12,7 +12,6 @@ type Config struct {
 	RecipeAPIURL       string `envconfig:"RECIPE_API_URL"`
 	ImportAPIURL       string `envconfig:"IMPORT_API_URL"`
 	DatasetAPIURL      string `envconfig:"DATASET_API_URL"`
-	DownloadServiceURL string `envconfig:"DOWNLOAD_SERVICE_URL"`
 	UploadBucketName   string `envconfig:"UPLOAD_BUCKET_NAME"`
 	EncryptionDisabled bool   `envconfig:"ENCRYPTION_DISABLED"`
 	VaultAddr          string `envconfig:"VAULT_ADDR"`
@@ -36,7 +35,6 @@ func Get() (*Config, error) {
 		RecipeAPIURL:       "http://localhost:22300",
 		ImportAPIURL:       "http://localhost:21800",
 		DatasetAPIURL:      "http://localhost:22000",
-		DownloadServiceURL: "http://localhost:23600",
 		UploadBucketName:   "dp-frontend-florence-file-uploads",
 		EncryptionDisabled: false,
 		TableRendererURL:   "http://localhost:23300",
