917eb5f6f5fca801142d8eed49be5cc40e12bc15
Richard-95 <richillwest@gmail.com>
2018-07-02 13:00:47 +0100

    Cancel delete updates canBeApproved collection state
    Former-commit-id: 201665f2962a45ddde832d4ed551f9952917ee38
    Former-commit-id: 86cc4b9095d260486c9521c2df4bd98c72941d70
    Former-commit-id: 91b11d5ffd6297fe8322bfa6d4ace7b03f1d3193

--- src/app/views/collections/details/CollectionDetailsController.jsx
+++ src/app/views/collections/details/CollectionDetailsController.jsx
@@ -271,6 +271,7 @@ export class CollectionDetailsController extends Component {
                     return deletedPage.root.uri !== uri;
                 })
             };
+            updatedActiveCollection.canBeApproved = collectionMapper.collectionCanBeApproved(updatedActiveCollection);
             updatedActiveCollection.canBeDeleted = collectionMapper.collectionCanBeDeleted(updatedActiveCollection)
             this.props.dispatch(updateActiveCollection(updatedActiveCollection));
         }).catch(error => {
