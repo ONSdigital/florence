49e92f3bab433526a9185f8f9542f26e013f9c6f
Crispioso <crispin.merriman@gmail.com>
2018-07-10 09:03:22 +0100

    Remove redundant render import in tablebuilder wrapper component
    Former-commit-id: 4e9728d7dc9c01c9673e5e2726de659f9f6269a6
    Former-commit-id: 3a7b118f02dc251055dc97982197f6941c24c902
    Former-commit-id: ed7bd096a39f059bc857e7d5b5fe2c13948d282f

--- src/tablebuilder/tablebuilder.js
+++ src/tablebuilder/tablebuilder.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import ReactDOM, { render } from 'react-dom';
+import ReactDOM from 'react-dom';
 import TableBuilder from 'dp-table-builder-ui';
 import 'dp-table-builder-ui/dist/assets/scss/main.scss';
 
