f7094bea0f6666a4f48d4470eb319436a507f42d
Jon Dewi Jones <jondewijones@gmail.com>
2018-07-16 12:43:50 +0100

    On blur of input and textareas trim whitespsace
    Former-commit-id: d418336108d3bf23ea13fda9f8bdecc691699ae4
    Former-commit-id: c5e014983b30120b3a856aeef22421469da6e4d6
    Former-commit-id: 50325ab4cc40e9bb4c17fddc5eb5d7fb811ffe42

--- src/legacy/js/functions/_postContent.js
+++ src/legacy/js/functions/_postContent.js
@@ -28,6 +28,40 @@ function postContent(collectionId, path, content, overwriteExisting, recursive, 
         var url = "/zebedee/content/" + collectionId + "?uri=" + safePath + "/data.json";
     }
 
+    //var contentToClean = JSON.parse(content.replace(/"\s+|\s+"/g,'"'));
+    //var cleanedUpJSON = JSON.stringify(JSON.parse(content.replace(/"\s+|\s+"/g,'"')));
+    //var cleanedUpJSON = content.replace(/"\s+|\s+"/g,'"');
+    //console.log("CLEANED UP JSON =>", cleanedUpJSON);
+    //console.log("CONTENT =>", content);
+
+    //console.log("THE CONTENT =>", JSON.parse(content));
+
+    //var parsedContent = JSON.parse(content)
+
+    // for (var key in parsedContent) {
+    //     if (parsedContent.hasOwnProperty(key)) {
+    //         //console.log(key + " => " + parsedContent[key])
+    //         if (typeof parsedContent[key] === "object") {
+    //             //console.log(key + " is object!")
+    //             for (var subkey in parsedContent[key]) {
+    //                     //console.log(parsedContent[key][subkey])
+    //                     if (parsedContent[key][subkey] && typeof parsedContent[key][subkey] === "string") {
+    //                         console.log("About to trim ", parsedContent[key][subkey])
+    //                         parsedContent[key][subkey].trim()
+    //                     }
+    //             }
+    //         }
+    //     }
+    // }
+
+    // var edition = parsedContent.description.edition
+
+    // console.log("parsedContent length ", edition.length);
+    // var newEdition = edition.trim();
+    // console.log("parsedContent length ", newEdition.length);
+
+    // console.log("PARSED CONTENT =>", parsedContent);
+
     var url = url + '&overwriteExisting=' + overwriteExisting;
     var url = url + '&recursive=' + recursive;
 
@@ -40,6 +74,7 @@ function postContent(collectionId, path, content, overwriteExisting, recursive, 
         dataType: 'json',
         contentType: 'application/json',
         type: 'POST',
+        //data: cleanedUpJSON,
         data: content,
         success: function (response) {
             isUpdatingModal.remove();
