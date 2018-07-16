6bb571247bb5b8a4486b17dba1b9247c31cb2159
Crispioso <crispin.merriman@gmail.com>
2018-07-16 15:14:12 +0100

    Add unit tests to collectionController component

--- src/app/views/collections/CollectionsController.test.js
+++ src/app/views/collections/CollectionsController.test.js
@@ -1,8 +1,9 @@
 import React, { Component } from 'react';
-import { CollectionsController } from './CollectionsController';
+import { CollectionsController, mapStateToProps } from './CollectionsController';
 import { shallow } from 'enzyme';
-import { ADD_ALL_COLLECTIONS } from '../../config/actions';
+import { ADD_ALL_COLLECTIONS, EMPTY_ACTIVE_COLLECTION, DELETE_COLLECTION_FROM_ALL_COLLECTIONS } from '../../config/actions';
 import collectionMapper from './mapper/collectionMapper';
+import collections from '../../utilities/api-clients/collections';
 
 console.error = () => {};
 
@@ -22,9 +23,9 @@ jest.mock('../../utilities/notifications', () => {
 
 jest.mock('../../utilities/api-clients/collections', () => {
     return {
-        getAll: () => {
+        getAll: jest.fn(() => {
             return Promise.resolve(mockedAllCollections);
-        },
+        }),
         deletePage: () => {
             return Promise.resolve();
         },
@@ -112,78 +113,6 @@ const mockedAllCollections = [
     }
 ];
 
-const collection = {
-    id: 'testcollection-12345',
-    name: 'Test collection',
-    type: 'manual',
-    teams: ['cpi', 'cpih'],
-    inProgress: [
-        {
-            uri: "/",
-            type: "homepage",
-            description: {
-                title: "Home"
-            },
-            events: [
-                {
-                    email: "foobar@email.com",
-                    date: "2017-12-14T11:36:03.402Z"
-                }
-            ]
-        },
-        {
-            uri: "/economy/inflationsandprices/consumerinflation/bulletins/consumerpriceinflation/july2017",
-            type: "bulletin",
-            description: {
-                title: "Consumer Price Inflation",
-                edition: "July 2017"
-            },
-            events: [
-                {
-                    email: "foobar@email.com",
-                    date: "2017-12-14T11:36:03.402Z"
-                }
-            ]
-        }
-    ],
-    complete: [
-        {
-            uri: "/businessindustryandtrade",
-            type: "taxonomy_landing_page",
-            description: {
-                title: "Business industry and trade"
-            },
-            events: [
-                {
-                    email: "foobar@email.com",
-                    date: "2017-12-14T11:36:03.402Z"
-                },
-                {
-                    email: "foobar@email.com",
-                    date: "2017-12-10T10:21:43.402Z"
-                }
-            ]
-        }
-    ],
-    reviewed: [],
-    deletes: [{
-        user: "foobar@email.com",
-        root: {
-            uri: "/about/surveys",
-            type: "generic_static_page",
-            description: {
-                title: "Surveys"
-            },
-            children: [],
-            deleteMarker: true,
-            contentPath: "/about/surveys"
-        },
-        totalDeletes: 1
-    }],
-    datasets: [],
-    datasetVersion: []
-};
-
 let dispatchedActions = [];
 
 const defaultProps = {
@@ -205,13 +134,65 @@ const component = shallow(
     <CollectionsController {...defaultProps} />
 );
 
-describe("On creation of a collection", () => {
+beforeEach(() => {
+    // Reset our record of the dispatched actions, so now to break future tests
+    dispatchedActions = [];
+});
 
-    beforeEach(() => {
-        // Reset our record of the dispatched actions, so now to break future tests
-        dispatchedActions = [];
+describe("On mount of the collections screen", () => {
+    it("fetches data for all collections", () => {
+        const getCollectionsCalls = collections.getAll.mock.calls.length;
+        component.instance().componentWillMount();
+        expect(collections.getAll.mock.calls.length).toBe(getCollectionsCalls+1);
     });
 
+    it("updates state to show it's fetching data for all collections", async () => {
+        expect(component.state('isFetchingCollections')).toBe(false);
+
+        // Tests that state is set correctly before asynchronous requests have finished
+        component.instance().componentWillMount();
+        expect(component.state('isFetchingCollections')).toBe(true);
+
+        // Tests that state is set correctly after asynchronous requests were successful
+        await component.instance().componentWillMount();
+        expect(component.state('isFetchingCollections')).toBe(false);
+
+        // Tests that state is set correctly when asynchronous requests failed
+        collections.getAll.mockImplementationOnce(() => (
+            Promise.reject({status: 500})
+        ));
+        await component.instance().componentWillMount();
+        expect(component.state('isFetchingCollections')).toBe(false);
+    });
+
+    it("excludes adding collections to state that are in the publish queue or published", async () => {
+        // Verify that the collection we expect to be excluded will be returned on mount 
+        // otherwise this test could give a false positive
+        expect(mockedAllCollections.some(collection => collection.id === "test-sau39393uyqha8aw8y3n3")).toBe(true);
+
+        await component.instance().componentWillMount();
+        expect(dispatchedActions[0].collections.length).toBe(mockedAllCollections.length-1);
+        expect(dispatchedActions[0].collections.some(collection => collection.id === "test-sau39393uyqha8aw8y3n3")).toBe(false);
+    });
+});
+
+describe("On unmount of the collections screen", () => {
+    it("removes the active collection from state", () => {
+        component.instance().componentWillUnmount();
+        expect(dispatchedActions[0].type).toBe(EMPTY_ACTIVE_COLLECTION);
+    });
+});
+
+describe("Selecting a collection", () => {
+    it("routes to the collection's URL", () => {
+        component.instance().handleCollectionSelection({id: "testcollection-12345"});
+        expect(dispatchedActions[0].type).toBe("@@router/CALL_HISTORY_METHOD");
+        expect(dispatchedActions[0].payload.method).toBe("push");
+        expect(dispatchedActions[0].payload.args[0]).toBe("/florence/collections/testcollection-12345");
+    });
+});
+
+describe("On creation of a collection", () => {
     const createdCollection = {
         approvalStatus: "NOT_STARTED",
         events: [{}],
@@ -246,15 +227,62 @@ describe("On creation of a collection", () => {
     });
 });
 
+describe("Marking a collection to be deleted from list of collections", () => {
+    it("removes the collection from state", () => {
+        component.setProps({
+            collectionsToDelete: {"test-collection-12345": null}
+        });
+        expect(dispatchedActions[0].type).toBe(DELETE_COLLECTION_FROM_ALL_COLLECTIONS);
+        expect(dispatchedActions[0].collectionID).toBe("test-collection-12345");
+    });
 
-// TODO: 
-// - on create it adds new collection to state [DONE]
-// - on create it maps the new collection to the expected structure
-// - on create it routes to the URL of the new collection
-// - fetches all collections on load
-// - changes state to show that all collections are being fetched
-// - excludes collections that are in the publish queue or published
-// - removes the active collection on unmount
-// - routes to the URL when a collection is selected from the list
-// - test case for removing collections 
-// - test case for not removing collections until collections are fetched
+    it("doesn't remove the collection if all collections are still being fetched", () => {
+        component.instance().componentWillMount();
+        expect(component.state("isFetchingCollections")).toBe(true);
+        component.setProps({
+            collectionsToDelete: {"test-collection-12345": null}
+        });
+        expect(dispatchedActions).toMatchObject([]);
+    });
+});
+
+describe("mapStateToProps function", () => {
+    const state = {
+        state: {
+            user: {userType: "ADMIN"},
+            collections: {
+                all: [
+                    {id: "an-example-collection-12345"},
+                    {id: "deleted-example-collection-12345"}
+                ],
+                active: {id: "an-example-collection-12345"},
+                toDelete: {"deleted-example-collection-12345": null}
+            },
+            rootPath: "/florence"
+        }
+    };
+
+    const mappedProps = mapStateToProps(state);
+
+    it("returns the 'user' object", () => {
+        expect(mappedProps.user).toMatchObject({
+            userType: "ADMIN"
+        });
+    });
+    
+    it("returns the list of collections", () => {
+        expect(mappedProps.collections).toMatchObject([{id: "an-example-collection-12345"},{id: "deleted-example-collection-12345"}]);
+    });
+
+    it("returns the active collection", () => {
+        expect(mappedProps.activeCollection).toMatchObject({id: "an-example-collection-12345"});
+    });
+    
+    it("returns the map of collection to be deleted", () => {
+        expect(mappedProps.collectionsToDelete).toMatchObject({"deleted-example-collection-12345": null});
+    });
+    
+    it("returns the application's root path", () => {
+        expect(mappedProps.rootPath).toBe("/florence");
+    });
+});
