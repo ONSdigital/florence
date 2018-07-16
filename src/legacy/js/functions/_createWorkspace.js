d767745a0110db8b135598ea93ff8852483b6c4f
Ian Kent <email@iankent.co.uk>
2018-07-16 12:16:28 +0100

    remove forgotten console logs
    Former-commit-id: 6e729be92c6b846f8ad498998962cca944cf47c0
    Former-commit-id: a971076d384667a5d483ef6791063f971c3fa112
    Former-commit-id: 050bab0b28d158934765a5e07f6627c21087cdc2

--- src/legacy/js/functions/_createWorkspace.js
+++ src/legacy/js/functions/_createWorkspace.js
@@ -198,8 +198,6 @@ function createWorkspace(path, collectionId, menu, collectionData, stopEventList
                 contentType: 'application/json',
                 cache: false,
                 success: function (response, textStatus, xhr) {
-                    console.log(response);
-                    console.log(collectionData);
                     if (xhr.status == 204 || response === collectionData.name) {
                         loadPageDataIntoEditor(Florence.globalVars.pagePath, collectionId);
                         return;
