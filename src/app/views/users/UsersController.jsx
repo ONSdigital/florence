52490488ff7fd64ba745c08812b634333684f837
Jon Dewi Jones <jondewijones@gmail.com>
2018-08-16 11:53:55 +0100

    GEt all users and populate selectable box

--- src/app/views/users/UsersController.jsx
+++ src/app/views/users/UsersController.jsx
@@ -20,16 +20,47 @@ class UsersController extends Component {
         }
     }
 
+    componentWillMount() {
+        this.getAllUsers();
+    }
+
+    getAllUsers() {
+        this.setState({isFetchingUsers: true})
+        users.getAll()
+            .then(allUsersResponse => {
+                const allUsers = allUsersResponse.map(user => {
+                    return this.mapUserToState(user)
+                })
+                this.setState({allUsers, isFetchingUsers: false})
+            }).catch(error => {
+                // TODO handle errors properly
+                console.error(`Error getting all users`, error)
+                this.setState({isFetchingUsers: false})
+            })
+            
+    }
+
+    mapUserToState(user) {
+        const id = user.email;
+        const columnValues = [user.name, user.email]
+        const returnValue = {id: user.email}
+        return {...user, id, columnValues, returnValue};
+    }
+
+    handleUserSelection(user) {
+        console.log(user)
+    }
+
     render() {
         return (
             <div className="grid grid--justify-space-around">
                     <div className="grid__col-4">
                         <h1>Select a user</h1>
                         <SelectableBox 
-                            columns={[{heading: "User", width: "4"}, {heading: "Email", width: "4"}]}
+                            columns={[{heading: "User", width: "6"}, {heading: "Email", width: "6"}]}
                             rows={this.state.allUsers}
                             isUpdating={this.state.isFetchingUsers}
-                            handleItemClick={function(){}}
+                            handleItemClick={this.handleUserSelection}
                         
                         />
                     </div>
