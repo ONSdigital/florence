c7dae22170939fd3d6d37358df433355e59d2aba
Jon Dewi Jones <jondewijones@gmail.com>
2018-08-17 14:39:40 +0100

    Add validation to handle submit

--- src/app/views/users/create/UsersCreateController.jsx
+++ src/app/views/users/create/UsersCreateController.jsx
@@ -33,6 +33,7 @@ class UsersCreateController extends Component {
 
         this.handleInputChange = this.handleInputChange.bind(this);
         this.handleUserTypeChange = this.handleUserTypeChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
 
     }
 
@@ -41,8 +42,8 @@ class UsersCreateController extends Component {
         const value = event.target.value
 
         const newPropertyState = {
-            ...this.state.newUser[propertyName],
-            value: value
+            value: value,
+            error: ""
         }
 
         const newUserState = {
@@ -64,10 +65,54 @@ class UsersCreateController extends Component {
         this.setState({newUser: newUserState});
     }
 
+    handleSubmit(event) {
+        event.preventDefault();
+        const newUser = this.state.newUser
+
+        if (!newUser.username.value.length) {
+            const usernameState = {
+                ...newUser.username,
+                error: "You must enter a username"
+            }
+            const newUserState = {
+                ...newUser,
+                username: usernameState
+            }
+            this.setState({newUser: newUserState})
+            return;
+        }
+
+        if (!newUser.email.value.length) {
+            const emailState = {
+                ...newUser.email,
+                error: "You must enter a email"
+            }
+            const newUserState = {
+                ...newUser,
+                email: emailState
+            }
+            this.setState({newUser: newUserState})
+            return;
+        }
+
+        if (!newUser.password.value.length) {
+            const passwordState = {
+                ...newUser.password,
+                error: "You must enter a password"
+            }
+            const newUserState = {
+                ...newUser,
+                password: passwordState
+            }
+            this.setState({newUser: newUserState})
+            return;
+        }
+    }
+
     render() {
         return (
             <div>
-                <form onSubmit={function(e){e.preventDefault(); console.log("submitted")}}>
+                <form onSubmit={this.handleSubmit}>
                     <Input id="username" 
                         label="Username" 
                         type="text"
@@ -77,7 +122,7 @@ class UsersCreateController extends Component {
                     />
                     <Input id="email" 
                         label="Email" 
-                        type="text"
+                        type="email"
                         onChange={this.handleInputChange}
                         value={this.state.newUser.email.value}
                         error={this.state.newUser.email.error}
@@ -102,11 +147,11 @@ class UsersCreateController extends Component {
                         inline={true}
                     />
 
-                    <button type="submit" className="btn btn--positive margin-top--1" disabled={this.props.isSubmitting}>
+                    <button type="submit" className="btn btn--positive margin-top--1" disabled={this.state.isSubmitting}>
                         Create User
                     </button>
 
-                    {this.props.isSubmitting ? <div className="form__loader loader loader--dark margin-left--1"></div> : ""}
+                    {this.state.isSubmitting ? <div className="form__loader loader loader--dark margin-left--1"></div> : ""}
 
                 </form>
             </div>
