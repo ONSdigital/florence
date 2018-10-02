939213be38e3b4e2afc2c2313249c0b9d65fb6cc
Crispioso <crispin.merriman@gmail.com>
2018-10-02 14:12:50 +0100

    Fix broken unit tests

--- src/app/views/collections/CollectionsController.test.js
+++ src/app/views/collections/CollectionsController.test.js
@@ -133,7 +133,8 @@ const defaultProps = {
     collections: [],
     activeCollection: null,
     collectionsToDelete: {},
-    routes: [{}]
+    routes: [{}],
+    enableDatasetImport: false
 };
 
 const component = shallow(
@@ -274,7 +275,10 @@ describe("mapStateToProps function", () => {
                 active: {id: "an-example-collection-12345"},
                 toDelete: {"deleted-example-collection-12345": null}
             },
-            rootPath: "/florence"
+            rootPath: "/florence",
+            config: {
+                enableDatasetImport: false
+            }
         }
     };
 
