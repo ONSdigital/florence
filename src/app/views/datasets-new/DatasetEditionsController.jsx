524ef4f23c92ef8371c120ca5c0c11abdd76a23c
Jon Dewi Jones <jondewijones@gmail.com>
2018-11-19 10:26:11 +0000

    Add select an edition page

---
+++ src/app/views/datasets-new/DatasetEditionsController.jsx
@@ -0,0 +1,175 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { push } from 'react-router-redux';
+import PropTypes from 'prop-types';
+
+import datasets from '../../utilities/api-clients/datasets';
+import notifications from '../../utilities/notifications';
+import url from '../../utilities/url'
+
+import SimpleSelectableList from '../../components/simple-selectable-list/SimpleSelectableList';
+
+const propTypes = {
+
+}
+
+class DatasetEditionsController extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            fetchingDataset: false,
+            dataset: {},
+            fetchingEditions: false,
+            editions: [],
+        }
+
+    }
+
+    async componentWillMount() {
+        const datasetID = this.props.routeParams.datasetID;
+        this.setState({isFetchingEditions: true});
+        datasets.getEditions(datasetID).then(async (editions) => {
+
+            await this.getDataset(datasetID);
+            
+            this.setState({
+                isFetchingEditions: false, 
+                editions: this.mapDatasetEditionsToView(editions.items)
+            });
+ 
+        }).catch(error => {
+            switch (error.status) {
+                case(404): {
+                    const notification = {
+                        "type": "warning",
+                        "message": "No API route available for a list of editions.",
+                        isDismissable: true
+                    }
+                    notifications.add(notification)
+                    break;
+                }
+                case("RESPONSE_ERR"):{
+                    const notification = {
+                        "type": "warning",
+                        "message": "An error's occurred whilst trying to get a list of editions.",
+                        isDismissable: true
+                    }
+                    notifications.add(notification)
+                    break;
+                }
+                case("FETCH_ERR"): {
+                    const notification = {
+                        type: "warning",
+                        message: "There's been a network error whilst trying to get the submitted datasets. Please check you internet connection and try again in a few moments.",
+                        isDismissable: true
+                    }
+                    notifications.add(notification);
+                    break;
+                }
+                case("UNEXPECTED_ERR"): {
+                    const notification = {
+                        type: "warning",
+                        message: "An unexpected error has occurred whilst trying to get a list of editions.",
+                        isDismissable: true
+                    }
+                    notifications.add(notification);
+                    break;
+                }
+                default: {
+                    const notification = {
+                        type: "warning",
+                        message: "An unexpected error's occurred whilst trying to get a list of editions.",
+                        isDismissable: true
+                    }
+                    notifications.add(notification);
+                    break;
+                }
+            }
+            this.setState({isFetchingEditions: false});
+        });
+    }
+
+    getDataset = async(datasetID) => {
+        this.setState({isFetchingDatasets: true});
+        return datasets.get(datasetID).then(dataset => {
+            this.setState({dataset: this.mapDatasetToState(dataset)})
+            this.setState({isFetchingDatasets: false});
+            return dataset;
+        }).catch(error => {
+            console.error(`Error getting dataset (${datasetID}):\n`, error);
+            this.setState({isFetchingDatasets: false});
+        })
+    }
+
+    mapDatasetToState = datasetResponse => {
+        try {
+            const dataset = datasetResponse.current || datasetResponse.next || datasetResponse;
+            return {
+                title: dataset.title
+            }
+        } catch (error) {
+            const notification = {
+                type: "warning",
+                message: "An unexpected error occurred when trying to get dataset details, so some functionality in Florence may not work as expected. Try refreshing the page",
+                isDismissable: true
+            }
+            notifications.add(notification);
+            console.error("Error getting dataset details to state:\n", error);
+        }
+    }
+
+    mapDatasetEditionsToView = editions => {
+        try {
+            return editions.map(edition => {
+                return {
+                    title: this.state.dataset.title, 
+                    id: edition.current.edition,
+                    url:  this.props.location.pathname + "/editions/" + edition.current.edition,
+                    details: [
+                        "Edition: " + edition.current.edition,
+                        "Release date: XX/XX/XXXX"
+                    ]
+                }
+            })
+        } catch (error) {
+            const notification = {
+                type: "warning",
+                message: "An unexpected error occurred when trying to get edition details, so some functionality in Florence may not work as expected. Try refreshing the page",
+                isDismissable: true
+            }
+            notifications.add(notification);
+            console.error("Error getting mapping editions to state:\n", error);
+        }
+    }
+
+    handleBackButton = () => {
+        const previousUrl = url.resolve("../");
+        this.props.dispatch(push(previousUrl));
+    }
+
+    render() {
+        return (
+            <div className="grid grid--justify-center">
+                <div className="grid__col-9">
+                    <div className="margin-top--2">
+                        &#9664; <button type="button" className="btn btn--link" onClick={this.handleBackButton}>Back</button>
+                    </div>
+                    <h1 className="margin-top--1 margin-bottom--1">Select an Edition</h1>
+                    <p><span className="bold">Dataset</span>: {this.state.dataset.title ? this.state.dataset.title : "loading..."}</p>
+                    <SimpleSelectableList rows={this.state.editions} isFetchingData={this.state.fetchingEditions}/>
+               </div>
+            </div>
+        )
+    }
+}
+
+DatasetEditionsController.propTypes = propTypes;
+
+function mapStateToProps(state) {
+    return {
+        
+    }
+}
+export default connect(mapStateToProps)(DatasetEditionsController);
+
