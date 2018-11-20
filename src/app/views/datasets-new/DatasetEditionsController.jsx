e6aabf6af40bf63ca123881181fc8dca5f2654bd
Jon Dewi Jones <jondewijones@gmail.com>
2018-11-20 17:12:41 +0000

    Add createListOfEditions and data getters

--- src/app/views/datasets-new/DatasetEditionsController.jsx
+++ src/app/views/datasets-new/DatasetEditionsController.jsx
@@ -6,6 +6,7 @@ import PropTypes from 'prop-types';
 import datasets from '../../utilities/api-clients/datasets';
 import notifications from '../../utilities/notifications';
 import url from '../../utilities/url'
+import date from '../../utilities/date'
 
 import SimpleSelectableList from '../../components/simple-selectable-list/SimpleSelectableList';
 
@@ -28,66 +29,95 @@ class DatasetEditionsController extends Component {
 
     async componentWillMount() {
         const datasetID = this.props.routeParams.datasetID;
-        this.setState({isFetchingEditions: true});
-        datasets.getEditions(datasetID).then(async (editions) => {
+        this.createListOfEditions(datasetID);
+        // this.setState({isFetchingEditions: true});
+        // datasets.getEditions(datasetID).then(async (editions) => {
 
-            await this.getDataset(datasetID);
+        //     await this.getDataset(datasetID);
             
-            this.setState({
-                isFetchingEditions: false, 
-                editions: this.mapDatasetEditionsToView(editions.items)
-            });
+        //     this.setState({
+        //         isFetchingEditions: false, 
+        //         editions: this.mapDatasetEditionsToView(editions.items)
+        //     });
  
-        }).catch(error => {
-            switch (error.status) {
-                case(404): {
-                    const notification = {
-                        "type": "warning",
-                        "message": "No API route available for a list of editions.",
-                        isDismissable: true
-                    }
-                    notifications.add(notification)
-                    break;
-                }
-                case("RESPONSE_ERR"):{
-                    const notification = {
-                        "type": "warning",
-                        "message": "An error's occurred whilst trying to get a list of editions.",
-                        isDismissable: true
-                    }
-                    notifications.add(notification)
-                    break;
-                }
-                case("FETCH_ERR"): {
-                    const notification = {
-                        type: "warning",
-                        message: "There's been a network error whilst trying to get the submitted datasets. Please check you internet connection and try again in a few moments.",
-                        isDismissable: true
-                    }
-                    notifications.add(notification);
-                    break;
-                }
-                case("UNEXPECTED_ERR"): {
-                    const notification = {
-                        type: "warning",
-                        message: "An unexpected error has occurred whilst trying to get a list of editions.",
-                        isDismissable: true
-                    }
-                    notifications.add(notification);
-                    break;
-                }
-                default: {
-                    const notification = {
-                        type: "warning",
-                        message: "An unexpected error's occurred whilst trying to get a list of editions.",
-                        isDismissable: true
-                    }
-                    notifications.add(notification);
-                    break;
-                }
-            }
-            this.setState({isFetchingEditions: false});
-        });
+        // }).catch(error => {
+        //     switch (error.status) {
+        //         case(404): {
+        //             const notification = {
+        //                 "type": "warning",
+        //                 "message": "No API route available for a list of editions.",
+        //                 isDismissable: true
+        //             }
+        //             notifications.add(notification)
+        //             break;
+        //         }
+        //         case("RESPONSE_ERR"):{
+        //             const notification = {
+        //                 "type": "warning",
+        //                 "message": "An error's occurred whilst trying to get a list of editions.",
+        //                 isDismissable: true
+        //             }
+        //             notifications.add(notification)
+        //             break;
+        //         }
+        //         case("FETCH_ERR"): {
+        //             const notification = {
+        //                 type: "warning",
+        //                 message: "There's been a network error whilst trying to get the submitted datasets. Please check you internet connection and try again in a few moments.",
+        //                 isDismissable: true
+        //             }
+        //             notifications.add(notification);
+        //             break;
+        //         }
+        //         case("UNEXPECTED_ERR"): {
+        //             const notification = {
+        //                 type: "warning",
+        //                 message: "An unexpected error has occurred whilst trying to get a list of editions.",
+        //                 isDismissable: true
+        //             }
+        //             notifications.add(notification);
+        //             break;
+        //         }
+        //         default: {
+        //             const notification = {
+        //                 type: "warning",
+        //                 message: "An unexpected error's occurred whilst trying to get a list of editions.",
+        //                 isDismissable: true
+        //             }
+        //             notifications.add(notification);
+        //             break;
+        //         }
+        //     }
+        //     this.setState({isFetchingEditions: false});
+        // });
+    }
+
+    createListOfEditions = async(datasetID) => {
+        this.setState({isFetchingDatasets: true, isFetchingEditions: true, isFetchingLatestVersion: true});
+        const dataset = await this.getDataset(datasetID);
+        const editions = await this.getEditions(datasetID);
+        const versionPromises = editions.map(async(edition) => {
+            return await this.getVersion(datasetID, edition.id, edition.latestVersion);
+        })
+        const allVersions = await Promise.all(versionPromises).then(version => {
+            return version;
+        })
+        const versionDates = allVersions.map(version => {
+            return date.format(version.release_date, "dd mmmm yyyy")
+        })
+
+        const editionsWithLatestDate = editions.map((edition, index) => {
+            edition.details[1] = `Release date: ${versionDates[index]}`
+            return edition;
+        })
+
+        console.log(editionsWithLatestDate);
+
+        this.setState({editions: editionsWithLatestDate});
+
+
+        //console.log(await this.getVersion(datasetID, editions[0].id, editions[0].latestVersion))
+        console.log(versionDates);
     }
 
     getDataset = async(datasetID) => {
@@ -95,7 +125,7 @@ class DatasetEditionsController extends Component {
         return datasets.get(datasetID).then(dataset => {
             this.setState({dataset: this.mapDatasetToState(dataset)})
             this.setState({isFetchingDatasets: false});
-            return dataset;
+            return this.mapDatasetToState(dataset);
         }).catch(error => {
             console.error(`Error getting dataset (${datasetID}):\n`, error);
             this.setState({isFetchingDatasets: false});
@@ -119,6 +149,12 @@ class DatasetEditionsController extends Component {
         }
     }
 
+    getEditions = async(datasetID) => {
+        return datasets.getEditions(datasetID).then(editions => {
+            return this.mapDatasetEditionsToView(editions.items);
+        })
+    }
+
     mapDatasetEditionsToView = editions => {
         try {
             return editions.map(edition => {
@@ -128,8 +164,9 @@ class DatasetEditionsController extends Component {
                     url:  this.props.location.pathname + "/editions/" + edition.current.edition,
                     details: [
                         "Edition: " + edition.current.edition,
-                        "Release date: XX/XX/XXXX"
-                    ]
+                        "Release date: loading..."
+                    ],
+                    latestVersion: edition.current.links.latest_version.id
                 }
             })
         } catch (error) {
@@ -141,6 +178,12 @@ class DatasetEditionsController extends Component {
             notifications.add(notification);
             console.error("Error getting mapping editions to state:\n", error);
         }
+    }
+
+    getVersion = (datasetID, edition, version) => {
+        return datasets.getVersion(datasetID, edition, "3").then(versionResp => {
+            return versionResp;
+        })
     }
 
     handleBackButton = () => {
@@ -156,7 +199,7 @@ class DatasetEditionsController extends Component {
                         &#9664; <button type="button" className="btn btn--link" onClick={this.handleBackButton}>Back</button>
                     </div>
                     <h1 className="margin-top--1 margin-bottom--1">Select an Edition</h1>
-                    <p><span className="bold">Dataset</span>: {this.state.dataset.title ? this.state.dataset.title : "loading..."}</p>
+                    <p className="margin-bottom--1 font-size--18"><span className="font-weight--600">Dataset</span>: {this.state.dataset.title ? this.state.dataset.title : "loading..."}</p>
                     <SimpleSelectableList rows={this.state.editions} isFetchingData={this.state.fetchingEditions}/>
                </div>
             </div>
