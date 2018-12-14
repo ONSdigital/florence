1cbfd7d2a4be2b0930ffd6d1423488229cf8965a
Jon Dewi Jones <jondewijones@gmail.com>
2018-12-14 14:27:28 +0000

    Clean up formatting

--- src/app/views/datasets-new/edit-metadata/DatasetMetadata.jsx
+++ src/app/views/datasets-new/edit-metadata/DatasetMetadata.jsx
@@ -25,12 +25,38 @@ class DatasetMetadata extends Component {
                     <p className="margin-bottom--1 font-size--18"><span className="font-weight--600">Version</span>: {this.props.metadata.version ? this.props.metadata.version : "loading..."}</p>
 
                     <h2>Title</h2>
-                    <Input id="title" value={this.props.metadata.title} onChange={this.props.handleStringInputChange} disabled={this.props.isSaving || this.props.isGettingData}/>
+                    <Input id="title" 
+                        value={this.props.metadata.title} 
+                        onChange={this.props.handleStringInputChange} 
+                        disabled={this.props.isSaving || this.props.isGettingData}
+                    />
 
                     <h2>Release dates</h2>
-                    <Input id="release-date" name="releaseDate" label="Release date" type="date" onChange={this.props.handleDateInputChange} value={this.props.metadata.releaseDate && date.format(this.props.metadata.releaseDate, "yyyy-mm-dd")} disabled={this.props.isSaving || this.props.isGettingData || this.props.versionIsPublished}/>
-                    <Input id="next-release" name="nextReleaseDate" label="Next release date" type="date" onChange={this.props.handleDateInputChange} value={this.props.metadata.nextReleaseDate && date.format(this.props.metadata.nextReleaseDate, "yyyy-mm-dd")} disabled={this.props.isSaving || this.props.isGettingData}/>
-                    <Input id="release-frequency" name="releaseFrequency" label="Release frequency" onChange={this.props.handleStringInputChange} value={this.props.metadata.releaseFrequency} disabled={this.props.isSaving || this.props.isGettingData}/>
+                    <Input id="release-date" 
+                        name="releaseDate" 
+                        label="Release date" 
+                        type="date" 
+                        onChange={this.props.handleDateInputChange} 
+                        value={this.props.metadata.releaseDate && date.format(this.props.metadata.releaseDate, "yyyy-mm-dd")} 
+                        disabled={this.props.isSaving || this.props.isGettingData || this.props.versionIsPublished}
+                    />
+
+                    <Input id="next-release" 
+                        name="nextReleaseDate" 
+                        label="Next release date" 
+                        type="date" 
+                        onChange={this.props.handleDateInputChange} 
+                        value={this.props.metadata.nextReleaseDate && date.format(this.props.metadata.nextReleaseDate, "yyyy-mm-dd")} 
+                        disabled={this.props.isSaving || this.props.isGettingData}
+                    />
+
+                    <Input id="release-frequency" 
+                        name="releaseFrequency" 
+                        label="Release frequency" 
+                        onChange={this.props.handleStringInputChange} 
+                        value={this.props.metadata.releaseFrequency} 
+                        disabled={this.props.isSaving || this.props.isGettingData}
+                    />
 
                     <h2>Notices</h2>
                     <p className="margin-bottom--1">Add an alert, correction, change summary or usage note.</p>
@@ -44,22 +70,57 @@ class DatasetMetadata extends Component {
                     />
                     
                     <h2 className="margin-top--1">About</h2>
-                    <Input id="summary" label="Summary" type="textarea" value={this.props.metadata.summary} onChange={this.props.handleStringInputChange} disabled={this.props.isSaving || this.props.isGettingData}/>
-                    <Input id="unit-of-measure" name="unit" label="Unit of measure" type="input" value={this.props.metadata.unitOfMeasure} onChange={this.props.handleStringInputChange}disabled={this.props.isSaving || this.props.isGettingData}/>
+                    <Input id="summary" 
+                        label="Summary" type="textarea" v
+                        alue={this.props.metadata.summary} 
+                        onChange={this.props.handleStringInputChange} 
+                        disabled={this.props.isSaving || this.props.isGettingData}
+                    />
+
+                    <Input id="unit-of-measure" 
+                        name="unit" 
+                        label="Unit of measure" 
+                        type="input" 
+                        value={this.props.metadata.unitOfMeasure} 
+                        onChange={this.props.handleStringInputChange} 
+                        disabled={this.props.isSaving || this.props.isGettingData}
+                    />
 
                     <h2>Dimensions</h2>
                     {this.props.metadata.dimensions.map(dimension => {
                         return (
                             <div key={`dimension-${dimension.id}`}>
-                            <Input id={`dimension-title-${dimension.id}`} data-dimension-id={dimension.id} label="Title" value={dimension.name} onChange={this.props.handleDimensionNameChange} disabled={this.props.isSaving || this.props.isGettingData || this.props.versionIsPublished}/>
-                            <Input id={`dimension-description-${dimension.id}`} label="Description" type="textarea" value={dimension.description} onChange={this.props.handleDimensionDescriptionChange} disabled={this.props.isSaving || this.props.isGettingData || this.props.versionIsPublished}/>
+                                <Input id={`dimension-title-${dimension.id}`} 
+                                    label="Title" 
+                                    value={dimension.name} 
+                                    onChange={this.props.handleDimensionNameChange} 
+                                    disabled={this.props.isSaving || this.props.isGettingData || this.props.versionIsPublished}
+                                />
+                                <Input id={`dimension-description-${dimension.id}`} 
+                                    label="Description" 
+                                    type="textarea" 
+                                    value={dimension.description} 
+                                    onChange={this.props.handleDimensionDescriptionChange} 
+                                    disabled={this.props.isSaving || this.props.isGettingData || this.props.versionIsPublished}
+                                />
                             </div>
                         )
                     })} 
 
                     <h2>Meta</h2>
-                    <Input id="keywords" label="Keywords" value={this.props.metadata.keywords.join(", ")} disabled={this.props.isSaving || this.props.isGettingData}/>
-                    <Input id="licence" label="Licence" onChange={this.props.handleStringInputChange} value={this.props.metadata.licence} disabled={this.props.isSaving || this.props.isGettingData}/>
+                    <Input id="keywords" 
+                        label="Keywords" 
+                        value={this.props.metadata.keywords.join(", ")} 
+                        disabled={this.props.isSaving || this.props.isGettingData}
+                    />
+
+                    <Input id="licence" 
+                        label="Licence" 
+                        onChange={this.props.handleStringInputChange} 
+                        value={this.props.metadata.licence} 
+                        disabled={this.props.isSaving || this.props.isGettingData}
+                    />
+
                     <RadioGroup groupName="national-statistic" 
                         radioData={[
                             {id: "national-statistic-yes", value: "true", label: "Yes"},
@@ -72,9 +133,29 @@ class DatasetMetadata extends Component {
                     /> 
 
                     <h2>Contact details</h2>
-                    <Input id="contact-name" name="contactName" label="Contact name" onChange={this.props.handleStringInputChange} value={this.props.metadata.contactName} disabled={this.props.isSaving || this.props.isGettingData}/>
-                    <Input id="contact-email" name="contactEmail" label="Contact email" onChange={this.props.handleStringInputChange} value={this.props.metadata.contactEmail} disabled={this.props.isSaving || this.props.isGettingData}/>
-                    <Input id="contact-telephone" name="contactTelephone" label="Contact telephone" onChange={this.props.handleStringInputChange} value={this.props.metadata.contactTelephone} disabled={this.props.isSaving || this.props.isGettingData}/>
+                    <Input id="contact-name" 
+                        name="contactName" 
+                        label="Contact name" 
+                        onChange={this.props.handleStringInputChange} 
+                        value={this.props.metadata.contactName} 
+                        disabled={this.props.isSaving || this.props.isGettingData}
+                    />
+
+                    <Input id="contact-email" 
+                        name="contactEmail" 
+                        label="Contact email" 
+                        onChange={this.props.handleStringInputChange} 
+                        value={this.props.metadata.contactEmail} 
+                        disabled={this.props.isSaving || this.props.isGettingData}
+                    />
+
+                    <Input id="contact-telephone" 
+                        name="contactTelephone" 
+                        label="Contact telephone" 
+                        onChange={this.props.handleStringInputChange} 
+                        value={this.props.metadata.contactTelephone} 
+                        disabled={this.props.isSaving || this.props.isGettingData}
+                    />
 
                     <h2>Related links</h2>
                     <SimpleEditableList addText={"Add a related link"} 
@@ -87,10 +168,17 @@ class DatasetMetadata extends Component {
                     />
 
                     <div className="margin-top--2">
-                    <button type="button" className="btn btn--primary margin-right--1" onClick={this.props.handleSave} disabled={this.props.isSaving || this.props.isGettingData}>Save</button>
-                    <button type="button" className="btn btn--positive margin-right--1" disabled={this.props.isSaving || this.props.isGettingData}>Save and submit for review</button>
-                    <Link to="/preview">Preview</Link>
-                    {this.props.isSaving && <div className="form__loader loader loader--dark margin-left--1"></div>}
+                        <button type="button" 
+                            className="btn btn--primary margin-right--1" 
+                            onClick={this.props.handleSave} 
+                            disabled={this.props.isSaving || this.props.isGettingData}>Save
+                        </button>
+                        <button type="button" 
+                            className="btn btn--positive margin-right--1" 
+                            disabled={this.props.isSaving || this.props.isGettingData}>Save and submit for review
+                        </button>
+                        <Link to="/preview">Preview</Link>
+                        {this.props.isSaving && <div className="form__loader loader loader--dark margin-left--1"></div>}
                     </div>
                 </div>
         );
