a6257f18810e0bb611b6bd589925ba9f7c5a847b
Jon Dewi Jones <jondewijones@gmail.com>
2019-01-25 17:56:29 +0000

    Add http, auth and severity methods

--- src/app/utilities/log.ts
+++ src/app/utilities/log.ts
@@ -1,22 +1,23 @@
 
 interface Loggable {
-    attach(event: object): object,
+    attach(event: EventData): EventData,
 }
 
-interface LogEvent {
+interface EventData {
     created_at: string,
     namespace: string,
     event: string,
+    client_loaded_at: string,
     //trace_id: string,
     //span_id: string,
-    //severity: number,
-    http?: HttpEvent,
-    auth?: AuthEvent,
-    data?: object,
+    severity: number,
+    http?: EventHttp,
+    auth?: EventAuth,
+    data?: Map<string,string>,
     testString?: string
 }
 
-interface HttpEvent {
+interface EventHttp {
     method: string,
     path: string,
     query: string,
@@ -29,59 +30,127 @@ interface HttpEvent {
     duration: number
 }
 
-interface AuthEvent {
+interface EventAuth {
     identity: string,
     identity_type: string
 }
 
-export interface Http {
-    httpEvent: HttpEvent
+interface Http {
+    httpEvent: EventHttp
+}
+interface Auth {
+    authEvent: EventAuth
 }
 
-export interface Auth {
-    authEvent: AuthEvent
+interface Fatal {
+    severity: number
 }
+
+interface Error{
+    severity: number
+}
+
+interface Warn {
+    severity: number
+}
+
+interface Info {
+    severity: number
+}
+
+interface Data {
+    dataEvent: Map<string,string>,
+}
+
+const client_loaded_at = new Date(window.performance.timing.navigationStart + window.performance.now()).toISOString();
 
 export default class log {
-
-
     static event = (event: string, ...opts: any[]): void => {
-
-        const eventLog: LogEvent = {
+        const eventData: EventData = {
             created_at: new Date(window.performance.timing.navigationStart + window.performance.now()).toISOString(),
             namespace: "florence",
             event: event,
+            client_loaded_at: client_loaded_at,
+            severity: 3
         }
+
         if (opts) {
             opts.map(opt => {
-                opt.attach(eventLog)
+                opt.attach(eventData)
             })
         }
-        console.log("I'm logging something =>", eventLog)
+        console.log("LOG =>", eventData)
+    }
+    static http = (httpEvent: EventHttp): Http => {
+        return new Http(httpEvent);
+    }
+    static auth = (authEvent: EventAuth): Auth => {
+        return new Auth(authEvent);
+    }
+    static data = (dataEvent: Map<string,string>): Data => {
+        return new Data(dataEvent);
     }
 
+    static fatal = (): Fatal => new Fatal()
+    static error = (): Error => new Error()
+    static warn = (): Warn => new Warn()
+    static info = (): Info => new Info()
 }
 
-export class Http implements Loggable {
-    constructor(httpEvent: HttpEvent) {
+class Http implements Loggable {
+    constructor(httpEvent: EventHttp) {
         this.httpEvent = httpEvent;
-        this.attach = this.attach;
     }
-
-    attach = (event: LogEvent) => {
+    attach = (event: EventData): EventData => {
         event.http = this.httpEvent
-        return event
+        return event;
     }
 }
 
-export class Auth implements Loggable {
-    constructor(authEvent: AuthEvent) {
+class Auth implements Loggable {
+    constructor(authEvent: EventAuth) {
         this.authEvent = authEvent;
-        this.attach = this.attach;
     }
-
-    attach = (event: LogEvent) => {
+    attach = (event: EventData): EventData => {
         event.auth = this.authEvent
-        return event
+        return event;
+    }
+}
+
+class Data implements Loggable {
+    constructor(dataEvent: Map<string,string>) {
+        this.dataEvent = dataEvent;
+    }
+    attach = (event: EventData): EventData => {
+        event.data = this.dataEvent
+        return event;
+    }
+}
+
+class Fatal implements Loggable {
+    attach = (event: EventData): EventData => {
+        event.severity = 0
+        return event;
+    }
+}
+
+class Error implements Loggable {
+    attach = (event: EventData): EventData => {
+        event.severity = 1
+        return event;
+    }
+}
+
+class Warn implements Loggable {
+    attach = (event: EventData): EventData => {
+        event.severity = 2
+        return event;
+    }
+}
+
+class Info implements Loggable {
+    attach = (event: EventData): EventData => {
+        event.severity = 3
+        return event;
     }
 }
