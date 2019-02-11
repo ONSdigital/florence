55529791a92891d015e0c05132ac74468327f2b6
Jon Dewi Jones <jondewijones@gmail.com>
2019-02-11 20:31:16 +0000

    Remove test console log

--- src/app/utilities/logging/log.js
+++ src/app/utilities/logging/log.js
@@ -27,7 +27,6 @@ export default class log {
                 opt.attach(eventData)
             })
         }
-        console.log("LOG =>", eventData);
         websocket.send(`log:${JSON.stringify(eventData)}`);
         return eventData;
     }
