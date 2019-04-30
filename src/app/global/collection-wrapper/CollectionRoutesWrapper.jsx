171d4c015b9d9994bb436c595537ae89f3ab84ac
Jon Dewi Jones <jondewijones@gmail.com>
2019-04-30 20:05:55 +0100

    Tidy up error messages

--- src/app/global/collection-wrapper/CollectionRoutesWrapper.jsx
+++ src/app/global/collection-wrapper/CollectionRoutesWrapper.jsx
@@ -27,7 +27,7 @@ export class CollectionRoutesWrapper extends Component {
     componentDidMount = () => {
         const collectionID = this.props.params.collectionID
         if (!collectionID) {
-            this.props.dispatch(push(`${this.props.rootPath}/collections`))
+            this.props.dispatch(push(`${this.props.rootPath}/collections`));
             return;
         }
         if (this.props.workingOn) {
@@ -36,10 +36,9 @@ export class CollectionRoutesWrapper extends Component {
         collections.get(collectionID).then(response => {
             const workingOn = this.mapCollectionResponseToWorkingOnState(response);
             if (workingOn) {
-                this.props.dispatch(updateWorkingOn(workingOn.id, workingOn.name, workingOn.url, response))
+                this.props.dispatch(updateWorkingOn(workingOn.id, workingOn.name, workingOn.url, response));
             }
         }).catch(error => {
-            console.log(error);
             switch(error.status) {
                 case(401): {
                     // do nothing - this is handled by the request function itself
@@ -48,27 +47,27 @@ export class CollectionRoutesWrapper extends Component {
                 case(404): {
                     const notification = {
                         type: 'neutral',
-                        message: `Collection couldn't be found so you've been redirected to the collections screen`,
+                        message: "Collection couldn't be found so you've been redirected to the collections screen",
                         autoDismiss: 5000
                     };
                     notifications.add(notification);
-                    this.props.dispatch(push(`${this.props.rootPath}/collections`))
+                    this.props.dispatch(push(`${this.props.rootPath}/collections`));
                     break;
                 }
                 case(403): {
                     const notification = {
                         type: 'neutral',
-                        message: `You don't have permissions to access this collection so you've been redirect to the collections screen`,
+                        message: "You don't have permissions to access this collection so you've been redirect to the collections screen",
                         autoDismiss: 5000
                     };
                     notifications.add(notification);
-                    this.props.dispatch(push(`${this.props.rootPath}/collections`))
+                    this.props.dispatch(push(`${this.props.rootPath}/collections`));
                     break;
                 }
                 case('FETCH_ERR'): {
                     const notification = {
                         type: 'warning',
-                        message: `There was a network error whilst getting this collection, please check your connection and refresh the page`,
+                        message: "There was a network error whilst getting details about this collection, please check your connection and refresh the page",
                         autoDismiss: 5000
                     };
                     notifications.add(notification);
@@ -77,7 +76,7 @@ export class CollectionRoutesWrapper extends Component {
                 default: {
                     const notification = {
                         type: 'warning',
-                        message: 'An unexpected error occurred whilst getting this collection',
+                        message: "An unexpected error occurred whilst getting details about this collection, please refresh the page",
                         autoDismiss: 5000
                     };
                     notifications.add(notification);
@@ -97,7 +96,7 @@ export class CollectionRoutesWrapper extends Component {
         } catch (error) {
             log.event("error mapping collection response to 'working on' state", log.error(error), 
                 log.data({collectionID: this.props.params.collectionID, url: this.props.location.pathname})
-            )
+            );
             console.error("Error mapping collection response to 'working on' state", error);
         }
     }
