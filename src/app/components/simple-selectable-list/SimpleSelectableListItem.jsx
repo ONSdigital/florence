1f9beead1256619d76b61c1e1570c463ed8d97ce
Jon Dewi Jones <jondewijones@gmail.com>
2019-05-07 18:40:04 +0100

    Add styling and option to disabled an item on simple selectable list

--- src/app/components/simple-selectable-list/SimpleSelectableListItem.jsx
+++ src/app/components/simple-selectable-list/SimpleSelectableListItem.jsx
@@ -6,7 +6,8 @@ const propTypes = {
     title: PropTypes.string.isRequired,
     id: PropTypes.string.isRequired,
     url: PropTypes.string.isRequired,
-    details: PropTypes.arrayOf(PropTypes.string)
+    details: PropTypes.arrayOf(PropTypes.string),
+    disabled: PropTypes.disabled
 }
 
 export default class SimpleSelectableListItem extends Component {
@@ -15,10 +16,15 @@ export default class SimpleSelectableListItem extends Component {
     }
 
     render() {
+        console.log(this.props)
         const details = this.props.details || [];
         return (
             <li className="simple-select-list__item">
-                <Link to={this.props.url}><h2>{this.props.title}</h2></Link>
+                { this.props.disabled ? 
+                    <h2 className="simple-select-list__item--disabled">{this.props.title}</h2> 
+                : 
+                    <Link to={this.props.url}><h2>{this.props.title}</h2></Link> 
+                }
                 {details.map((detail, i) => {
                     return <p key={`detail-${i}`}>{detail}</p>
                 })}
