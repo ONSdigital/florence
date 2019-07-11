c8a4bbe08398c3f9845f39a3fd071404f859c630
Jon Dewi Jones <jondewijones@gmail.com>
2019-07-11 09:48:51 +0100

    Remove debug console.log

--- src/app/views/datasets-new/edit-metadata/DatasetMetadataController.jsx
+++ src/app/views/datasets-new/edit-metadata/DatasetMetadataController.jsx
@@ -365,7 +365,6 @@ export class DatasetMetadataController extends Component {
     mapVersionCollectionStateToState = versions => {
         try {
             const version = versions.find(version => {
-                console.log(version)
                 return version.version === this.props.params.versionID && 
                         version.edition === this.props.params.editionID && 
                         version.id === this.props.params.datasetID;
