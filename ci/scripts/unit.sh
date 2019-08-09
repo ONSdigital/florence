fc60faf308048f03f7cd245065da5c6488196ff4
Daniel W <danieldwalford@gmail.com>
2019-08-09 08:05:37 +0100

    Added Exit on format issues found

--- ci/scripts/unit.sh
+++ ci/scripts/unit.sh
@@ -5,6 +5,6 @@ export GOPATH=$(pwd)/go
 pushd $GOPATH/src/github.com/ONSdigital/florence
 make test
   if  prettier --check "src/app/**/*.js" | tee /dev/stderr | grep -q "Code style issues found"; then
-    #exit 123
+    exit 1
   fi
 popd
