f9ae008ddb5f548233776461cfd99c78fac5808c
David Subiros <subiros.david@gmail.com>
2020-02-06 11:16:59 +0000

    Fixed Makefile and go generate

--- assets/assets.go
+++ assets/assets.go
@@ -1,4 +1,4 @@
-//go:generate go get github.com/jteeuwen/go-bindata/go-bindata
+//go:generate go get -u github.com/jteeuwen/go-bindata/go-bindata
 //go:generate go-bindata -ignore=node_modules -o prod.go -pkg assets ../dist/...
 
 package assets
