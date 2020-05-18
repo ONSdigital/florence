abec924d6306fc7636706352e3716caab2bcb5ea
Ravi <ravipradhan@protonmail.com>
2020-05-18 12:00:17 +0100

    created review actions component and started implementing editable list component into edit homepage

--- src/app/views/homepage/edit/EditHomepageController.jsx
+++ src/app/views/homepage/edit/EditHomepageController.jsx
@@ -1,5 +1,9 @@
 import React, { Component } from "react";
 import EditHomepage from "./EditHompage";
+import PropTypes from "prop-types";
+
+import url from "../../../utilities/url";
+import { push } from "react-router-redux";
 
 class EditHomepageController extends Component {
     constructor(props) {
@@ -10,13 +14,30 @@ class EditHomepageController extends Component {
         };
     }
 
+    handleBackButton = () => {
+        const previousUrl = url.resolve("../../");
+        this.props.dispatch(push(previousUrl));
+    };
+
     render() {
         return (
             <div className="grid grid--justify-center">
-                <EditHomepage />
+                <EditHomepage highlightedContent={this.state.highlightedContent} handleBackButton={this.handleBackButton} />
             </div>
         );
     }
 }
+
+const propTypes = {
+    location: PropTypes.shape({
+        pathname: PropTypes.string.isRequired
+    }),
+    params: PropTypes.shape({
+        collectionID: PropTypes.string.isRequired
+    }),
+    dispatch: PropTypes.func.isRequired
+};
+
+EditHomepageController.propTypes = propTypes;
 
 export default EditHomepageController;
--- src/app/views/homepage/edit/EditHompage.jsx
+++ src/app/views/homepage/edit/EditHompage.jsx
@@ -1,9 +1,42 @@
 import React, { Component } from "react";
+import PropTypes from "prop-types";
+import SimpleEditableList from "../../../components/simple-editable-list/SimpleEditableList";
 
 class EditHomepage extends Component {
     render() {
-        return <div>This is the Edit Homepage presentational component</div>;
+        return (
+            <div className="grid__col-6 margin-bottom--4">
+                <div className="margin-top--2">
+                    &#9664;{" "}
+                    <button type="button" className="btn btn--link" onClick={this.props.handleBackButton}>
+                        Back
+                    </button>
+                </div>
+                <h1 className="margin-top--1 margin-bottom--1">Edit Homepage</h1>
+                <h2 className="margin-top--1 margin-bottom--1">Headlines</h2>
+                <SimpleEditableList
+                    addText={"Add a dataset"}
+                    fields={this.props.highlightedContent}
+                    editingStateFieldName="homepageHeadline"
+                    handleAddClick={this.props.handleSimpleEditableListAdd}
+                    handleEditClick={this.props.handleSimpleEditableListEdit}
+                    handleDeleteClick={this.props.handleSimpleEditableListDelete}
+                    disableActions={this.props.disableForm}
+                />
+            </div>
+        );
     }
 }
+
+const propTypes = {
+    highlightedContent: PropTypes.array,
+    handleBackButton: PropTypes.func.isRequired,
+    handleSimpleEditableListAdd: PropTypes.func.isRequired,
+    handleSimpleEditableListDelete: PropTypes.func.isRequired,
+    handleSimpleEditableListEdit: PropTypes.func.isRequired,
+    disableForm: PropTypes.bool.isRequired
+};
+
+EditHomepage.propTypes = propTypes;
 
 export default EditHomepage;
