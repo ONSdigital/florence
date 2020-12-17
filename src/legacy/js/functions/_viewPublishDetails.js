fcaa45ba760f25f50412109625ef36a677dbd768
Jon Dewi Jones <jondewijones@gmail.com>
2020-12-17 19:09:44 +0000

    Remove console.logs

--- src/legacy/js/functions/_viewPublishDetails.js
+++ src/legacy/js/functions/_viewPublishDetails.js
@@ -65,13 +65,11 @@ function viewPublishDetails(collections) {
 
         $('.btn-collection-publish').click(function () {
             var collection = $(this).closest('.js-accordion').find('.collection-name').attr('data-id');
-            console.log(collection);
             publish(collection);
         });
 
         $('.btn-collection-unlock').click(function () {
             var collection = $(this).closest('.js-accordion').find('.collection-name').attr('data-id');
-            console.log(collection);
 
             if (result.date !== manual) {
                 swal({
