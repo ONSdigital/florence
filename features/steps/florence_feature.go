2d9614b72f418d1621c0bcdd894c1e3e59babf48
Jon Acker <jon.acker@yoyo.org>
2021-03-19 11:33:43 +0200

    Almost complete feature of adding new collection

--- features/steps/florence_feature.go
+++ features/steps/florence_feature.go
@@ -19,8 +19,15 @@ import (
 	"os"
 	"os/signal"
 	"syscall"
+	"testing"
 	"time"
 )
+
+type Chrome struct {
+	execAllocatorCanceller context.CancelFunc
+	ctxCanceller           context.CancelFunc
+	ctx                    context.Context
+}
 
 type FlorenceFeature struct {
 	ErrorFeature  componenttest.ErrorFeature
@@ -29,17 +36,18 @@ type FlorenceFeature struct {
 	HTTPServer    *http.Server
 	ctx           context.Context
 	FakeApiRouter *httpfake.HTTPFake
+	chrome        Chrome
+	LoggedInUser  string
 }
 
-func NewFlorenceFeature() (*FlorenceFeature, error) {
+func NewFlorenceFeature(t *testing.T) (*FlorenceFeature, error) {
 
 	f := &FlorenceFeature{
 		HTTPServer:    &http.Server{},
 		errorChan:     make(chan error),
-		FakeApiRouter: httpfake.New(),
+		FakeApiRouter: httpfake.New(httpfake.WithTesting(t)),
 		ctx:           context.Background(),
 	}
-
 
 	signals := make(chan os.Signal, 1)
 	signal.Notify(signals, syscall.SIGINT, syscall.SIGTERM)
@@ -51,75 +59,114 @@ func NewFlorenceFeature() (*FlorenceFeature, error) {
 
 	cfg.APIRouterURL = f.FakeApiRouter.ResolveURL("")
 
-	f.FakeApiRouter.NewHandler().Post("/ping").Reply(200)
-
 	initFunctions := &initialiser.InitialiserMock{
-		DoGetHealthCheckFunc:  f.DoGetHealthcheckOk,
 		DoGetHTTPServerFunc:   f.DoGetHTTPServer,
-		DoGetHealthClientFunc: f.DoGetHealthClient,
+		DoGetHealthCheckFunc:  DoGetHealthcheckOk,
+		DoGetHealthClientFunc: DoGetHealthClient,
 	}
 
 	serviceList := service.NewServiceList(initFunctions)
 
-	go func() {
-		f.svc, _ = service.Run(f.ctx, cfg, serviceList, "1", "", "", f.errorChan)
-
-		// blocks until an os interrupt or a fatal error occurs
-		select {
-		case err := <-f.errorChan:
-			dplog.Event(f.ctx, "service error received", dplog.ERROR, dplog.Error(err))
-		case sig := <-signals:
-			dplog.Event(f.ctx, "os signal received", dplog.Data{"signal": sig}, dplog.INFO)
-		}
-	}()
+	f.runApplication(cfg, serviceList, signals)
 
 	return f, nil
 }
 
 func (f *FlorenceFeature) RegisterSteps(ctx *godog.ScenarioContext) {
-	ctx.Step(`^I upload the dataset "([^"]*)"$`, f.iUploadTheDataset)
+	ctx.Step(`^I am logged in as "([^"]*)"$`, f.iAmLoggedInAs)
+	ctx.Step(`^I create a new collection called "([^"]*)"$`, f.iCreateANewCollectionCalled)
 }
 
-func (f *FlorenceFeature) iUploadTheDataset(datasetFileName string) error {
-	opts := append(chromedp.DefaultExecAllocatorOptions[:],
-		chromedp.DisableGPU,
-		chromedp.Flag("headless", false),
-	)
+func (f *FlorenceFeature) iAmLoggedInAs(username string) error {
 
-	allocCtx, cancel := chromedp.NewExecAllocator(context.Background(), opts...)
-	defer cancel()
-
-	ctx, cancel := chromedp.NewContext(allocCtx, chromedp.WithLogf(log.Printf))
-	defer cancel()
-
-	time.Sleep(5 * time.Second)
-
-	log.Print("starting chrome ...")
-
-	var example string
-
-	err := chromedp.Run(ctx,
+	err := chromedp.Run(f.chrome.ctx,
 		chromedp.Navigate("http://localhost:8080/florence/login"),
 		chromedp.WaitVisible(`#app`),
-		chromedp.OuterHTML(`#app`, &example),
+		chromedp.SendKeys("#email", username),
+		chromedp.SendKeys("#password", "anything"),
+		chromedp.Click(".form button"),
 	)
 
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	fmt.Printf("Got HTML:\n%s", example)
+	f.LoggedInUser = username
+	return nil
+}
 
+func (f *FlorenceFeature) iCreateANewCollectionCalled(collectionName string) error {
+	_ = chromedp.Run(f.chrome.ctx,
+		//chromedp.WaitVisible(`#collection-name"`),
+
+		chromedp.SendKeys("#collection-name", collectionName),
+		chromedp.Click(`input[value="manual"]`),
+		chromedp.Click("button"),
+	)
+	time.Sleep(100 * time.Second)
 	return nil
 }
 
 func (f *FlorenceFeature) Reset() *FlorenceFeature {
+	f.FakeApiRouter.Reset()
+
+	f.FakeApiRouter.NewHandler().Post("/login").Reply(200).Body([]byte(`bfe8fb97c7527d7f0f9db6b92e82e4f0866284f17d66ee5a8294dee9793880d6`))
+	f.setJsonResponseForPost("/ping", `{"hasSession":true}`)
+
+	f.FakeApiRouter.NewHandler().AssertBody([]byte(`{"name":"Census 2021"}`)).Post("/collection").Reply(200).Body([]byte(`{"id":"abc123","name":"Census 2021"}`))
+
+	f.setJsonResponseForGet("/collectionDetails/abc123", `
+{
+    "approvalStatus": "IN_PROGRESS",
+    "publishComplete": false,
+    "isEncrypted": false,
+    "timeseriesImportFiles": [],
+    "id": "abc123",
+    "name": "my collection",
+    "type": "manual",
+    "teams": []
+}`)
+	f.setJsonResponseForGet("/permission", fmt.Sprintf(`{"email":"%s","admin":true,"editor":true}`, f.LoggedInUser))
+	f.setJsonResponseForGet("/collections", `
+[{
+    "approvalStatus": "IN_PROGRESS",
+    "publishComplete": false,
+    "isEncrypted": false,
+    "timeseriesImportFiles": [],
+    "id": "testg-4e611aeced894265d544038432771a1351ca892cd5e1796086c15c92ebfee5a5",
+    "name": "TEST G",
+    "type": "manual",
+    "teams": []
+}]`)
+	f.setJsonResponseForGet("/teams", `{"teams":[{"id":3,"name":"Some team","members":["gracie@gracie.com"]}]}`)
+
+	opts := append(chromedp.DefaultExecAllocatorOptions[:],
+		chromedp.DisableGPU,
+		chromedp.Flag("headless", false),
+	)
+
+	allocCtx, cancel := chromedp.NewExecAllocator(context.Background(), opts...)
+	f.chrome.execAllocatorCanceller = cancel
+
+	ctx, cancel := chromedp.NewContext(allocCtx, chromedp.WithLogf(log.Printf))
+	f.chrome.ctxCanceller = cancel
+
+	time.Sleep(1 * time.Second)
+	log.Print("starting chrome ...")
+
+	f.chrome.ctx = ctx
 	return f
 }
 
 func (f *FlorenceFeature) Close() error {
 	dplog.Event(f.ctx, "Shutting down app from test ...")
-	_ = f.svc.Close(f.ctx)
+	if f.svc != nil {
+		_ = f.svc.Close(f.ctx)
+	}
+
+	f.chrome.ctxCanceller()
+	f.chrome.execAllocatorCanceller()
+
 	return nil
 }
 
@@ -133,8 +180,30 @@ func (f *FlorenceFeature) DoGetHTTPServer(bindAddr string, router http.Handler) 
 	return f.HTTPServer
 }
 
+func (f *FlorenceFeature) setJsonResponseForGet(url string, responseBody string) {
+	f.FakeApiRouter.NewHandler().Get(url).Reply(200).SetHeader("Content-Type", "application/json").Body([]byte(responseBody))
+}
+
+func (f *FlorenceFeature) setJsonResponseForPost(url string, responseBody string) {
+	f.FakeApiRouter.NewHandler().Post(url).Reply(200).SetHeader("Content-Type", "application/json").Body([]byte(responseBody))
+}
+
+func (f *FlorenceFeature) runApplication(cfg *config.Config, serviceList *service.ExternalServiceList, signals chan os.Signal) {
+	go func() {
+		f.svc, _ = service.Run(f.ctx, cfg, serviceList, "1", "", "", f.errorChan)
+
+		// blocks until an os interrupt or a fatal error occurs
+		select {
+		case err := <-f.errorChan:
+			dplog.Event(f.ctx, "service error received", dplog.ERROR, dplog.Error(err))
+		case sig := <-signals:
+			dplog.Event(f.ctx, "os signal received", dplog.Data{"signal": sig}, dplog.INFO)
+		}
+	}()
+}
+
 // DoGetHealthCheck creates a healthcheck with versionInfo
-func (e *FlorenceFeature) DoGetHealthcheckOk(cfg *config.Config, buildTime, gitCommit, version string) (service.HealthChecker, error) {
+func DoGetHealthcheckOk(cfg *config.Config, buildTime, gitCommit, version string) (service.HealthChecker, error) {
 	return &mock.HealthCheckerMock{
 		AddCheckFunc: func(name string, checker healthcheck.Checker) error { return nil },
 		StartFunc:    func(ctx context.Context) {},
@@ -143,6 +212,6 @@ func (e *FlorenceFeature) DoGetHealthcheckOk(cfg *config.Config, buildTime, gitC
 }
 
 // GetHealthClient returns a healthclient for the provided URL
-func (e *FlorenceFeature) DoGetHealthClient(name, url string) *health.Client {
+func DoGetHealthClient(name, url string) *health.Client {
 	return &health.Client{}
 }
