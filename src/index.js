82f4a405a617b0d5f60622818e5307fa2126c581
Daniel W <danieldwalford@gmail.com>
2021-04-08 15:33:46 +0100

    rename loginController2 to signInController

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ import { setConfig } from "./app/config/actions";
 import App from "./app/App";
 import Layout from "./app/global/Layout";
 import LoginController from "./app/views/login/LoginController";
-import LoginController2 from "./app/views/login/SignInController";
+import SignInController from "./app/views/login/SignInController";
 import CollectionsController from "./app/views/collections/CollectionsController";
 import TeamsController from "./app/views/teams/TeamsController";
 import SelectADataset from "./app/views/datasets-new/DatasetsController";
@@ -194,8 +194,7 @@ class Index extends Component {
                             )}
                             <Route path={`${rootPath}/selectable-list`} component={SelectableTest} />
                             <Route path={`${rootPath}/logs`} component={Logs} />
-                            <Route path={`${rootPath}/login`} component={config.enableNewSignIn ? LoginController2 : LoginController} />
-                            <Route path={`${rootPath}/login`} component={LoginController2} />
+                            <Route path={`${rootPath}/login`} component={config.enableNewSignIn ? SignInController : LoginController} />
                             <Route path="*" component={UnknownRoute} />
                         </Route>
                     </Route>
