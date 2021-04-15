582756f5698b0d09b00014f4ddfd433dc48de044
Jon Acker <jon.acker@yoyo.org>
2021-04-15 16:58:00 +0100

    Add dependency libraries

--- ci/scripts/component.sh
+++ ci/scripts/component.sh
@@ -3,8 +3,11 @@
 cwd=$(pwd)
 
 pushd $cwd/florence
+    apt-get -y update
+    apt-get install -y libappindicator1 fonts-liberation libnss3 libxkbcommon0 libxshmfence1 xdg-utils libasound2 libgbm1
+#    apt-get install -y --no-install-recommends apt-utils
     wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-    dpkg -i google-chrome-stable_current_amd64.deb
-    apt -f install
+    dpkg -i --force-depends google-chrome-stable_current_amd64.deb
+#    apt -f -y  install
     make test-component
 popd
