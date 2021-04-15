362824a216c55217c95ccd8d87294084426b4cde
Jon Acker <jon.acker@yoyo.org>
2021-04-15 16:32:08 +0100

    use canonical-chromium-builds

--- ci/scripts/component.sh
+++ ci/scripts/component.sh
@@ -4,6 +4,9 @@ cwd=$(pwd)
 
 pushd $cwd/florence
     apt-get -y update
-    apt-get -y install google-chrome-stable
+    apt-get -y install software-properties-common
+    add-apt-repository ppa:canonical-chromium-builds/stage
+    apt-get -y update
+    apt-get -y install chromium-browser
     make test-component
 popd
