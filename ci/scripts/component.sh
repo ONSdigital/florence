d0fdc2d5fb5af5c8c8c7bb84347779b95582a403
Jon Acker <jon.acker@yoyo.org>
2021-04-16 12:13:01 +0100

    ubuntu 18 method

--- ci/scripts/component.sh
+++ ci/scripts/component.sh
@@ -3,11 +3,14 @@
 cwd=$(pwd)
 
 pushd $cwd/florence
-    apt-get -y update
-    apt-get install -y libappindicator1 fonts-liberation
+    echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google-chrome.list
 #    apt-get install -y --no-install-recommends apt-utils
-    wget https://dl-ssl.google.com/linux/direct/google-chrome-stable_current_i386.deb
-    dpkg -i --force-depends google-chrome-stable_current_i386.deb
+#    wget https://dl-ssl.google.com/linux/direct/google-chrome-stable_current_i386.deb
+#    dpkg -i --force-depends google-chrome-stable_current_i386.deb
 #    apt -f -y  install
+    wget https://dl.google.com/linux/linux_signing_key.pub
+    apt-key add linux_signing_key.pub
+    apt -y update
+    apt -y install google-chrome-stable
     make test-component
 popd
