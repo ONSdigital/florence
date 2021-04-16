7531b158d5c201395fa2f5c9e3f8358e2a3ed7db
Jon Acker <jon.acker@yoyo.org>
2021-04-16 09:18:05 +0100

    use i386 image

--- ci/scripts/component.sh
+++ ci/scripts/component.sh
@@ -4,10 +4,10 @@ cwd=$(pwd)
 
 pushd $cwd/florence
     apt-get -y update
-    apt-get install -y libappindicator1 fonts-liberation libnss3 libxkbcommon0 libxshmfence1 xdg-utils libasound2 libgbm1
+    apt-get install -y libappindicator1 fonts-liberation
 #    apt-get install -y --no-install-recommends apt-utils
-    wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-    dpkg -i --force-depends google-chrome-stable_current_amd64.deb
+    wget https://dl-ssl.google.com/linux/direct/google-chrome-stable_current_i386.deb
+    dpkg -i --force-depends google-chrome-stable_current_i386.deb
 #    apt -f -y  install
     make test-component
 popd
