131a0a66896e179f6365fc68ee0fa594fd2f9c91
Jon Acker <jon.acker@yoyo.org>
2021-04-16 16:25:03 +0100

    try gdebi-core

--- ci/scripts/component.sh
+++ ci/scripts/component.sh
@@ -3,15 +3,13 @@
 cwd=$(pwd)
 
 pushd $cwd/florence
-    echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google-chrome.list
 #    apt-get install -y --no-install-recommends apt-utils
-#    wget https://dl-ssl.google.com/linux/direct/google-chrome-stable_current_i386.deb
-#    dpkg -i --force-depends google-chrome-stable_current_i386.deb
-#    apt -f -y  install
-    wget https://dl.google.com/linux/linux_signing_key.pub
-    apt-key add linux_signing_key.pub
     apt -y update
-    apt -u install apt-utils
-    apt -y install google-chrome-stable
+    apt -y install gdebi-core
+#    wget https://dl-ssl.google.com/linux/direct/google-chrome-stable_current_i386.deb
+    wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+    gdebi google-chrome-stable_current_amd64.deb
+#    apt -f -y  install
+#    apt -y install google-chrome-stable
     make test-component
 popd
