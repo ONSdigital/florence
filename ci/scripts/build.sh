d275a60af6c7e6e40f3713afa6b2a31d5f9d2082
Jessica Jenkins <jess.jenkins@ons.gov.uk>
2021-07-15 17:25:47 +0100

    Restore build.sh and move sleep workaround to makefile

--- ci/scripts/build.sh
+++ ci/scripts/build.sh
@@ -3,16 +3,5 @@
 cwd=$(pwd)
 
 pushd $cwd/florence
-  make node-modules
-
-  # Sleeping for a bit if necessary as a workaround to allow time for main.min.css to finish writing
-  for i in {1..10}
-    do if [ -e dist/legacy-assets/css/main.min.css ]
-      then break
-    fi
-    echo "Waiting for main.min.css $i/10"
-    sleep 2
-  done
-
-  make generate-go-prod && cp Dockerfile.concourse build/florence $cwd/build
+  make build && cp Dockerfile.concourse build/florence $cwd/build
 popd
