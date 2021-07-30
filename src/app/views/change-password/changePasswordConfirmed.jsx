c1fb9b9974fb69436e10e4aed1c4327f2135c915
sandhya padmanabhan <sandy.padmanabhan@ons.gov.uk>
2021-07-30 13:59:10 +0100

    Deleted unnecessary proptypes and files

--- src/app/views/change-password/changePasswordConfirmed.jsx
+++ src/app/views/change-password/changePasswordConfirmed.jsx
@@ -2,33 +2,18 @@ import React from "react";
 import PropTypes from "prop-types";
 import Panel from "../../components/panel/Panel";
 
-const propTypes = {
-    validationErrors: PropTypes.shape({
-        hading: PropTypes.string,
-        body: PropTypes.oneOfType([PropTypes.string, PropTypes.element])
-    }),
-    onSubmit: PropTypes.func,
-    emailInput: PropTypes.shape({
-        id: PropTypes.string,
-        label: PropTypes.string,
-        type: PropTypes.string,
-        onChange: PropTypes.func,
-        error: PropTypes.string
-    })
-};
-
 const ChangePasswordConfirmed = props => {
     const changePasswordPanelBody = (
         <p>
             If you have technical problems with your account, please email
-            <a href="mailto:publishing@ons.gov.uk">publishing@ons.gov.uk</a>
+            <a href="mailto:publishing@ons.gov.uk"> publishing@ons.gov.uk</a>
         </p>
     );
 
     return (
         <div className="grid grid--justify-center">
             <div className="grid__col-3">
-                <h1> Your password has been changed</h1>
+                <h1>Your password has been changed</h1>
                 <a href="/florence/collections" className="margin-bottom--2 margin-right--5 anchor-btn">
                     Start using Florence
                 </a>
@@ -37,5 +22,5 @@ const ChangePasswordConfirmed = props => {
         </div>
     );
 };
-ChangePasswordConfirmed.propTypes = propTypes;
+
 export default ChangePasswordConfirmed;
--- src/app/views/change-password/changePasswordRequest.jsx
+++ src/app/views/change-password/changePasswordRequest.jsx
@@ -1,30 +1,15 @@
 import React from "react";
 import PropTypes from "prop-types";
 
-const propTypes = {
-    validationErrors: PropTypes.shape({
-        hading: PropTypes.string,
-        body: PropTypes.oneOfType([PropTypes.string, PropTypes.element])
-    }),
-    onSubmit: PropTypes.func,
-    emailInput: PropTypes.shape({
-        id: PropTypes.string,
-        label: PropTypes.string,
-        type: PropTypes.string,
-        onChange: PropTypes.func,
-        error: PropTypes.string
-    })
-};
-
 const ChangePasswordRequest = props => {
     // TODO this page is to be done in an upcoming task
     return (
         <div className="grid grid--justify-center">
             <div className="grid__col-3">
-                <h1>We are on change password request</h1>
+                <h1>Request password change</h1>
             </div>
         </div>
     );
 };
-ChangePasswordRequest.propTypes = propTypes;
+
 export default ChangePasswordRequest;
