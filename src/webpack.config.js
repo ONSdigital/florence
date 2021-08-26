171e4c1c96ca25e822ca7a8cf9489e38a8eb1e84
Anna Emson <17829828+AnnaEmson@users.noreply.github.com>
2021-08-26 17:06:36 +0100

    add modes to the config and update

--- src/webpack.config.js
+++ src/webpack.config.js
@@ -2,71 +2,73 @@ const path = require('path');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
-module.exports = {
-    mode: process.env.NODE_ENV,
-    entry: {
-        app: ['./index.js'],
-        // tablebuilder: './tablebuilder/tablebuilder.js'
-    },
-    output: {
-        path: path.resolve(__dirname, '../dist'),
-        filename: 'js/[name].bundle.js',
-    },
-    resolve: {
-        modules: [path.resolve(__dirname, 'src'), 'node_modules'],
-        extensions: ['.js', '.jsx']
-    },
-    devServer: {
-        contentBase: path.join(__dirname, 'src')
-    },
-    module: {
-        rules: [
-            {
-                test: /\.(js|jsx)$/,
-                exclude: /(node_modules)/,
-                use: {
-                    loader: 'babel-loader', 
-                    options: {
-                        presets: 
-                        [
+module.exports = env => {
+    return {
+        mode: env.development ? 'development': 'production',
+        entry: {
+            app: ['./index.js'],
+            // tablebuilder: './tablebuilder/tablebuilder.js'
+        },
+        output: {
+            path: path.resolve(__dirname, '../dist'),
+            filename: 'js/[name].bundle.js',
+        },
+        resolve: {
+            modules: [path.resolve(__dirname, 'src'), 'node_modules'],
+            extensions: ['.js', '.jsx']
+        },
+        devServer: {
+            contentBase: path.join(__dirname, 'src')
+        },
+        module: {
+            rules: [
+                {
+                    test: /\.(js|jsx)$/,
+                    exclude: /(node_modules)/,
+                    use: {
+                        loader: 'babel-loader', 
+                        options: {
+                            presets: 
                             [
-                                '@babel/preset-env',
-                                {
-                                    targets: {
-                                    "esmodules": true,
+                                [
+                                    '@babel/preset-env',
+                                    {
+                                        targets: {
+                                        "esmodules": true,
+                                        },
                                     },
-                                },
+                                ],
+                                '@babel/preset-react'
                             ],
-                            '@babel/preset-react'
-                        ],
-                    }
-                } 
+                        }
+                    } 
+                },
+                {
+                    test: /\.scss$/i,
+                    use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
             },
             {
-                test: /\.scss$/i,
-                use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
-        },
-        {
-            test: /\.(jpg|jpeg|png|gif|mp3|svg)$/i,
-            use: [{
-                loader: 'url-loader',
-                options: { limit: 100000,},
+                test: /\.(jpg|jpeg|png|gif|mp3|svg)$/i,
+                use: [{
+                    loader: 'url-loader',
+                    options: { limit: 100000,},
+                },
+                ],
             },
-            ],
-        },
-    ]},
-    plugins: [
-        new CopyWebpackPlugin({
-            patterns: [
-            { from: 'refactored.html', to: 'refactored.html' },
-            { from: 'manifest.json', to: 'manifest.json' },
-            { from: 'service-worker.js', to: 'service-worker.js' },
-            { from: 'img', to: 'img' }
-            ]
-        }),
-        new MiniCssExtractPlugin({filename: "css/[name].css"})
-    ],
-    externals: {
-        resumeablejs: "Resumable"
+        ]},
+        plugins: [
+            new CopyWebpackPlugin({
+                patterns: [
+                { from: 'refactored.html', to: 'refactored.html' },
+                { from: 'manifest.json', to: 'manifest.json' },
+                { from: 'service-worker.js', to: 'service-worker.js' },
+                { from: 'img', to: 'img' }
+                ]
+            }),
+            new MiniCssExtractPlugin({filename: "css/[name].css"})
+        ],
+        externals: {
+            resumeablejs: "Resumable"
+        }
     }
 };
