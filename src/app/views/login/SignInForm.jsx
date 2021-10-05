a4856ce0e8b44fe5ed5cac44cf279c5ea22174b0
Daniel W <danieldwalford@gmail.com>
2021-10-05 12:00:32 +0100

    First time sign in check added

--- src/app/views/login/SignInForm.jsx
+++ src/app/views/login/SignInForm.jsx
@@ -25,23 +25,27 @@ const propTypes = {
 
 const LoginForm = props => {
     const showValidationErrorPanel = props.validationErrors.body && props.validationErrors.body.length > 0;
+    const firstTimeSignIn = new URLSearchParams(location.search).get("firstTimeSignIn");
+    const isFirstTimeSignIn = (firstTimeSignIn != null) && (firstTimeSignIn === 'true');
     return (
         <div className="grid grid--justify-center">
             <div className="grid__col-3">
                 <h1>Sign in to your Florence account</h1>
                 {showValidationErrorPanel && (
                     <div className="margin-bottom--1">
-                        <Panel type={"error"} heading={props.validationErrors.heading} body={props.validationErrors.body} />
+                        <Panel type={"error"} heading={props.validationErrors.heading}
+                               body={props.validationErrors.body}/>
                     </div>
                 )}
                 <form className="form" onSubmit={props.onSubmit}>
                     {props.inputs.map((input, index) => {
-                        return <Input key={index} {...input} disabled={props.isSubmitting} allowAutoComplete={true} />;
+                        return <Input key={index} {...input} disabled={props.isSubmitting} allowAutoComplete={true}/>;
                     })}
+                    {!isFirstTimeSignIn &&
                     <div>
                         <a href={"/florence/forgotten-password"}>Forgotten your password?</a>
-                    </div>
-                    <ButtonWithSpinner isSubmitting={props.isSubmitting} buttonText="Sign in" />
+                    </div>}
+                    <ButtonWithSpinner isSubmitting={props.isSubmitting} buttonText="Sign in"/>
                 </form>
             </div>
         </div>
