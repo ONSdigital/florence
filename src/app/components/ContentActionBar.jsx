6fb22e42e982fe1258963829513523c77d1cdd72
Daniel W <danieldwalford@gmail.com>
2021-10-12 15:27:53 +0100

    Content-action-bar with buttons ready
    WIP Links, tests and floating variation to complete next

--- src/app/components/ContentActionBar.jsx
+++ src/app/components/ContentActionBar.jsx
@@ -3,10 +3,9 @@ import React from "react";
 import PropTypes from "prop-types";
 
 const buttonStyle = {
+    STANDARD: "standard",
     POSITIVE: "positive",
     PRIMARY: "primary",
-    NEGATIVE: "negative",
-    INVERSE: "inverse",
     WARNING: "warning",
     SUBTLE: "subtle",
     INVERT_PRIMARY: "invert-primary",
@@ -29,40 +28,43 @@ const propTypes = {
 
 const ContentActionBar = props => {
 
-    let outerContainerClasses = props.stickToBottom ? "content-action-bar" : "";
+    const outerContainerClasses = props.stickToBottom ? "grid grid--justify-center content-action-bar content-action-bar--bottom-fixed" : "content-action-bar";
+    const innerContainerClasses = props.stickToBottom ? "grid__col-10" : "";
     return (
         <div className={outerContainerClasses}>
-            <div className="grid grid--justify-left padding-bottom--1 padding-top--1">
-                {props.buttons.map((btn, index) => {
-                    let classes = "btn"
-                    if (btn.style !== null) {
-                        classes += ` btn--${btn.style}`
-                    }
-                    classes += (index > 0) ? " margin-left--1" : ""
-                    return (<button
-                        id={btn.id}
+            <div className={innerContainerClasses}>
+                <div className="padding-bottom--1 padding-top--1">
+                    {props.buttons.map((btn, index) => {
+                        let classes = "btn"
+                        if ((btn.style !== null) && (btn.style !== buttonStyle.STANDARD)) {
+                            classes += ` btn--${btn.style}`
+                        }
+                        classes += (index > 0) ? " btn--margin-right btn--margin-left" : ""
+                        return (<button
+                            id={btn.id}
+                            type="button"
+                            onClick={btn.onClick}
+                            disabled={btn.disabled}
+                            className={classes}
+                        >{btn.text}</button>)
+                    })}
+                    {props.cancel && <button
+                        id="cancel"
                         type="button"
-                        onClick={btn.onClick}
-                        disabled={btn.disabled}
-                        className={classes}
-                    >{btn.text}</button>)
-                })}
-                {props.cancel && <button
-                    id="cancel"
-                    type="button"
-                    onClick={props.cancel}
-                    disabled={props.cancelDisabled}
-                    className="btn btn--invert-primary margin-left--1"
-                >Cancel</button>}
-                {props.unsavedChanges &&
-                <svg className="svg-icon--action-bar margin-left--1" xmlns="http://www.w3.org/2000/svg"
-                     viewBox="0 0 512 512" focusable="false">
-                    <path d="M256,34.297L0,477.703h512L256,34.297z M256,422.05c-9.22,0-16.696-7.475-16.696-16.696s7.475-16.696,16.696-16.696
-                        c9.22,0,16.696,7.475,16.696,16.696S265.22,422.05,256,422.05z M239.304,344.137V177.181h33.391v166.956H239.304z"/>
-                </svg>}
-                {props.unsavedChanges &&
-                <span className="content-action-bar__warn"> You have unsaved changes</span>}
-
+                        onClick={props.cancel}
+                        disabled={props.cancelDisabled}
+                        className="btn btn--invert-primary btn--margin-left"
+                    >Cancel</button>}
+                    {props.unsavedChanges &&
+                    <span className="content-action-bar__warn">
+                        <svg className="svg-icon--action-bar" xmlns="http://www.w3.org/2000/svg"
+                             viewBox="0 0 512 512" focusable="false">
+                            <path d="M256,34.297L0,477.703h512L256,34.297z M256,422.05c-9.22,0-16.696-7.475-16.696-16.696s7.475-16.696,16.696-16.696
+                            c9.22,0,16.696,7.475,16.696,16.696S265.22,422.05,256,422.05z M239.304,344.137V177.181h33.391v166.956H239.304z"/>
+                        </svg>
+                        <span className="content-action-bar__warn__text"> You have unsaved changes</span>
+                    </span>}
+                </div>
             </div>
         </div>
     );
