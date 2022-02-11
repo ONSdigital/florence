1f9af45fad7ab867554ab5c5cebff86c05037bd4
Joshua Singh <joshua.singh@ons.gov.uk>
2022-02-11 13:00:53 +0000

    Added comments related to plugin

--- src/legacy/js/functions/_tags.js
+++ src/legacy/js/functions/_tags.js
@@ -7,15 +7,21 @@
     var html = templates.tags()
     $('#tags').replaceWith(html);
 
+    // Set up select2 plugin for Topics
+    // Set multiple to true to get associated 'tag' style but set maximumSelectionLength to 1 for now with potential to remove in the future.
     $('#selectTopic').select2({
         placeholder: 'Select Topics',
         multiple: true,
         maximumSelectionLength: 1,
     });
+
+    // Set up select2 plugin for Subtopics 
     $('#selectSubTopic').select2({
         placeholder: 'Select Subtopics',
         multiple: true,
     });
+
+    // Prevents the dropdown appearing when you clear a tag as it reads that as also focusing the select. 
     $("select").on('select2:unselecting', function (e) { 
         $("select").on('select2:opening', function (e) {
           e.preventDefault();
@@ -23,6 +29,7 @@
         });
     });
 
+    // Prepopulate widget based on existing entries on disk.
     getTopics(templateData.description.primaryTopic)
     getSubTopics(templateData.description.primaryTopic, templateData.description.secondaryTopics)
 }
