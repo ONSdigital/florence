347fe9b09aafe3fec799ed2bbd197b0e4b1aa47f
James Stephens <james.stephens@nitrondevelopments.co.uk>
2022-05-24 15:14:10 +0100

    Fix: Rename ons_user to ons_auth_state.

--- src/app/utilities/auth.js
+++ src/app/utilities/auth.js
@@ -1,7 +1,7 @@
 /**
  * Auth Token:
  *  The concept of Auth Token is an object as a string stored in LocalStorage with a
- *  key of `ons_user`. The value is in the format of -
+ *  key of `ons_auth_state`. The value is in the format of -
  *  {
  *      "email":"<EMAIL>",
  *      "admin": true,
@@ -11,77 +11,77 @@
 import user from "../utilities/api-clients/user";
 import fp from "lodash/fp";
 
-const _AUTH_TOKEN_NAME = "ons_user";
+const _AUTH_TOKEN_NAME = "ons_auth_state";
 
 export const UserRole = Object.freeze({
-    ADMIN: "ADMIN",
-    EDITOR: "EDITOR",
-    VIEWER: "VIEWER",
+  ADMIN: "ADMIN",
+  EDITOR: "EDITOR",
+  VIEWER: "VIEWER",
 });
 
 export default class auth {
-    static isAuthenticated({ isAuthenticated }) {
-        return isAuthenticated;
-    }
+  static isAuthenticated({ isAuthenticated }) {
+    return isAuthenticated;
+  }
 
-    static isViewer({ userType }) {
-        return userType === UserRole.VIEWER;
-    }
+  static isViewer({ userType }) {
+    return userType === UserRole.VIEWER;
+  }
 
-    static isAdminOrEditor({ userType }) {
-        return userType === UserRole.ADMIN || userType === UserRole.EDITOR;
-    }
+  static isAdminOrEditor({ userType }) {
+    return userType === UserRole.ADMIN || userType === UserRole.EDITOR;
+  }
 
-    static isAdmin({ userType }) {
-        return userType === UserRole.ADMIN;
-    }
+  static isAdmin({ userType }) {
+    return userType === UserRole.ADMIN;
+  }
 
-    static canViewCollectionsDetails({ userType }) {
-        return userType === UserRole.ADMIN || userType === UserRole.EDITOR;
-    }
+  static canViewCollectionsDetails({ userType }) {
+    return userType === UserRole.ADMIN || userType === UserRole.EDITOR;
+  }
 }
 
 export function setAuthToken(userData) {
-    const userJSONData = JSON.stringify(userData);
-    window.localStorage.setItem(_AUTH_TOKEN_NAME, userJSONData);
-    /* Legacy florence */
-    window.localStorage.setItem("loggedInAs", userData.email);
-    // Store the user type in localStorage. Used in old Florence
-    // where views can depend on user type. e.g. Browse tree
-    localStorage.setItem("userType", user.getOldUserType(userData));
+  const userJSONData = JSON.stringify(userData);
+  window.localStorage.setItem(_AUTH_TOKEN_NAME, userJSONData);
+  /* Legacy florence */
+  window.localStorage.setItem("loggedInAs", userData.email);
+  // Store the user type in localStorage. Used in old Florence
+  // where views can depend on user type. e.g. Browse tree
+  localStorage.setItem("userType", user.getOldUserType(userData));
 }
 
-/** Assumes user is authenticated if ons_user exists in local storage */
+/** Assumes user is authenticated if ons_auth_state exists in local storage */
 export function getAuthToken() {
-    let userData = window.localStorage.getItem(_AUTH_TOKEN_NAME);
-    try {
-        userData = JSON.parse(userData);
-    } catch (err) {
-        console.error("Could not parse auth token from local storage: ");
-        return undefined;
-    }
-    return userData;
+  let userData = window.localStorage.getItem(_AUTH_TOKEN_NAME);
+  try {
+    userData = JSON.parse(userData);
+  } catch (err) {
+    console.error("Could not parse auth token from local storage: ");
+    return undefined;
+  }
+  return userData;
 }
 
 export function removeAuthToken() {
-    window.localStorage.removeItem(_AUTH_TOKEN_NAME);
-    /* ENABLE_NEW_SIGN_IN legacy */
-    window.localStorage.removeItem("access_token");
-    /* Florence legacy */
-    window.localStorage.removeItem("loggedInAs");
-    window.localStorage.removeItem("userType");
+  window.localStorage.removeItem(_AUTH_TOKEN_NAME);
+  /* ENABLE_NEW_SIGN_IN legacy */
+  window.localStorage.removeItem("access_token");
+  /* Florence legacy */
+  window.localStorage.removeItem("loggedInAs");
+  window.localStorage.removeItem("userType");
 }
 
 export function getIsAdminFromAuthToken() {
-    return fp.get("admin")(getAuthToken());
+  return fp.get("admin")(getAuthToken());
 }
 
 /** User Schema - represents the `user` type stored in redux. */
 class _UserSchema {
-    isAuthenticated = false;
-    email = "";
-    userType = ""; // PUBLISHING_SUPPORT...
-    isAdmin = false;
+  isAuthenticated = false;
+  email = "";
+  userType = ""; // PUBLISHING_SUPPORT...
+  isAdmin = false;
 }
 
 /**
@@ -90,16 +90,16 @@ class _UserSchema {
  * @returns {_UserSchema}
  */
 export function getUserTypeFromAuthToken() {
-    const userData = getAuthToken();
-    if (!userData) {
-        // Return a new empty user object to respect the factory functions return type
-        return new _UserSchema();
-    }
-    const _user = new _UserSchema();
-    // We assume the user is authenticated if ons_user exists in local storage
-    _user.isAuthenticated = !!userData;
-    _user.email = userData.email;
-    _user.userType = user.getUserRole(userData.admin, userData.editor);
-    _user.isAdmin = userData.admin;
-    return _user;
+  const userData = getAuthToken();
+  if (!userData) {
+    // Return a new empty user object to respect the factory functions return type
+    return new _UserSchema();
+  }
+  const _user = new _UserSchema();
+  // We assume the user is authenticated if ons_auth_state exists in local storage
+  _user.isAuthenticated = !!userData;
+  _user.email = userData.email;
+  _user.userType = user.getUserRole(userData.admin, userData.editor);
+  _user.isAdmin = userData.admin;
+  return _user;
 }
--- src/legacy/js/classes/florence.js
+++ src/legacy/js/classes/florence.js
@@ -1,101 +1,107 @@
 // The florence object is used for storing application state.
 var Florence = Florence || {
-        babbageBaseUrl: window.location.origin,
-        refreshAdminMenu: function () {
-            // Display a message to show users are on dev or sandpit
-            Florence.environment = isDevOrSandpit();
-            Florence.showDatasetsTab = Florence.globalVars.config.enableDatasetImport;
+  babbageBaseUrl: window.location.origin,
+  refreshAdminMenu: function () {
+    // Display a message to show users are on dev or sandpit
+    Florence.environment = isDevOrSandpit();
+    Florence.showDatasetsTab = Florence.globalVars.config.enableDatasetImport;
 
-            var mainNavHtml = templates.mainNav(Florence);
-            $('.js-nav').html(mainNavHtml);
-        },
-        setActiveCollection: function (collection) {
-            document.cookie = "collection=" + collection.id + ";path=/";
-            if (!collection.publishDate) {
-                var formattedDate = null;
-            } else {
-                var formattedDate = StringUtils.formatIsoDateString(collection.publishDate);
-            }
-            Florence.collection = {
-                id: collection.id,
-                name: collection.name,
-                date: formattedDate,
-                publishDate: collection.publishDate,
-                type: collection.type
-            };
-        }
+    var mainNavHtml = templates.mainNav(Florence);
+    $(".js-nav").html(mainNavHtml);
+  },
+  setActiveCollection: function (collection) {
+    document.cookie = "collection=" + collection.id + ";path=/";
+    if (!collection.publishDate) {
+      var formattedDate = null;
+    } else {
+      var formattedDate = StringUtils.formatIsoDateString(
+        collection.publishDate
+      );
+    }
+    Florence.collection = {
+      id: collection.id,
+      name: collection.name,
+      date: formattedDate,
+      publishDate: collection.publishDate,
+      type: collection.type,
     };
-
+  },
+};
 
 Florence.Editor = {
-    isDirty: false,
-    data: {}
+  isDirty: false,
+  data: {},
 };
 
 // Can't use randomId function in StringUtils because it is later in the main.js (because the concatenation is done alphabetically)
 function S4() {
-    return (((1 + Math.random()) * 0x10000) | 0).toString(16).substring(1);
-};
-Florence.instanceID = (S4() + S4());
+  return (((1 + Math.random()) * 0x10000) | 0).toString(16).substring(1);
+}
+Florence.instanceID = S4() + S4();
 
 Florence.CreateCollection = {
-    selectedRelease: ""
+  selectedRelease: "",
 };
 
 Florence.collection = {};
 
 Florence.collectionToPublish = {};
 
-Florence.globalVars = {pagePath: '', activeTab: false, pagePos: '', welsh: false};
+Florence.globalVars = {
+  pagePath: "",
+  activeTab: false,
+  pagePos: "",
+  welsh: false,
+};
 
 Florence.Authentication = {
-    accessToken: function () {
-        var cookie = CookieUtils.getCookieValue("access_token");
-        var token = localStorage.getItem("ons_user");
-        return cookie || token;
-    },
-    isAuthenticated: function () {
-        return Florence.Authentication.accessToken() !== '';
-    },
-    loggedInEmail: function () {
-        return localStorage.getItem("loggedInAs");
-    },
-    userType: function() {
-        return localStorage.getItem("userType");
-    }
+  accessToken: function () {
+    var cookie = CookieUtils.getCookieValue("access_token");
+    var token = localStorage.getItem("ons_auth_state");
+    return cookie || token;
+  },
+  isAuthenticated: function () {
+    return Florence.Authentication.accessToken() !== "";
+  },
+  loggedInEmail: function () {
+    return localStorage.getItem("loggedInAs");
+  },
+  userType: function () {
+    return localStorage.getItem("userType");
+  },
 };
 
 Florence.ping = {
-    get: function() {
-        return this.entries[this.latestEntryIndex-1];
-    },
-    add: function(ping) {
-        var timeStamp = new Date();
-        this.entries[this.latestEntryIndex] = {timeStamp, ping};
-        this.latestEntryIndex++;
-        if (this.latestEntryIndex >= this.entries.length) this.latestEntryIndex=0;
-    },
-    latestEntryIndex: 0,
-    entries: new Array(200)
-}
+  get: function () {
+    return this.entries[this.latestEntryIndex - 1];
+  },
+  add: function (ping) {
+    var timeStamp = new Date();
+    this.entries[this.latestEntryIndex] = { timeStamp, ping };
+    this.latestEntryIndex++;
+    if (this.latestEntryIndex >= this.entries.length) this.latestEntryIndex = 0;
+  },
+  latestEntryIndex: 0,
+  entries: new Array(200),
+};
 
 Florence.Handler = function (e) {
-    if (Florence.Editor.isDirty) {
-        var result = confirm("You have unsaved changes. Are you sure you want to continue?");
-        if (result === true) {
-            Florence.Editor.isDirty = false;
-            processPreviewLoad();
-            return true;
-        } else {
-            e.preventDefault();
-            return false;
-        }
+  if (Florence.Editor.isDirty) {
+    var result = confirm(
+      "You have unsaved changes. Are you sure you want to continue?"
+    );
+    if (result === true) {
+      Florence.Editor.isDirty = false;
+      processPreviewLoad();
+      return true;
+    } else {
+      e.preventDefault();
+      return false;
     }
+  }
 };
 
 // if running in a node environment export this as a module.
-if (typeof module !== 'undefined') {
-    module.exports = Florence;
+if (typeof module !== "undefined") {
+  module.exports = Florence;
 }
-
-
