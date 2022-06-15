de5a80eed6272fd3725de29c0a933f6bf896b597
Michał Ordon <michal@tehidea.com>
2022-06-15 15:48:11 +0100

    Add resumable upload template

--- src/legacy/js/functions/_loadResumableUploader.js
+++ src/legacy/js/functions/_loadResumableUploader.js
@@ -1,5 +1,10 @@
-function loadResumableUploader() {
+function loadResumableUploader(data) {
+    const html = templates.resumableUploader(data);
+
+    $('body').append(html);
 
     console.log("INFO: loadResumableUploader()")
+
+    startResumableUploader("resumable-uploader-app", data);
 
 }
