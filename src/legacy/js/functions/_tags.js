1ccc6347e1157c2e0a288afd6fd2f1ebf412f33e
Jon Dewi Jones <jondewijones@gmail.com>
2023-02-22 11:06:12 +0000

    abstract logic for adding tag data to page data

--- src/legacy/js/functions/_tags.js
+++ src/legacy/js/functions/_tags.js
@@ -123,3 +123,20 @@ async function getAllTopics(){
         $('#selectSubtopicsSubtopics').append(newOption)
     })
 }
+
+function validateAndSaveTags(data) {
+    if ($("#selectPrimaryTopic").length === 0 && $("#selectSubtopic").length ===0) {
+        console.log("KABOOOOM!")
+        return 
+    }
+
+    if ($("#selectPrimaryTopic").val() && $("#selectSubtopic").val()) {
+        data.description.canonicalTopic = $("#selectPrimaryTopic").val()[0]
+        data.description.secondaryTopics = $("#selectSubtopic").val()
+        console.log("HERE 2")
+    }
+    else if ($("#selectPrimaryTopic").val() && !$("#selectSubtopic").val()) {
+        sweetAlert("Cannot save this page", "A value is required for 'Subtopic' if a 'Topic' has been selected");
+        return
+    } 
+}
