517627e5f39ad7a4e46bc89a090b39c9ab0a245a
Nick Lupton <19624419+mr-nick17@users.noreply.github.com>
2024-11-22 14:54:46 +0000

    1708 fix class name bug

--- src/app/components/redirect-view/Redirect.jsx
+++ src/app/components/redirect-view/Redirect.jsx
@@ -6,7 +6,7 @@ const RedirectView = () => {
         <div className="grid grid--justify-center">
             <h1>Redirecting...</h1>
             <Loader classNames="grid grid--align-center grid--align-self-center" />
-            <a href={location.href} class="margin-top--2">
+            <a href={location.href} className="margin-top--2">
                 Go directly to {location.href}
             </a>
         </div>
