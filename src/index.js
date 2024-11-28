2d2f76bd8079bae2d508f2b13d6f0cfba7f699ab
Nick Lupton <19624419+mr-nick17@users.noreply.github.com>
2024-11-28 15:19:26 +0000

    2086 remove legacy teams controller

--- src/index.js
+++ src/index.js
@@ -11,7 +11,6 @@ import Layout from "./app/components/layout";
 import SignInController from "./app/views/login/SignIn";
 import ForgottenPasswordController from "./app/views/new-password/forgottenPasswordController";
 import Collections from "./app/views/collections";
-import TeamsController from "./app/views/teams/TeamsController";
 import CreateTeam from "./app/views/teams/team-create/CreateTeam"
 import SelectADataset from "./app/views/datasets-new/DatasetsController";
 import DatasetEditionsController from "./app/views/datasets-new/editions/DatasetEditionsController";
