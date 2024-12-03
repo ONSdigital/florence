58f1288835671bb9bfc3a4b6f9f49d8767fc5217
Nick Lupton <19624419+mr-nick17@users.noreply.github.com>
2024-12-03 16:00:52 +0000

    2086 remove flag from _handleApiError.js

--- src/legacy/js/functions/_handleApiError.js
+++ src/legacy/js/functions/_handleApiError.js
@@ -9,26 +9,23 @@ function handleApiError(response) {
         return;
 
     if (response.status === 401) {
-        if (Florence.globalVars.config.enableNewSignIn) {
-            const authState = getAuthState();
-            const refresh_expiry_time = new Date(authState.refresh_expiry_time);
-            console.debug("[FLORENCE] Timers / enableNewSignIn: requesting a new access_token");
-            renewSession()
-                .then(res => {
-                    if (res) {
-                        console.debug("[FLORENCE] Updating access_token & session timer: ", res)
-                        // update the authState, start the session timer with the next session response value
-                        // & restart the refresh timer with the existing refresh value.
-                        const expirationTime = convertUTCToJSDate(res.expirationTime);
-                        setSessionExpiryTime(expirationTime, refresh_expiry_time);
-                    }
-                })
-                .catch(err => console.error("[FLORENCE]: ", err));
-        } else {
-            logout(window.location.pathname);
-        }
+        const authState = getAuthState();
+        const refresh_expiry_time = new Date(authState.refresh_expiry_time);
+        console.debug("[FLORENCE] Timers : requesting a new access_token");
+        renewSession()
+            .then(res => {
+                if (res) {
+                    console.debug("[FLORENCE] Updating access_token & session timer: ", res)
+                    // update the authState, start the session timer with the next session response value
+                    // & restart the refresh timer with the existing refresh value.
+                    const expirationTime = convertUTCToJSDate(res.expirationTime);
+                    setSessionExpiryTime(expirationTime, refresh_expiry_time);
+                }
+            })
+            .catch(err => console.error("[FLORENCE]: ", err));
+
         return;
-    }    
+    }
     if (response.status === 403) {
         //sweetAlert('You are not logged in', 'Please refresh Florence and log back in.');
         // passing pathname through so the user can be brought back to the page once they log back in
@@ -40,13 +37,13 @@ function handleApiError(response) {
 
         if (response.responseJSON) {
             message = response.responseJSON.message;
-            
+
             if (response.responseJSON.data) {
                 let kvs = [];
                 for (let k in response.responseJSON.data) {
                     kvs.push(k + ": " + response.responseJSON.data[k]);
                 }
-                if(kvs.length > 0) {
+                if (kvs.length > 0) {
                     message += "\n\n" + kvs.join("\n");
                 }
             }
