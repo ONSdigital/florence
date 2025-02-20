c7379848531da7280af5d981ee1634174410ba51
Rhys St Romaine <rhys.st.romaine@ons.gov.uk>
2025-02-20 14:05:25 +0000

    Update /groups to use proxy

--- src/app/utilities/api-clients/teams.js
+++ src/app/utilities/api-clients/teams.js
@@ -1,5 +1,7 @@
 import http from "../http";
 
+const API_PROXY_PATH = `/api/${window.getEnv().apiRouterVersion}`;
+
 export default class teams {
     static getAll() {
         return http.get(`/zebedee/teams`).then(response => {
@@ -40,61 +42,61 @@ export default class teams {
     // TODO: new auth work
 
     static createTeam(body) {
-        return http.post(`/groups`, body).then(response => {
+        return http.post(`${API_PROXY_PATH}/groups`, body).then(response => {
             return response;
         });
     }
 
     static addMemberToTeam(id, userID) {
-        return http.post(`/groups/${id}/members`, { user_id: userID }).then(response => {
+        return http.post(`${API_PROXY_PATH}/groups/${id}/members`, { user_id: userID }).then(response => {
             return response;
         });
     }
 
     static deleteMemberFromTeam(id, body) {
-        return http.post(`/groups/${id}/members`, body).then(response => {
+        return http.post(`${API_PROXY_PATH}/groups/${id}/members`, body).then(response => {
             return response;
         });
     }
 
     static getGroups() {
-        return http.get(`/groups?sort=name:asc`).then(response => {
+        return http.get(`${API_PROXY_PATH}/groups?sort=name:asc`).then(response => {
             return response;
         });
     }
 
     static getGroup(id) {
-        return http.get(`/groups/${id}`).then(response => {
+        return http.get(`${API_PROXY_PATH}/groups/${id}`).then(response => {
             return response;
         });
     }
 
     static updateGroup(id, body) {
-        return http.put(`/groups/${id}`, body).then(response => {
+        return http.put(`${API_PROXY_PATH}/groups/${id}`, body).then(response => {
             return response;
         });
     }
 
     static updateGroupMembers(id, body) {
-        return http.put(`/groups/${id}/members`, body).then(response => {
+        return http.put(`${API_PROXY_PATH}/groups/${id}/members`, body).then(response => {
             return response;
         });
     }
 
     static deleteGroup(id) {
-        return http.delete(`/groups/${id}`).then(response => {
+        return http.delete(`${API_PROXY_PATH}/groups/${id}`).then(response => {
             return response;
         });
     }
 
     static getGroupMembers(id) {
-        return http.get(`/groups/${id}/members?sort=forename:asc`).then(response => {
+        return http.get(`${API_PROXY_PATH}/groups/${id}/members?sort=forename:asc`).then(response => {
             return response;
         });
     }
 
     static addGroupsToUser(name, body) {
-        return http.post(`/groups/${name}`, body).then(response => {
+        return http.post(`${API_PROXY_PATH}/groups/${name}`, body).then(response => {
             return response;
         });
     }
