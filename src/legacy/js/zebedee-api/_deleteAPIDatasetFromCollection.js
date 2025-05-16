391d27a4b8dbf6fc532fa5eb3e95f09873d40d60
Jessica Jenkins <jess.jenkins@ons.gov.uk>
2025-05-16 12:41:58 +0100

    Fix typo in legacy JS causing runtime failure

--- src/legacy/js/zebedee-api/_deleteAPIDatasetFromCollection.js
+++ src/legacy/js/zebedee-api/_deleteAPIDatasetFromCollection.js
@@ -1,7 +1,7 @@
 function deleteAPIDataset(collectionId, instanceId, success, error) {
 
     $.ajax({
-        url: `${API_PROXY.VERSIONED_PATH}/collections/${collectionId}/datasets/${instanceId},
+        url: `${API_PROXY.VERSIONED_PATH}/collections/${collectionId}/datasets/${instanceId}`,
         type: 'DELETE',
         success: function (response) {
             if (success)
