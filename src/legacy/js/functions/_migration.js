870badb01b3dc39319515a1225395f001b3295e6
Nick Lupton <19624419+mr-nick17@users.noreply.github.com>
2025-06-20 15:30:48 +0100

    add migration template file and handler

---
+++ src/legacy/js/functions/_migration.js
@@ -0,0 +1,44 @@
+/**
+ * manage migration link
+ * @param templateData
+ * @param data
+ */
+
+async function migration(templateData, data) {
+    let html = templates.migration(templateData)
+    $('#migration').replaceWith(html);
+
+    $('#migration_link').on('input', function () {
+        let input = $(this).val();
+        data.description.migrationLink = input.trim();
+    });
+}
+
+// isRelativePath validates if the given path is a relative path
+function isRelativePath(path) {
+    if (typeof path !== 'string') {
+        return false;
+    }
+    // Exclude periods
+    if (path.includes('.')) {
+        return false; 
+    }
+    // Exclude whitespace
+    if (path.includes(' ')) {
+        return false; 
+    }
+    // Check if the path starts with '/'
+    if (path.startsWith('/')) {
+        return true; 
+    }
+    return false;
+}
+
+// validationMigrationPath handles the validation of the migration path input 
+function validateMigrationPath(input) {
+    if (input && input !== '') {
+        const isValid = isRelativePath(input);
+        return isValid;
+    }
+    return true; // allow empty field
+}
