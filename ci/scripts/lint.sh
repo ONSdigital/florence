a8f8444b1adb7eb4b29df18015acb9384cb0b337
Nick Lupton <19624419+mr-nick17@users.noreply.github.com>
2025-06-24 15:40:31 +0100

    bump go linter to v2 in CI

--- ci/scripts/lint.sh
+++ ci/scripts/lint.sh
@@ -2,8 +2,7 @@
 
 cwd=$(pwd)
 
-go install github.com/golangci/golangci-lint/cmd/golangci-lint@v1.64.6
-
 pushd $cwd/florence
+  go install github.com/golangci/golangci-lint/v2/cmd/golangci-lint@v2.1.6
   make lint
 popd
